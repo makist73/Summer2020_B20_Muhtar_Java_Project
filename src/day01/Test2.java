6ed0497ebf45cd2066d8995613a75d4f9d3baad3
cybertekschool <github@cybertekschool.com>
2020-06-03 21:12:04 -0400

    Day02 Class

--- src/day01/Test2.java
+++ src/day01/Test2.java
@@ -1,4 +1,7 @@
 package day01;
 
 public class Test2 {
+
+
 }
+
---
+++ src/day02_printStatements/HelloCybertek.java
@@ -0,0 +1,14 @@
+package day02_printStatements;
+
+public class HelloCybertek {
+
+    public static void main (String[] args){
+
+        System.out.println("Hello Cybertek");
+        System.out.println("Hello Batch 20");
+        System.out.println("        I Love Java Programming Language");
+
+    }
+
+
+}

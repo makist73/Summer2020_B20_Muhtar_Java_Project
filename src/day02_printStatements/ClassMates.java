a0d10af9b1cf475b01c6e5faf6ac8b35d51b8fe1
cybertekschool <github@cybertekschool.com>
2020-06-03 21:30:54 -0400

    Day02 Class

---
+++ src/day02_printStatements/ClassMates.java
@@ -0,0 +1,15 @@
+package day02_printStatements;
+
+public class ClassMates {
+
+    public static void main(String[] args){
+        System.out.println("   Aalia");
+        System.out.println("   Sefika");
+        System.out.println("   Ayhan");
+        System.out.println("   Tahsin");
+        System.out.println("   Zaiiadin");
+
+    }
+
+
+}

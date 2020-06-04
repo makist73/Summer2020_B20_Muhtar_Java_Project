9a5ce49acb36a422f33625420f8a32508f0821ce
cybertekschool <github@cybertekschool.com>
2020-06-03 21:39:20 -0400

    Day02 Class

---
+++ src/day02_printStatements/ShortCuts.java
@@ -0,0 +1,12 @@
+package day02_printStatements;
+
+public class ShortCuts {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+
+
+    }
+
+}

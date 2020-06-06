d23a4f887585cc60f409df18db20d9663187d640
cybertekschool <github@cybertekschool.com>
2020-06-06 10:34:53 -0400

    Day03 Class

---
+++ src/day03_sequences_Variables/EmployeeInfo.java
@@ -0,0 +1,13 @@
+package day03_sequences_Variables;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Company Name: Bank Of America");
+        System.out.println("Employee Name: John");
+        System.out.println("Employee ID: 12345");
+        System.out.println("Job Title: SDET");
+        System.out.println("Salary: $100000");
+    }
+
+}

24b52fae83294d98273915364b7ce3bd721b661e
cybertekschool <github@cybertekschool.com>
2020-06-06 11:27:07 -0400

    Day02 Class

---
+++ src/day03_sequences_Variables/Print_VS_Println.java
@@ -0,0 +1,20 @@
+package day03_sequences_Variables;
+
+public class Print_VS_Println {
+
+    public static void main(String[] args) {
+        System.out.println("A"); // A + new line
+        System.out.println("B"); // B + new line
+        System.out.println("C"); // C +new line
+
+        System.out.println("=======================");
+
+        System.out.print("A"); // A
+        System.out.print("B"); // B
+        System.out.print("C"); // C
+
+
+
+    }
+
+}

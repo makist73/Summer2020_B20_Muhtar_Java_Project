286e70370b543f687f7fdd1ecd52f58ad9933384
cybertekschool <github@cybertekschool.com>
2020-06-06 10:51:04 -0400

    Day03 Class

--- src/day03_sequences_Variables/EmployeeInfo.java
+++ src/day03_sequences_Variables/EmployeeInfo.java
@@ -1,9 +1,11 @@
 package day03_sequences_Variables;
 
-public class EmployeeInfo {
+public class EmployeeInfo {  //this is a class header
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  //this is main method, used for running the java codes
+
         System.out.println("Company Name: Bank Of America");
+                //print statements is used for printing
         System.out.println("Employee Name: John");
         System.out.println("Employee ID: 12345");
         System.out.println("Job Title: SDET");
@@ -11,3 +13,7 @@ public class EmployeeInfo {
     }
 
 }
+
+
+
+    // today is: 06/06/2020

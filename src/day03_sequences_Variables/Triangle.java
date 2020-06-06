8fd82aaba7f60f3c1bc8859e2ef63881e6a5688c
cybertekschool <github@cybertekschool.com>
2020-06-06 12:55:42 -0400

    Day03 Class

---
+++ src/day03_sequences_Variables/Triangle.java
@@ -0,0 +1,25 @@
+package day03_sequences_Variables;
+/*
+task:
+
+        ^
+       / \
+      /   \
+     /     \
+    /       \
+    ---------
+*/
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t     ^");
+        System.out.println("\t\t    / \\");
+        System.out.println("\t\t   /   \\");
+        System.out.println("\t\t  /     \\");
+        System.out.println("\t\t /_______\\");
+
+    }
+
+
+}

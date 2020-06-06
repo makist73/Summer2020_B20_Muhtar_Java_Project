bdbc9b42a532506fdfc3f8d5faaefda331358681
cybertekschool <github@cybertekschool.com>
2020-06-06 17:08:24 -0400

    Day03 Class

---
+++ src/day03_sequences_Variables/KgToPound.java
@@ -0,0 +1,13 @@
+package day03_sequences_Variables;
+
+public class KgToPound {
+
+    public static void main(String[] args) {
+        double kg = 83.5;
+        double pound = kg * 2.2;
+
+        System.out.println(pound);
+
+    }
+
+}

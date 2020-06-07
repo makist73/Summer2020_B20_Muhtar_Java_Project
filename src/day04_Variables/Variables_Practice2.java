83f506b0ce3d91540cff83ea386de9ff46a751da
cybertekschool <github@cybertekschool.com>
2020-06-07 11:29:10 -0400

    Day04 Class

---
+++ src/day04_Variables/Variables_Practice2.java
@@ -0,0 +1,31 @@
+package day04_Variables;
+
+public class Variables_Practice2 {
+// double > float > long > int > short > byte
+
+    public static void main(String[] args) {
+        int iNum = 50;
+        long lNum = iNum;  //50
+
+       // int iNum2 = lNum;
+        float fNum = lNum; // 50.0
+
+
+    //    float fNum = 500;
+
+    //    float 123fNum = 500;
+
+        float fNum123 = 500;
+        float num$ = 1000;
+
+        //1,000,000
+    int num1 = 1_000_000;
+
+        System.out.println(num1);
+
+
+
+
+    }
+
+}

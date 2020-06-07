b5fa5f9da2e91ae7915342f23d69ed712946ff1d
cybertekschool <github@cybertekschool.com>
2020-06-07 16:41:04 -0400

    Day04 Class

---
+++ src/day04_Variables/Concate_Add.java
@@ -0,0 +1,26 @@
+package day04_Variables;
+
+public class Concate_Add {
+
+    public static void main(String[] args) {
+        // addition: number + number
+        System.out.println( 12 + 13 ); //25
+        System.out.println( 'A' + 2); // 67
+        System.out.println( 'A' + 'B'); // 131
+
+        // Concatenation: append
+        System.out.println("12" + 13); //1213
+        System.out.println("A"+2); //A2
+        System.out.println("Gender: "+'M'); // Gender: M
+        System.out.println("Tax: "+3.5+'%'); // Tax: 3.5
+                        //"Tax: 3.5"+ '%'
+        System.out.println(3.5+'%'+" Tax");
+                         //  3.5+37+"Tax "
+                         //   40.5 + " Tax"
+                        //   40.5 Tax
+
+
+
+    }
+
+}

90a9c090bfc1723f6f182af892c05e37263b9365
cybertekschool <github@cybertekschool.com>
2020-06-07 11:54:15 -0400

    Day04 Class

---
+++ src/day04_Variables/Variables.java
@@ -0,0 +1,48 @@
+package day04_Variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+      boolean result1 =  true;
+       boolean result2 =  false;
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+        //  > , <
+         boolean result3 =   10 > 6000   ;
+
+        System.out.println(result3);
+
+        int a = 300;
+        int b = 45;
+
+        boolean result4 = b < a ;
+
+        System.out.println(result4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}

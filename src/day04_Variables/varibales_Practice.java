f4165b27f3e44ef7a5fa135b1295d2f890c750b1
cybertekschool <github@cybertekschool.com>
2020-06-07 10:58:22 -0400

    Day04 Class

---
+++ src/day04_Variables/varibales_Practice.java
@@ -0,0 +1,37 @@
+package day04_Variables;
+/*
+ create a class called varibales_Practice:
+        decleare the following variables:
+        byte   num1 = 100;
+        short   num2 = 10000;
+        double   num3 = 3.5;
+        float  num4 = 2.5f;
+        long   num5 = 999999999999;
+        int  num6 = 850000;
+
+        print each of the variables above on the console
+        double > float > long > int > short > byte
+*/
+
+public class varibales_Practice {
+
+    public static void main(String[] args) {
+
+      byte  num1 = 100;
+      short num2 = 10000;
+      double  num3 = 3.5;
+      float  num4 = 2.5f;
+      long num5 = 999999999999L;
+
+
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+
+    }
+
+
+}

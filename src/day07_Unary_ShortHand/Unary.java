ca9548b22fb046b2c53013cc9aab3bff329f159f
cybertekschool <github@cybertekschool.com>
2020-06-13 11:02:07 -0400

    Day06 Class

---
+++ src/day07_Unary_ShortHand/Unary.java
@@ -0,0 +1,13 @@
+package day07_Unary_ShortHand;
+
+public class Unary {
+    public static void main(String[] args) {
+        System.out.println( -1 + -10);
+                       //    -1 - 10 ==> -11
+
+        System.out.println(-11 -  -10 );
+        //                 -11 + 10 =-1
+
+    }
+
+}

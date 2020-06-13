573921454c9d10a461d1455ea29c8d05a67c1cae
cybertekschool <github@cybertekschool.com>
2020-06-13 10:18:32 -0400

    Day06 Class

---
+++ src/day07_Unary_ShortHand/LeapYear.java
@@ -0,0 +1,18 @@
+package day07_Unary_ShortHand;
+/*
+1. create a class called LeapYear, and write a program that can identify if the given is Leap Year, print true if it's leap year, otherwise print false
+ */
+
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        int year = 2040;
+          boolean result =  year % 4 == 0;
+
+        System.out.println("Year " + year + " is a leap year: " + result); // 2021 is a leap year: false
+
+    }
+
+}

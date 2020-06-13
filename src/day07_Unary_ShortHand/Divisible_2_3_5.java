51421df71e9e0cddcccb71c5c191b774463cc469
cybertekschool <github@cybertekschool.com>
2020-06-13 10:39:31 -0400

    Day06 Class

---
+++ src/day07_Unary_ShortHand/Divisible_2_3_5.java
@@ -0,0 +1,22 @@
+package day07_Unary_ShortHand;
+
+// write a program that can check if a number is evenly divisible by 2, 3, 5
+
+public class Divisible_2_3_5 {
+
+    public static void main(String[] args) {
+        int number = 25;
+
+        boolean divisibleBy2 = number % 2 ==0 ;
+        boolean divisibleBy3 = number % 3 ==0 ;
+        boolean divisibleBy5 = number % 5 ==0 ;
+
+        System.out.println(number+" is evenly divisible by 2: " + divisibleBy2);
+        System.out.println(number+" is evenly divisible by 3: " + divisibleBy3);
+        System.out.println(number+" is evenly divisible by 5: " + divisibleBy5);
+
+
+    }
+
+
+}

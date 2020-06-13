09daebf1727433d7542963eaeb7820e75d06b94a
cybertekschool <github@cybertekschool.com>
2020-06-13 14:15:18 -0400

    Day06 Class

---
+++ src/day07_Unary_ShortHand/Post_VS_Pre.java
@@ -0,0 +1,30 @@
+package day07_Unary_ShortHand;
+
+public class Post_VS_Pre {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println( ++a ); //11
+        System.out.println(a); // 11
+
+        System.out.println("=================================");
+
+        int b = 10;
+        System.out.println( b++ ); //10
+        System.out.println(b); //11
+
+        System.out.println("==========================");
+        int c = 25;
+        int d = c++;  // d = 25;
+        //  c= 26
+        System.out.println(c);
+        System.out.println(d);
+
+        System.out.println("============================");
+        int e = 25;
+        e++;
+        System.out.println(e);
+
+    }
+
+}

f41a2fd9f16194d115b9e6d8d04c4c99117f78ab
cybertekschool <github@cybertekschool.com>
2020-06-13 12:39:22 -0400

    Day06 Class

---
+++ src/day07_Unary_ShortHand/Positive_Negative_0.java
@@ -0,0 +1,19 @@
+package day07_Unary_ShortHand;
+
+public class Positive_Negative_0 {
+
+    public static void main(String[] args) {
+        double num = 0;
+
+        boolean positive = num > 0;
+        boolean negative = num < 0;
+        boolean zero =   num == 0;
+
+        System.out.println(num +" is positive? "+positive );
+        System.out.println(num +" is negative? "+negative);
+        System.out.println(num + " is Zero? " + zero);
+
+
+    }
+
+}

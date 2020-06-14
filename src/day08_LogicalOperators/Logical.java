f7459ae1543c7f4c877cba3e67c4182c92ffef95
cybertekschool <github@cybertekschool.com>
2020-06-14 11:38:02 -0400

    Day08 Class

---
+++ src/day08_LogicalOperators/Logical.java
@@ -0,0 +1,29 @@
+package day08_LogicalOperators;
+
+public class Logical {
+
+    public static void main(String[] args) {
+       // !: opposite boolean
+       boolean r1 = 9 > 7; // true
+       boolean r2 = !r1;  // false
+
+        System.out.println(r1+" : "+r2);
+
+        System.out.println( !false ); // true
+        System.out.println( !true  ); // false
+
+        boolean r3 = !false == true;
+          //          true ==  true  ==> true
+
+        System.out.println(r3);
+
+        boolean r4 = true == !true; // false
+        System.out.println(r4);
+
+        boolean r5 = 9 > 5 && 9 > 10;
+        //            true  &&  false
+
+
+    }
+
+}

70aeec33aa663bedd89caced623407859220ad5e
cybertekschool <github@cybertekschool.com>
2020-06-16 21:48:09 -0400

    Day09 Class

---
+++ src/day09_IfStatement/Odd_Even.java
@@ -0,0 +1,20 @@
+package day09_IfStatement;
+
+public class Odd_Even {
+
+    public static void main(String[] args) {
+        int number = 101;
+
+        if( number%2 != 0 ){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+
+
+
+
+
+    }
+
+}

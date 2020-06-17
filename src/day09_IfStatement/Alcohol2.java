896cd837fc89df04f0fbce26c8581c17c7b8ac5f
cybertekschool <github@cybertekschool.com>
2020-06-16 21:42:11 -0400

    Day09 Class

---
+++ src/day09_IfStatement/Alcohol2.java
@@ -0,0 +1,17 @@
+package day09_IfStatement;
+
+public class Alcohol2 {
+
+    public static void main(String[] args) {
+        int age = 26;
+
+        if(age >= 21){
+            System.out.println("You can buy alcohol");
+        }else{  // age < 21
+            System.out.println("You can not buy alcohol");
+        }
+
+
+    }
+
+}

7e07fab0afecbbfa4f9bbc70bf8e25984de6922c
cybertekschool <github@cybertekschool.com>
2020-06-17 20:45:44 -0400

    Day10 Class

---
+++ src/day10_ifStatements/MultiBranch.java
@@ -0,0 +1,35 @@
+package day10_ifStatements;
+
+public class MultiBranch {
+
+    public static void main(String[] args) {
+        int number = 100;
+
+        if(number > 0){
+            System.out.println(number +" is positive");
+        }
+
+        if(number < 0){
+            System.out.println(number+ " is negative");
+        }
+
+        if(number == 0){
+            System.out.println(number + " is zero");
+        }
+
+        System.out.println("======================================");
+
+        if(number > 0){
+            System.out.println(number+" is positive");
+        }else if(number < 0){
+            System.out.println(number+" is negative");
+        } else { // number ==0
+            System.out.println(number +" is zero");
+        }
+
+
+
+
+    }
+
+}

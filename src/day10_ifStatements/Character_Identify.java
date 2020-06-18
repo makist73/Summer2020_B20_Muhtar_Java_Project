b21739cad7a208d21867b9ea127f2ae014d9050b
cybertekschool <github@cybertekschool.com>
2020-06-17 20:00:07 -0400

    Day10 Class

--- src/day10_ifStatements/Character_Identify.java
+++ src/day10_ifStatements/Character_Identify.java
@@ -3,12 +3,16 @@ package day10_ifStatements;
 public class Character_Identify {
 
     public static void main(String[] args) {
-        char character = 'A';
+        char character = '@';
         //                        65 <= character <= 90             ||   97 <= character <= 122
         boolean isAlphabetic = (character >= 65 && character <= 90) || (character >= 97 && character <= 122);
                     //           upper case letters     ||  lower Case letter
+
                 // 48 <= CHARACTER <= 57
         boolean isDigit = character >= 48  && character <= 57;
+
+        boolean specialChar = isAlphabetic == false && isDigit == false;
+                // !isAlphabetic && !isDigit
 
         String alphabet = "";
 
@@ -23,6 +27,7 @@ public class Character_Identify {
 
         System.out.println(alphabet);
 
+
         String digit = "";
 
         if(isDigit == true ){
@@ -33,6 +38,15 @@ public class Character_Identify {
 
         System.out.println(digit);
 
+
+        String specialCharacter = "";
+        if(specialChar == true){
+            specialCharacter = character+" is a special character";
+        }else{
+            specialCharacter = character+" is not a special character";
+        }
+
+        System.out.println(specialCharacter);
 
 
     }

148c7924243fd17764f2d709eef64df1ee24425f
cybertekschool <github@cybertekschool.com>
2020-06-20 13:04:26 -0400

    Day11 Class

--- src/day11_NestedIf_Switch/Ternary_Practice2.java
+++ src/day11_NestedIf_Switch/Ternary_Practice2.java
@@ -1,6 +1,8 @@
 package day11_NestedIf_Switch;
 
 public class Ternary_Practice2 {
+    // ?  means if, : else
+
     public static void main(String[] args) {
         int num = 100;
         String result = "";
@@ -13,8 +15,24 @@ public class Ternary_Practice2 {
             result = "Zero";
         }
 
+        String result2 =  (num > 0) ? "Positive" : (num < 0)  ? "Negative" : "Zero";
 
+        System.out.println(result);
+        System.out.println(result2);
 
+        System.out.println("================================================");
+        /*
+            write a program that can compare two number
+                a = 10, b= 20
+               output:  b is greater
+
+               a = 30, b = 10;
+               output: a is greater
+
+               a = 20; b= 20;
+               output: a is equal to b
+
+         */
 
 
     }

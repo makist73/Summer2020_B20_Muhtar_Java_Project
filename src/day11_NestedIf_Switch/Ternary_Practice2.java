77cbaae3bb0d9ff334dc7b915a59e98b2323b466
cybertekschool <github@cybertekschool.com>
2020-06-20 12:53:33 -0400

    Day11 Class

---
+++ src/day11_NestedIf_Switch/Ternary_Practice2.java
@@ -0,0 +1,18 @@
+package day11_NestedIf_Switch;
+
+public class Ternary_Practice2 {
+    public static void main(String[] args) {
+        int num = 100;
+        String result = "";
+
+        if(num > 0){
+            result = "Positive";
+        } else if(num < 0){
+            result = "Negative";
+        }else{
+            result = "Zero";
+        }
+
+
+    }
+}

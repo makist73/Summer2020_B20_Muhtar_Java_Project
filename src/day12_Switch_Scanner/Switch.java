07ac47bf4e676bbf1d576be7fb225e05d2269080
cybertekschool <github@cybertekschool.com>
2020-06-21 12:20:49 -0400

    Day12 Class

---
+++ src/day12_Switch_Scanner/Switch.java
@@ -0,0 +1,29 @@
+package day12_Switch_Scanner;
+
+public class Switch {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        switch(a){
+
+            case 5:
+                System.out.println("Five");
+                break;
+
+            case 1:
+                System.out.println("One");
+                break;
+
+            default:
+                System.out.println("Invalid Case");
+
+        }
+
+
+
+
+    }
+
+}

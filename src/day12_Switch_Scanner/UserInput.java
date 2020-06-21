9b69024e8844a41e5dce9a31973614d0ce494fa8
cybertekschool <github@cybertekschool.com>
2020-06-21 15:49:11 -0400

    Day12 Class

---
+++ src/day12_Switch_Scanner/UserInput.java
@@ -0,0 +1,26 @@
+package day12_Switch_Scanner;
+
+import java.util.Scanner;  // only imports scanner class from java.util
+
+// import java.util.*;  // imports every thing from java util
+
+public class UserInput {
+
+    public static void main(String[] args) {
+        Scanner  input = new Scanner(System.in);
+
+        System.out.println("Enter your number");
+         // int num1 =   input.nextInt();
+        //  long num2 =   input.nextLong();
+
+            double num3    = input.nextDouble();
+
+
+      System.out.println("You entered: " + num3 );
+
+
+
+    }
+
+
+}

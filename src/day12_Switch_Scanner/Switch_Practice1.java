465ae82d1eec8cac30980a10883e84328a4bc054
cybertekschool <github@cybertekschool.com>
2020-06-21 12:47:52 -0400

    Day12 Class

---
+++ src/day12_Switch_Scanner/Switch_Practice1.java
@@ -0,0 +1,51 @@
+package day12_Switch_Scanner;
+
+public class Switch_Practice1 {
+
+    public static void main(String[] args) {
+        int num = 4;
+        String result = "";
+
+        switch(num){
+            case 1:
+               result = "Monday";
+                break;
+
+            case 2:
+               result = "Tuesday";
+                break;
+
+            case 3:
+                result = "Wednesday";
+                break;
+
+            case 4:
+                result = "Thursday";
+                break;
+
+            case 5:
+                result = "Friday";
+                break;
+
+            case 6:
+                result = "Saturday";
+                break;
+
+            case 7:
+               result = "Sunday";
+                break;
+
+            default:
+               result = "Invalid";
+
+        }
+
+
+        System.out.println(result);
+
+
+
+
+    }
+
+}

8cf9d656ebaf509cac8b978e5d321502dda39a1f
cybertekschool <github@cybertekschool.com>
2020-06-23 19:55:00 -0400

    Day13 class

---
+++ src/day13_Scanner/Browsers.java
@@ -0,0 +1,48 @@
+package day13_Scanner;
+/*
+ 2. write a program that can display the selected browser
+            1.1  declear a String variable called browserName
+            1.2  Assume that the valid browsers are: chrome, firefox, opera, safari, edge
+            1.3. if the browser name does not match with the valid browsers' names,
+                    out put should be: Invalid Browser Name
+
+        Do Not use: scanner, if statement, and ternary
+ */
+
+public class Browsers {
+
+    public static void main(String[] args) {
+        String browserName = "safari";
+        // boolean, long, double, float
+
+        switch(browserName){
+            case "chrome":
+                System.out.println("Opening Chrome Browser");
+                break;
+
+            case "firefox":
+                System.out.println("Opening Firefox Browser");
+                break;
+
+            case "opera":
+                System.out.println("Opening Opera Browser");
+                break;
+
+            case "safari":
+                System.out.println("Opening Safari Browser");
+                break;
+
+            case "edge":
+                System.out.println("Opening Edge Browser");
+                break;
+
+            default:
+                System.out.println("Invalid Browser Name");
+        }
+
+
+
+
+    }
+
+}

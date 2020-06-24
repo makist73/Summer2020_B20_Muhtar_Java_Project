6bb6b9ec10283de24a14659ce7a07de3fefbccdd
cybertekschool <github@cybertekschool.com>
2020-06-23 21:51:18 -0400

    Day13 class

--- src/day13_Scanner/nextline_VS_nextMethods.java
+++ src/day13_Scanner/nextline_VS_nextMethods.java
@@ -7,7 +7,11 @@ public class nextline_VS_nextMethods {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your salary: ");
-        double salary = scan.nextDouble();
+        double salary = scan.nextDouble(); //100000
+
+        scan.nextLine(); // Enter
+        // use use this to take out the Enters tht are left in the scanner. so that we can use the next netxline method
+
 
         System.out.println("Enter your full name: ");
         String fullName =  scan.nextLine();

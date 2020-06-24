c28d6a3c3e6bb9b991da56342daa80d795c1144d
cybertekschool <github@cybertekschool.com>
2020-06-24 18:52:36 -0400

    Office Hour: 06/24/2020

---
+++ src/Office_Hours/Practice_06_24_2020/Switch_ShortQuiz2.java
@@ -0,0 +1,38 @@
+package Office_Hours.Practice_06_24_2020;
+
+public class Switch_ShortQuiz2 {
+
+    public static void main(String[] args) {
+        String opt = "true";
+
+        switch(opt){
+
+            case "true":
+                System.out.println("True");
+            break;
+
+            default:
+                System.out.println("False");
+        }
+
+        System.out.println("Done");
+
+        System.out.println(12456 + 1024);
+
+
+        float f1 = (12_345.1 > 123_45.0) ? 12456 : 12456.02f;
+
+        System.out.println(12456 + 1024 );
+        System.out.println("==============================");
+
+        // line 1
+        String x = "1";
+        switch(x){
+            case "1":
+            case "2":
+        }
+
+
+    }
+
+}

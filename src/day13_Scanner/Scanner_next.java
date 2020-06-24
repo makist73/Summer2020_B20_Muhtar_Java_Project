81b2ad0705dac3fa259a7abad08fe3705c8856d4
cybertekschool <github@cybertekschool.com>
2020-06-23 20:48:16 -0400

    Day13 class

---
+++ src/day13_Scanner/Scanner_next.java
@@ -0,0 +1,17 @@
+package day13_Scanner;
+
+import java.util.Scanner;
+
+public class Scanner_next {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your company name: ");
+            String companyName =    scan.next();
+
+        System.out.println("Your company name: " +companyName);
+
+    }
+
+
+}

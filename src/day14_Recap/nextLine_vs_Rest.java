669af6947f8e1e6d90b2d86e95cc2dc4f01eacb9
cybertekschool <github@cybertekschool.com>
2020-06-24 20:04:59 -0400

    Day14 Class

---
+++ src/day14_Recap/nextLine_vs_Rest.java
@@ -0,0 +1,24 @@
+package day14_Recap;
+
+import java.util.Scanner;
+
+public class nextLine_vs_Rest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+         int age =   input.nextInt(); //19
+
+       input.nextLine(); // Enter
+
+        System.out.println("Enter your full Name");
+       String fullName = input.nextLine(); // Cybertek SchoolEnter
+
+        System.out.println("Enter your company Name: ");
+        String companyName = input.nextLine();
+
+        System.out.println(fullName+" is "+age+" years old");
+
+
+    }
+}

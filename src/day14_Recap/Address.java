6c6cff399b366ec7e12eecceea78074767ad9e1e
cybertekschool <github@cybertekschool.com>
2020-06-24 20:46:21 -0400

    Day14 Class

--- src/day14_Recap/Address.java
+++ src/day14_Recap/Address.java
@@ -1,4 +1,22 @@
 package day14_Recap;
+/*
+ 1. write a program for the shipping info that, the program should ask:
+                    building number (
+                    Street address (Assume it has more than one word)
+                    city name
+                    state name
+                    zip code
+                    full name of the person:
 
+            and prints the ship to info in the following format:
+
+            ex output:
+                    Ship To:  Jimmy joe
+                              7925 Jones Branch Dr
+                              MCLean, VA 22102
+ */
 public class Address {
+
+
+
 }

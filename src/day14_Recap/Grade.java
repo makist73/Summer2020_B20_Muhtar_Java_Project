d5adac9ebb89bf02c8ff07381f49558fad5538a8
cybertekschool <github@cybertekschool.com>
2020-06-24 21:43:21 -0400

    Day14 Class

---
+++ src/day14_Recap/Grade.java
@@ -0,0 +1,44 @@
+package day14_Recap;
+import java.util.Scanner;
+/*
+                    90 <= score <= 100 ==> Excellent
+                    80 <= score < 90 ==> Great
+                    70 <= score < 80 ==> Good
+                    60 <= score < 70 ==> Passed
+                    0 <= score <= 59 ==> Failed
+                    other == > Invalid Entry
+ */
+
+public class Grade {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double score = scan.nextDouble();
+
+        String result = "";
+
+        if(score >= 0 && score<= 100){ // 5 possible outcomes
+
+            if(score>= 90 ){
+                result = "Excellent";
+            }else if(score >= 80 ){
+                result = "Great";
+            }else if(score >= 70 ){
+                result = "Good";
+            }else if(score >= 60){
+                result = "Passed";
+            }else{
+                result = "Failed";
+            }
+
+        }else{ // 1 possible outcomes
+            result = "Invalid Entry";
+        }
+
+        System.out.println(result);
+
+
+
+    }
+
+}

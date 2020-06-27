c07fe58ab50447a3dfd90ff3bfe034ebb73b562b
cybertekschool <github@cybertekschool.com>
2020-06-27 13:02:48 -0400

    Day15 Class

--- src/day15_String/String_Methods.java
+++ src/day15_String/String_Methods.java
@@ -2,7 +2,7 @@ package day15_String;
 
 public class String_Methods {
     public static void main(String[] args) {
-        String name = "Cybertek";
+        String name = "Cybertek School is a great place";
         //             01234567
 
         // charAt(index):
@@ -10,6 +10,18 @@ public class String_Methods {
             char ch2 = name.charAt(5); //'t'
         System.out.println(ch1);
         System.out.println(ch2);
+
+        // length() ==> int
+         int l =   name.length();
+        System.out.println(l);
+
+        // last index: length of String - 1
+        int lastIndexNumber = name.length() - 1;
+
+        System.out.println( lastIndexNumber );
+
+        char lastChar = name.charAt(lastIndexNumber);
+        System.out.println(lastChar);
 
 
     }

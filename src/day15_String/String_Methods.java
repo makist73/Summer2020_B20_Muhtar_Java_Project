7fd65d11dfc29b6f01d9658ad8f914557f269554
cybertekschool <github@cybertekschool.com>
2020-06-27 12:38:59 -0400

    Day15 Class

---
+++ src/day15_String/String_Methods.java
@@ -0,0 +1,17 @@
+package day15_String;
+
+public class String_Methods {
+    public static void main(String[] args) {
+        String name = "Cybertek";
+        //             01234567
+
+        // charAt(index):
+            char ch1 = name.charAt(0); //'C'
+            char ch2 = name.charAt(5); //'t'
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+
+    }
+
+}

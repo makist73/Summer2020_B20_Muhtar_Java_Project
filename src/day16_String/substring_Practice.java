40cff3e52bd67f9791ffc9dc2cb0bc92963d1d7c
cybertekschool <github@cybertekschool.com>
2020-06-28 10:27:38 -0400

    Day16 Class

--- src/day16_String/substring_Practice.java
+++ src/day16_String/substring_Practice.java
@@ -13,6 +13,15 @@ import java.util.Scanner;
 
 public class substring_Practice {
     public static void main(String[] args) {
+        // extra task:
+            String str = "I like to drink Pepsi";
+            String drink = str.substring(16);
+            System.out.println(drink);
+
+            String action = str.substring(10, 14 + 1);
+        System.out.println(action);
+
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter your first word");

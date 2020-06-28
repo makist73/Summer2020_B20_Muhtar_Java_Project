32e313e909f57a71f8be13fe8c7ef6a165ae3e57
cybertekschool <github@cybertekschool.com>
2020-06-28 10:38:27 -0400

    Day16 Class

--- src/day16_String/FirstLastCharacters.java
+++ src/day16_String/FirstLastCharacters.java
@@ -15,6 +15,7 @@ public class FirstLastCharacters {
         Scanner scan= new Scanner(System.in);
         System.out.println("Enter a word");
         String word = scan.next();
+        scan.close();
 
         char firstChar = word.charAt(0);
         char lastChar = word.charAt( word.length()  - 1   );
@@ -22,9 +23,9 @@ public class FirstLastCharacters {
        // System.out.println( firstChar == lastChar );
 
         if(firstChar == lastChar ){
-            System.out.println("true");
+            System.out.println(true);
         }else{
-            System.out.println("false");
+            System.out.println(false);
         }
 
 

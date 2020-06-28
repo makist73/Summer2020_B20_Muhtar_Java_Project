c50e8859edd3de7558c1796d25546f8eca498303
cybertekschool <github@cybertekschool.com>
2020-06-28 15:51:26 -0400

    Day16 Class

--- src/day16_String/FizzBuzz.java
+++ src/day16_String/FizzBuzz.java
@@ -21,6 +21,8 @@ public class FizzBuzz {
 
         boolean result = word1.length() ==5  && word2.length() ==5;
 
+        /*
+
         if(result){
           char ch1 = word1.charAt( word1.length() -1 ); // last character of the first word
           char ch2 = word2.charAt(0); //first character of the second word
@@ -30,6 +32,24 @@ public class FizzBuzz {
             }else{
                 System.out.println("Buzz");
             }
+
+        }else{
+            System.out.println("need to be exactly 5 chars length");
+        }
+*/
+
+
+        if(result){
+            //                                "" +  'A' ==> "A"
+            boolean b1 = word1.endsWith( ""+word2.charAt(0) ); // word1 ends with first character of word2
+            boolean b2 = word2.startsWith( ""+ word1.charAt(word1.length()-1) );  // word2 starts with the last character of word1
+
+            if( b1 && b2 ) {
+                System.out.println("Fizz");
+            }else{
+                System.out.println("Buzz");
+            }
+
 
         }else{
             System.out.println("need to be exactly 5 chars length");

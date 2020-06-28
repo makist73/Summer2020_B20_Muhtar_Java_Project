8d8bc08d6a435f0179c93f3414afbbecc007fcc8
cybertekschool <github@cybertekschool.com>
2020-06-28 10:37:14 -0400

    Day16 Class

---
+++ src/day16_String/FirstLastCharacters.java
@@ -0,0 +1,33 @@
+package day16_String;
+/*
+    2. Ask user to enter a word. Print true is the first and last characters of the string are same characters, print false otherwise.
+            Input:
+                abba
+            Output:
+                true
+ */
+
+import java.util.Scanner;
+
+public class FirstLastCharacters {
+
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = scan.next();
+
+        char firstChar = word.charAt(0);
+        char lastChar = word.charAt( word.length()  - 1   );
+
+       // System.out.println( firstChar == lastChar );
+
+        if(firstChar == lastChar ){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+
+
+    }
+
+}

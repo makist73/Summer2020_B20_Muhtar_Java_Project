46d30963b089916abd02770cd28c0a90321c9f9b
cybertekschool <github@cybertekschool.com>
2020-06-30 18:57:42 -0400

    Office Hour: 06/30/2020

---
+++ src/Office_Hours/Practice_06_30_2020/classNote.java
@@ -0,0 +1,47 @@
+package Office_Hours.Practice_06_30_2020;
+
+public class classNote {
+
+    /*
+        06/30/2020
+    Practice Topic: If statement
+                    Ternary
+
+    create a sub Package named Practice_06_30_2020 under the package "Office_hours"
+
+
+    if(conition){  // for one condition
+        statementA;
+    }
+
+
+    if(Condition){  //for 2 condition
+        statementA;
+    }else{
+        statementB;
+    }
+
+
+
+    if(Condition){  // for more than 2 condition
+        statementA
+    }else if(Condition){
+        statementB
+    }else{
+        statementC
+    }
+
+    nested: pre-condition
+
+        pre-condition: 0 - 100 ==> A, B, C, D, F
+        otherwise ==> Invalid
+
+
+    ternary:
+            ? is if, conditions are places before
+            : else
+
+
+     */
+
+}

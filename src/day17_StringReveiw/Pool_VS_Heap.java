fc1db334463b3df7724e2c6b39a116dbbbc70fa1
cybertekschool <github@cybertekschool.com>
2020-06-30 19:32:11 -0400

    Day17 class

---
+++ src/day17_StringReveiw/Pool_VS_Heap.java
@@ -0,0 +1,28 @@
+package day17_StringReveiw;
+
+public class Pool_VS_Heap {
+
+    public static void main(String[] args) {
+        String s1 = "Cybertek";  // String pool
+        String s2 = "Cybertek";  // String pool
+
+        String s3 = new String("Cybertek"); // heap
+        String s4 = new String("Cybertek"); // heap
+
+        String s5 = "cybertek";  // String pool
+
+        System.out.println( s1 == s2); // true
+
+        System.out.println(s1 == s3); // false
+
+        System.out.println( s3 == s4); // false
+
+        System.out.println(s5 == s2) ;  // false
+
+        System.out.println(s5 == s1); // false
+
+
+
+    }
+
+}

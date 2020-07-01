3aa52aaa1a878f770a16e6707a3a25e7feb66c84
cybertekschool <github@cybertekschool.com>
2020-07-01 18:27:03 -0400

    Office Hour: 07/01/2020

---
+++ src/Office_Hours/Practice_07_01_2020/CombineTwoStrings.java
@@ -0,0 +1,43 @@
+package Office_Hours.Practice_07_01_2020;
+/*
+1. Ask user to enter two words. Then add them together and print. But if the last letter of the first
+ word and the first letter of the last letter is the same, print that character once.
+                    Input:
+                        one
+                        eight
+
+                    Output:
+                        oneight
+ */
+
+import java.util.Scanner;
+
+public class CombineTwoStrings {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str1 = scan.next();
+        String str2 =  scan.next(); // ight
+                    // 01234
+
+        // OneTwo
+        char ch1 = str1.charAt( str1.length() -1 ); // last char from str1
+        char ch2 = str2.charAt(0); //first char from str2
+
+        if( ch1 == ch2  ){
+            System.out.println(str1 + str2.substring(1));  // excluding the first character of the second string
+        }else{
+            System.out.println(str1 +str2);
+        }
+
+
+
+
+
+
+
+    }
+
+
+}

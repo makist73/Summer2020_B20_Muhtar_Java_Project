16bc3183cbd9fe7784952ecb796a84d0e831bdd5
cybertekschool <github@cybertekschool.com>
2020-07-01 19:27:27 -0400

    Day18 Class

--- src/day18_ForLoop/Initials.java
+++ src/day18_ForLoop/Initials.java
@@ -21,21 +21,22 @@ public class Initials {
         String firstName =scan.next();
         String lastName = scan.next();
 
-        /*
-       String initial = firstName.substring(0, 1)+ "." + lastName.substring(0,1); // "jp"
-               initial = initial.toUpperCase();  // "JP"
 
-        System.out.println(initial);
+       String initial1 = firstName.substring(0, 1)+ "." + lastName.substring(0,1); // "jp"
+               initial1 = initial1.toUpperCase();  // "JP"
 
-         */
+        System.out.println(initial1);
+
+
+
 
        char ch1 = firstName.charAt(0);
        char ch2 = lastName.charAt(0);
 
-       String initial ="" + ch1+ "." + ch2;
-              initial =  initial.toUpperCase();
+       String initial2 ="" + ch1+ "." + ch2;
+              initial2 =  initial2.toUpperCase();
 
-        System.out.println(initial);
+        System.out.println(initial2);
 
 
 

e79332374422773ab3260aaef75888b5410a93c8
cybertekschool <github@cybertekschool.com>
2020-06-30 22:03:26 -0400

    Day17 class

--- src/day17_StringReview/String_Methods2.java
+++ src/day17_StringReview/String_Methods2.java
@@ -2,7 +2,7 @@ package day17_StringReview;
 
 public class String_Methods2 {
      /*
-    String methods:  equals, equalsIgnorecase, isEmpty, ends, starts, contains
+    String methods:  contains,  ends, starts
      */
 
     public static void main(String[] args) {
@@ -47,6 +47,39 @@ public class String_Methods2 {
 
         str2 = ""; // length ==0
         System.out.println( str2.isEmpty() );
+
+        // contains: included or not
+
+        String str3 = "Java, C#, Python, Ruby, C++";
+
+        System.out.println(  str3.contains("Java") ); // true
+        System.out.println(str3.contains("java")); // false
+
+      //  System.out.println( str3.toLowerCase().contains("java")); // true
+
+        String str4 = "DABC";
+
+        System.out.println(str4.contains("D"));
+
+        // startsWith & endsWith:
+
+        String str5 = "Cybertek School";
+
+        System.out.println(str5.startsWith("E")  || str5.startsWith("C"));
+        //                  false     ||  true ==> true
+
+        System.out.println(str5.startsWith("CybABC")); // false
+
+        String str6 = "Today is a great day";
+
+        System.out.println( str6.endsWith("day"));  // true
+
+        System.out.println(str6.endsWith("great")); // false
+
+
+
+
+
 
 
 

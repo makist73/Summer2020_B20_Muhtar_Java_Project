4bfbfea9dea2614b4f29350e4a19abb973744317
cybertekschool <github@cybertekschool.com>
2020-07-01 22:01:50 -0400

    Day18 Class

---
+++ src/day18_ForLoop/Forloop_Practice.java
@@ -0,0 +1,34 @@
+package day18_ForLoop;
+
+public class Forloop_Practice {
+
+    public static void main(String[] args) {
+
+
+        for( int i = 1;  i <= 5; ++i ){ //i: 1, 2, 3, 4, 5, 6
+
+            System.out.println("Cybertek Batch 20"); //5
+
+        }
+
+        System.out.println("=============================================");
+
+        for(int i = 1;  i <= 100; i++ ){
+            System.out.print(i+" ");
+        }
+
+        System.out.println();
+
+        System.out.println("=================================================");
+
+        for(int i = 100; i == 1; i-- ){
+            System.out.print(i+" ");
+        }
+
+
+
+
+
+    }
+
+}

09959811199ad98010719d6a8fdc02c2acb781a7
cybertekschool <github@cybertekschool.com>
2020-07-01 21:25:29 -0400

    Day18 Class

---
+++ src/day18_ForLoop/ForLoop.java
@@ -0,0 +1,27 @@
+package day18_ForLoop;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println("====================================");
+
+        for( int i = 0; i < 10; i += 1 ){  //i: 0, 1, 2,3,4,5,6,7,8,9,10
+
+            System.out.println("Hello World");
+
+        }
+
+
+
+
+
+    }
+
+}

52b069385c767de587cdfec21c466f4eefeeb2a2
cybertekschool <github@cybertekschool.com>
2020-07-05 15:50:33 -0400

    Day19 Class

---
+++ src/day19_ForLoop/ReverseString.java
@@ -0,0 +1,22 @@
+package day19_ForLoop;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String str = "Cybertek School is a cool place";  // "rebyC"
+        //            01234
+
+    //String result = ""+str.charAt( 4 ) + str.charAt(3) + str.charAt(2) + str.charAt(1) +str.charAt(0) ;
+
+        String result = ""; //rebyC
+
+        for(int i = str.length()-1 ; i >= 0; i-- ){ //i: 4, 3, 2, 1, 0
+            result +=  str.charAt(i);
+        }
+
+
+        System.out.println(result);
+
+    }
+
+}

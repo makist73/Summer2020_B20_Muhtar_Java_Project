345018c7182902388df418c827d4261a1221cfba
cybertekschool <github@cybertekschool.com>
2020-07-05 12:41:45 -0400

    Day19 Class

---
+++ src/day19_ForLoop/LoopsWithoutCurlyBraces.java
@@ -0,0 +1,23 @@
+package day19_ForLoop;
+
+public class LoopsWithoutCurlyBraces {
+
+    public static void main(String[] args) {
+
+        for(int i = 5; i < 10; i++){
+            System.out.println("Hello");
+            System.out.println("How are you");
+        }
+
+        System.out.println("========================================================");
+
+        for(int i = 1; i <= 5; i++)
+            System.out.println("Hello");
+            System.out.println("How are you");
+
+
+
+
+    }
+
+}

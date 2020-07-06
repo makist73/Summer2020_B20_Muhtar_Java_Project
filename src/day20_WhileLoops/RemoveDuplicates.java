90724d6fa422d0b89c250824dc1ec51d12c0f9ec
cybertekschool <github@cybertekschool.com>
2020-07-06 19:58:35 -0400

    Day20 Class

---
+++ src/day20_WhileLoops/RemoveDuplicates.java
@@ -0,0 +1,50 @@
+package day20_WhileLoops;
+/*
+    3. write a program that can remove the duplicates from any given String
+            Ex:
+                input: abcabcaabb
+                output: abc
+ */
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();  //"ab"
+        //            0123
+
+        String result = ""; // "abcdefghi"
+
+        /*
+        for(int i = 0; i <= str.length()-1; i++  ){
+
+              String s =  ""+str.charAt(i); // a, a, b, b
+
+                if( result.contains(s) ){  // if the character is already contained in result, wre will not add it again
+                   continue;
+                }
+
+                result += s;
+
+        }
+    */
+
+        for(int i = 0; i <= str.length()-1; i++  ){
+
+            String s =  ""+str.charAt(i);  //a,  b,  a,  b
+
+            if( !result.contains(s) ){
+                result += s;
+            }
+
+        }
+
+        System.out.println(result);
+
+
+
+    }
+
+}

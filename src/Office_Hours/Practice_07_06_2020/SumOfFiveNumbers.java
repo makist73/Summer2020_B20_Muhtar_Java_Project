b946ed5d037ccb23f796b6b130a39b63c9f8abec
cybertekschool <github@cybertekschool.com>
2020-07-06 18:49:56 -0400

    Office Hour: 07/06/2020

--- src/Office_Hours/Practice_07_06_2020/SumOfFiveNumbers.java
+++ src/Office_Hours/Practice_07_06_2020/SumOfFiveNumbers.java
@@ -12,7 +12,7 @@ public class SumOfFiveNumbers {
         Scanner scan = new Scanner(System.in);
         int sum = 0; // 10 + 20 + 30 + 40 + 50 = 150
 
-        for(int i = 1; i <= 1000; i += 1){
+        for(int i = 1; i <= 5; i += 1){
 
             System.out.println("Enter a number: ");
             int num = scan.nextInt(); // 10 , 20, 30, 40, 50

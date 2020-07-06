1683f4086347351bcd891cb61c252f2e828209e6
cybertekschool <github@cybertekschool.com>
2020-07-06 19:31:33 -0400

    Day20 Class

--- src/day20_WhileLoops/FactorialNumber.java
+++ src/day20_WhileLoops/FactorialNumber.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class FactorialNumber {
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 

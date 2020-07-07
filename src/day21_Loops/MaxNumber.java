9b5d59f923d41b10a4ce9af96329bbe217d1d431
cybertekschool <github@cybertekschool.com>
2020-07-07 19:38:06 -0400

    Day21 Class

---
+++ src/day21_Loops/MaxNumber.java
@@ -0,0 +1,30 @@
+package day21_Loops;
+
+import java.util.Scanner;
+
+// 1. write a program that asks user to enter 5 numbers and returns the maximum number
+public class MaxNumber {
+
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+
+         int max  = -9999999;  // -100
+
+        for(int i = 10; i <= 50; i += 10) {
+
+            System.out.println("Enter a number");
+            int num = scan.nextInt(); //
+
+            if(num > max ){
+                max = num;
+            }
+
+        }
+
+         System.out.println("Max number: " +max);
+
+
+
+     }
+
+}

cc2911c4eedd1be70c77e1c5b73cf7ca819ba71a
cybertekschool <github@cybertekschool.com>
2020-07-07 19:51:48 -0400

    Day21 Class

---
+++ src/day21_Loops/MinNumber.java
@@ -0,0 +1,30 @@
+package day21_Loops;
+
+import java.util.Scanner;
+// 2. write a program that asks user to enter 5 numbers and returns the minimum number
+public class MinNumber {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int min = 999999999; //3
+
+        for(int i =1; i <= 10; i++){
+
+            System.out.println("Enter a number");
+            int num = scan.nextInt(); // 50, 10, 5, 20, 3
+
+            if(num < min){
+                min = num;
+            }
+
+        }
+
+        System.out.println("Minimum number: "+min);
+
+
+
+    }
+
+
+
+}

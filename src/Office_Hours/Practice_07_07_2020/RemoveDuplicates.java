aa99a157d455e53a46190e22b63483634d3df8e9
cybertekschool <github@cybertekschool.com>
2020-07-07 18:58:55 -0400

    Office hour: 07/07/2020

---
+++ src/Office_Hours/Practice_07_07_2020/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package Office_Hours.Practice_07_07_2020;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            String str = scan.nextLine();   // "nal"
+
+            String result = ""; //"nal"
+
+            for(int i = 0;  i <= str.length()-1; i++ ){
+
+                   String s =""+ str.charAt(i);  //s: n, a, l, a, n
+
+                if(result.contains(s)){
+                    continue;
+                }else{
+                    result += s;
+                }
+
+            }
+
+        System.out.println(result);
+
+
+    }
+
+}
--- src/Office_Hours/Practice_07_07_2020/StringReverse.java
+++ src/Office_Hours/Practice_07_07_2020/StringReverse.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class StringReverse {
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
         //            0123456

42d3bcf669439ad67f75276954775ef2b13b86df
cybertekschool <github@cybertekschool.com>
2020-07-06 20:31:16 -0400

    Day20 Class

---
+++ src/day20_WhileLoops/WhileLoop.java
@@ -0,0 +1,27 @@
+package day20_WhileLoops;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        if(true){
+            System.out.println("Hello World");
+        }
+
+        System.out.println("=====================================");
+
+        while(true){
+            System.out.println("Hello World");
+        }
+
+        /*
+        for(int i= 0; i ==0;){
+
+        }
+         */
+
+
+
+    }
+
+}

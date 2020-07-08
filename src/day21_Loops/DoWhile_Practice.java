501fa4568418861a356b91124a00bb735361641b
cybertekschool <github@cybertekschool.com>
2020-07-07 21:23:58 -0400

    Day21 Class

--- src/day21_Loops/DoWhile_Practice.java
+++ src/day21_Loops/DoWhile_Practice.java
@@ -1,4 +1,33 @@
 package day21_Loops;
 
 public class DoWhile_Practice {
+
+    public static void main(String[] args) {
+
+        int num = 1;
+
+        do{
+            System.out.print(num +" ");
+
+            num++;
+        }while(num <= 20);
+
+        System.out.println();
+
+        System.out.println(num);
+
+        System.out.println("===========================================");
+
+        char ch = 'Z';
+        do{
+            System.out.print(ch+" ");
+           ch--;
+        }while(ch >= 'A');
+
+
+
+
+
+    }
+
 }

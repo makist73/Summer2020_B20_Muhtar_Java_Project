c9413b818eca776397b83c141fd0acfe6ff80f08
cybertekschool <github@cybertekschool.com>
2020-07-07 20:02:28 -0400

    Day21 Class

---
+++ src/day21_Loops/MinAndMax.java
@@ -0,0 +1,35 @@
+package day21_Loops;
+
+import java.util.Scanner;
+
+public class MinAndMax {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int max = -999999999; // any user entered input WILL BE greater than  -999999999
+        int min = 999999999; // any user entered input WILL BE less than  999999999
+
+        for(int i=1; i<= 5; i++){
+
+            System.out.println("Enter a number");
+            int num = scan.nextInt();
+
+            if(num > max){
+                max =num;
+            }
+
+            if(num < min){
+                min =num;
+            }
+
+        }
+
+        System.out.println("Maximum number is: "+max);
+        System.out.println("minimum number is: "+min);
+
+
+
+    }
+
+}

4071422c2a8c76024d9ddbd74e4cd83a81934871
cybertekschool <github@cybertekschool.com>
2020-07-08 19:52:30 -0400

    Day22 class

--- src/day22_NestedLoop/OccurancesOfChar.java
+++ src/day22_NestedLoop/OccurancesOfChar.java
@@ -20,15 +20,29 @@ public class OccurancesOfChar {
 
         int count = 0 ; // 1+1 = 2, frequency of ch in str
 
+
         for(int i=0; i <= str.length()-1; i++){
-                                //ababc
+                        //ababc
            char each = str.charAt(i);  // a  b a b c
 
             if( each == ch ){  // if true, means ch is occured in str
                 count += 1;
             }
-
         }
+
+    /*
+            int index = 0;
+            while(index <= str.length()-1  ){
+
+                char each = str.charAt(index);  // a  b a b c
+
+                if( each == ch ){  // if true, means ch is occured in str
+                    count += 1;
+                }
+
+              index++;
+            }
+    */
 
         System.out.println(count);
 

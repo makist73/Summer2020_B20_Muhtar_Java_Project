a75d91f3be283683ba6c97a13d9eb79bbe532448
cybertekschool <github@cybertekschool.com>
2020-07-07 20:49:48 -0400

    Day21 Class

---
+++ src/day21_Loops/DoWhile.java
@@ -0,0 +1,22 @@
+package day21_Loops;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        boolean result = false;
+
+        while(result){
+            System.out.println("Hello World");
+        }
+
+        System.out.println("===============================");
+
+        do{ // just do it
+            System.out.println("Hello World");
+        }while(result);
+
+
+
+    }
+}

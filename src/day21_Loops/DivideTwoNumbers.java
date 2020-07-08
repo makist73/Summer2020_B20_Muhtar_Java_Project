b6d9243c113a0c095225e2973e2844953971fc2a
cybertekschool <github@cybertekschool.com>
2020-07-07 20:41:00 -0400

    Day21 Class

--- src/day21_Loops/DivideTwoNumbers.java
+++ src/day21_Loops/DivideTwoNumbers.java
@@ -24,12 +24,13 @@ public class DivideTwoNumbers {
 
     public static void main(String[] args) {
         int a = 20; //2
-        int b = 0;
+        int b = 3;
 
         if(b == 0){
             System.out.println("Invalid Input");
             System.exit(0);
         }
+
 
         int count = 0; //3
 
@@ -38,6 +39,7 @@ public class DivideTwoNumbers {
             count++;
         }
 
+        System.out.println(count+" with a remainder of "+a);
 
     }
 

0331f2654ea3e66001ddd5f979f666c081b7fa56
cybertekschool <github@cybertekschool.com>
2020-07-08 18:51:27 -0400

    Office Hours: 07/08/2020

---
+++ src/Office_Hours/Practice_07_08_2020/MinimumAndMaximum.java
@@ -0,0 +1,36 @@
+package Office_Hours.Practice_07_08_2020;
+
+import java.util.Scanner;
+
+public class MinimumAndMaximum {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int min = 2147483647;
+        int max =  -2147483648;
+
+
+        for(int i = 0; i < 100 ; i++ ){
+
+            System.out.println("Enter a number");
+            int n = scan.nextInt(); // 5  4  6  3  1
+
+            if( n < min ){
+                min = n ;
+            }
+
+            if(n > max){
+                max=n;
+            }
+
+        }
+
+        System.out.println("min: "+min);
+        System.out.println("max: "+max);
+
+
+    }
+
+}

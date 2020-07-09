dc46d2711faad57fad000d218817036db91e04db
cybertekschool <github@cybertekschool.com>
2020-07-08 20:42:56 -0400

    Day22 class

---
+++ src/day22_NestedLoop/Nested_Loop.java
@@ -0,0 +1,38 @@
+package day22_NestedLoop;
+
+public class Nested_Loop {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for(int i = 1; i <= 5; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for(int i = 1; i <= 5; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for(int i = 1; i <= 5; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for(int i = 1; i <= 5; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+
+
+
+
+    }
+
+}

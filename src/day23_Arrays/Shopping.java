3a49b2a112730b442addaecc824b1b6a21f2448c
cybertekschool <github@cybertekschool.com>
2020-07-11 15:50:24 -0400

    Day23 class

---
+++ src/day23_Arrays/Shopping.java
@@ -0,0 +1,30 @@
+package day23_Arrays;
+
+public class Shopping {
+
+    public static void main(String[] args) {
+
+        String[] shoppingList = new String[7];
+
+        shoppingList[0] = "Toilet Paper";
+        shoppingList[1] = "Hand Sanitizer";
+        shoppingList[4] = "Eggs";
+        shoppingList[6] = "Corona Beer";
+        shoppingList[2] = "Bread";
+        shoppingList[3] = "Milk";
+        shoppingList[5] = "Water";
+
+        for(int i = 0; i <= shoppingList.length-1; i++){
+            System.out.println(shoppingList[i] );
+        }
+
+
+
+
+
+
+
+
+    }
+
+}

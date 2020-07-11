2d3597dc1b6a02321b4f872c67e65d97beff1cd2
cybertekschool <github@cybertekschool.com>
2020-07-11 12:03:08 -0400

    Day23 class

---
+++ src/day23_Arrays/FrequencyOfChars.java
@@ -0,0 +1,43 @@
+package day23_Arrays;
+
+public class FrequencyOfChars {
+
+    public static void main(String[] args) {
+        String str = "ABAB";  //"A2B2"
+        String nonDup = "";  //"AB"
+        String result = "";  //"A2B2"
+
+
+        for(int i = 0; i <= str.length()-1; i++){
+             String ch = ""+ str.charAt(i);  // A, B, A, B
+            if(!nonDup.contains( ch )){
+                nonDup += ch;
+            }
+        }
+
+        System.out.println(nonDup);
+
+        // str ="ABAB"     nonDup = "AB"
+
+        for(int j=0; j <= nonDup.length()-1; j++ ){
+
+            char ch = nonDup.charAt(j); // A
+            int count = 0; //2
+            for(int i = 0; i <= str.length()-1; i++){
+                if(str.charAt(i) == ch){
+                    count+=1;
+                }
+            }
+
+            result += ""+ch + count;
+
+        }
+
+
+
+        System.out.println(result);
+
+
+    }
+
+}

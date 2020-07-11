b0d5e1a69bfdb236f7e5f160e8cb8a5298d647d7
cybertekschool <github@cybertekschool.com>
2020-07-11 15:56:10 -0400

    Day23 class

---
+++ src/day23_Arrays/Months.java
@@ -0,0 +1,17 @@
+package day23_Arrays;
+
+import java.util.Scanner;
+
+public class Months {
+
+    public static void main(String[] args) {
+        String months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number between 1 ~ 12");
+        int n = scan.nextInt(); // 5
+
+        System.out.println( months[n-1] );
+
+    }
+
+}

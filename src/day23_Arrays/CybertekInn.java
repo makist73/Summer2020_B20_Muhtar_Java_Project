72a7c1047bf79c698b8fac7b5bb4eff1b8cf664e
cybertekschool <github@cybertekschool.com>
2020-07-11 11:36:48 -0400

    Day23 class

--- src/day23_Arrays/CybertekInn.java
+++ src/day23_Arrays/CybertekInn.java
@@ -20,10 +20,51 @@ import java.util.Scanner;
 public class CybertekInn {
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        int total = 0;
         System.out.println("Welcome to Cybertek Inn");
 
+    while(true) {
 
+        System.out.println("Please choose from the following: ");
+        System.out.println("1. King Bed: $120");
+        System.out.println("2. Queen Bed: $100");
+        System.out.println("3. Single Bed: $80");
+        int option = scan.nextInt();
+
+        switch (option) {
+            case 1:
+                total += 120;
+                break;
+            case 2:
+                total += 100;
+                break;
+            case 3:
+                total += 80;
+                break;
+
+            default:
+                System.out.println("Get Out");
+                System.exit(0);
+        }
+
+        System.out.println("do you want to reserve another room?");
+        String a = scan.next().toLowerCase();
+
+        while(  !(a.equals("yes") || a.equals("no"))  ){ // while the answer is invalid
+            System.out.println("Invalid, Please re-enter");
+            System.out.println("do you want to reserve another room?");
+            a=scan.next().toLowerCase();
+        }
+
+        if(a.equals("no")){
+            break;
+        }
+
+    }
+
+        System.out.println("Your total price is: $"+total);
 
 
     }

fe12de5da329778eb4513a5d3139ffd234c344dc
cybertekschool <github@cybertekschool.com>
2020-07-11 11:00:01 -0400

    Day23 class

---
+++ src/day23_Arrays/CybertekInn.java
@@ -0,0 +1,31 @@
+package day23_Arrays;
+/*
+ 2. write a program for the room reservation:
+            King Bed ==> 120$
+            Queen Bed ==> 100$
+            single Bed ==> 80$
+
+            the program asks the user which bedroom does he/she wants to reserve and calculats the total price, and then the program will ask:
+                do you want to reserve another room:
+                            if yes ==> repeat the previous steps
+                            if no ==> your total price is: $$$
+                            while answer is neither yes or no ==> please re-enter
+
+            HINT: nested loop task, outter loop need to be an infinite loop
+
+ */
+
+import java.util.Scanner;
+
+public class CybertekInn {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Cybertek Inn");
+
+
+
+
+    }
+
+}

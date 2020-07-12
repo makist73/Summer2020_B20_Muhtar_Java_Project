df0793d09b0d9cea8b3fe96e7e9455faee3af055
cybertekschool <github@cybertekschool.com>
2020-07-12 10:54:49 -0400

    Day24 class

---
+++ src/day24_Arrays/Uniques2.java
@@ -0,0 +1,34 @@
+package day24_Arrays;
+
+public class Uniques2 {
+
+    public static void main(String[] args) {
+
+        String str = "aabccdeeeefffffffjhhhhhhhi";
+            // "aabcc"
+
+        String expectedResult = ""; // "b
+
+        for(int j = 0; j <= str.length()-1; j++){ // because we need the frequency of evry single character
+
+            char ch1 = str.charAt(j); // a a b c c
+            int count1 = 0; // 1+1  // frequnecy of ch
+            for(int i = 0; i <= str.length()-1; i++){  // used for finding the frequnecy of ch and assign it to count
+                char each = str.charAt(i);  // a  a  b  c  c
+                if(ch1 == each){
+                    count1+=1;
+                }
+            }
+
+            if(count1 == 1 ){ // if it only occured one time
+                expectedResult += ch1;
+            }
+
+        }
+
+        System.out.println(expectedResult);
+
+
+    }
+
+}

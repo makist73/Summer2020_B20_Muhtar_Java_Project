b27bacb1fe28311ad082b77514b3a865a2cec498
cybertekschool <github@cybertekschool.com>
2020-07-12 14:48:29 -0400

    Day24 class

--- src/day24_Arrays/String_Methods.java
+++ src/day24_Arrays/String_Methods.java
@@ -22,6 +22,32 @@ public class String_Methods {
         boolean r1 = Arrays.equals(ch1, ch2);
         System.out.println(r1);
 
+        System.out.println("==============================");
+        String sentence = "I Love Programming Language";
+
+        // Language Programming Love I
+
+       String[] words =  sentence.split(" ");  // {I, Love, Programming, Language}
+        System.out.println( Arrays.toString(words)  );
+
+        for(int i = words.length-1; i >= 0; i-- ){
+            System.out.print(words[i]+" ");
+        }
+        System.out.println();
+
+
+        System.out.println("=====================================");
+
+        String s = "JAVA";
+
+        String[] arr =  s.split("");  // every single single elements are string
+        System.out.println(Arrays.toString(arr));
+
+        char[] c = s.toCharArray(); // every single single elements are char
+        System.out.println(Arrays.toString(c));
+
+
+
 
 
 

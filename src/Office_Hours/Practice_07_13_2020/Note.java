972e42ea38f07387f45b3977eb45d5eaff4d6721
cybertekschool <github@cybertekschool.com>
2020-07-13 19:00:29 -0400

    Day24 class

---
+++ src/Office_Hours/Practice_07_13_2020/Note.java
@@ -0,0 +1,31 @@
+package Office_Hours.Practice_07_13_2020;
+
+public class Note {
+
+    /*
+    07/13/2020
+Practice Topic: String & loops
+
+package name: Practice_07_13_2020
+
+Task:
+	Frequency of characters
+
+		"JAVA";
+		J1A2V1
+
+		"DDEEEFFFF";
+		D2E3F4
+
+		steps:
+			remove duplicates "DEF"
+			Frequnecy:        2  3  4
+			combine:   D2E3F4
+
+
+tomorrow:
+	Unique
+
+
+     */
+}

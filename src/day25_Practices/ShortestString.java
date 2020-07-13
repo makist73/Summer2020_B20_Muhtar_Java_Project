097ae1ba6f065735319d4d3a5bdf7c58fc7ef0e3
cybertekschool <github@cybertekschool.com>
2020-07-13 19:40:13 -0400

    Day25 Class

--- src/day25_Practices/ShortestString.java
+++ src/day25_Practices/ShortestString.java
@@ -24,12 +24,5 @@ public class ShortestString {
         }
 
 
-
-
-
-
-
-
-
     }
 }
--- src/day25_Practices/ShortestString2.java
+++ src/day25_Practices/ShortestString2.java
@@ -2,4 +2,27 @@ package day25_Practices;
 
 public class ShortestString2 {
 
+    public static void main(String[] args) {
+
+        String[] arr = {"Anam", "Nickolas", "Amir", "Nurmamet"};
+
+        int minLength = arr[0].length(); //3
+
+        for(int i = 0; i <= arr.length-1; i++){  // to find out the minimum lenght of the string in arr
+            int l = arr[i].length();  // 4  8   4  8
+
+            if(l < minLength){
+                minLength = l;
+            }
+        }
+
+
+        for(int i = 0; i <= arr.length-1; i++){  // to see how many strings' lengths in the array is matching with minLength
+            if(arr[i].length() == minLength ){
+                System.out.println(arr[i]);
+            }
+        }
+
+
+    }
 }

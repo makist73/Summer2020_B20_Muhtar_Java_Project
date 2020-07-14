5fa28f11a4d666332e8a374a2931b7fa010fb389
cybertekschool <github@cybertekschool.com>
2020-07-14 18:58:22 -0400

    Office Hour: 07/17/2020

---
+++ src/Office_Hours/Practice_07_14_2020/Notes.java
@@ -0,0 +1,21 @@
+package Office_Hours.Practice_07_14_2020;
+
+public class Notes {
+    /*
+    07/14/2020
+Practice Topic: String & loop
+
+package name: Practice_07_14_2020
+
+Tasks:
+	finding the uniques
+
+	unique: frequency is 1
+
+
+
+
+next office hour:
+	finding the frequency of the word from a string
+     */
+}

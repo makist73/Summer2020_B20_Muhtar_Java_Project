51bb469795af6e5e4e79c787a3eaa958fac5b245
cybertekschool <github@cybertekschool.com>
2020-07-14 18:25:27 -0400

    Office Hour: 07/17/2020

---
+++ src/Office_Hours/Practice_07_14_2020/Unique.java
@@ -0,0 +1,39 @@
+package Office_Hours.Practice_07_14_2020;
+
+public class Unique {
+
+    public static void main(String[] args) {
+
+        String str = "ABABCDDEFFGHHIJJK";
+         String expectedResult = "";  // "C"
+
+        // str = "ABABC";
+
+        for(int j = 0; j < str.length() ; j++){
+
+            char ch = str.charAt(j); // A B A B C
+            int count = 0 ; // frequency of ch
+
+            for(int i = 0; i < str.length(); i++){  //helps us to calculate the frequency of ch and assign it to count
+                char each =  str.charAt(i);
+                if(each == ch){
+                    count+=1;
+                }
+            }
+
+            if(count == 1){
+                expectedResult += ch;
+            }
+
+        }
+
+
+
+        System.out.println(expectedResult);
+
+
+
+    }
+
+
+}

cc308d69b10b9cb441b2f3f2516fa6f4f0ce8a18
cybertekschool <github@cybertekschool.com>
2020-07-13 21:23:44 -0400

    Day25 Class

--- src/day25_Practices/FirstThreeChars.java
+++ src/day25_Practices/FirstThreeChars.java
@@ -18,13 +18,14 @@ public class FirstThreeChars {
         */
 
         for(int i =0; i <= words.length-1; i++){ // i: 0, 1, 2, 3, 4
-            System.out.println( words[i].substring(0, 3) );
+            System.out.print( words[i].substring(0, 3) );
         }
 
+        System.out.println();
         System.out.println("===================================================");
 
         for(String each: words){
-            System.out.println(each.substring(0,3));
+            System.out.print(each.substring(0,3));
         }
 
 

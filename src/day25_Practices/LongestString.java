ede6806635e1063342e8ae5d424d6cb0efa2671d
cybertekschool <github@cybertekschool.com>
2020-07-13 20:03:40 -0400

    Day25 Class

--- src/day25_Practices/LongestString.java
+++ src/day25_Practices/LongestString.java
@@ -4,17 +4,25 @@ public class LongestString {
 
     public static void main(String[] args) {
 
-        String[] arr = {"Anam", "Nickolas", "Amir", "Nurmamet"};
+        String[] arr = {"Anam", "Nickolas", "Amir", "Nurmamet", "Abuduljabar", "mamattursun"
+                , "Gebremariam", "VasilicaPutulica"};
 
-        int maxLength = arr[0].length();  //4
+        int maxLength = arr[0].length();
 
-        for( String each : arr){
+        for( String each : arr){ // to find max length
             if( each.length() > maxLength ){
                 maxLength = each.length();
             }
         }
 
-        System.out.println(maxLength);
+
+        for(String each: arr){
+            if(each.length() == maxLength){
+                System.out.println(each);
+            }
+        }
+
+
 
 
 

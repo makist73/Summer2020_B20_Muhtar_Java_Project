da968d190cd97cabde40d99c0f20e7f6a8459081
cybertekschool <github@cybertekschool.com>
2020-07-13 20:54:59 -0400

    Day25 Class

--- src/day25_Practices/CountOddEven.java
+++ src/day25_Practices/CountOddEven.java
@@ -3,7 +3,7 @@ package day25_Practices;
 public class CountOddEven {
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7,8,9,10,11};
+        int[] nums = {1,2,3,4,5,6,7,8,9,10,11,100, -100, -200, 400, 50000, 10000000, 123};
         int countEven = 0;
         int countOdd = 0;
 
@@ -20,7 +20,7 @@ public class CountOddEven {
                odd += each+" ";;
            }
 
-/*
+        /*
            if(each %2 ==0){
                countEven += 1;
                even += each+" ";
@@ -29,8 +29,10 @@ public class CountOddEven {
            countOdd++;
            odd += each+" ";
 
- */
+        */
+
         }
+
 
         System.out.println("Even Numbers: "+even);
         System.out.println(countEven);

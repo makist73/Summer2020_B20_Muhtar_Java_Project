e121ba83e19810e596dedf7a1aa5939ba053307b
cybertekschool <github@cybertekschool.com>
2020-07-15 18:54:01 -0400

    Office hour: 07/15/2020

---
+++ src/Office_Hours/Practice_07_15_2020/ClassNote.java
@@ -0,0 +1,31 @@
+package Office_Hours.Practice_07_15_2020;
+
+public class ClassNote {
+
+    /*
+    07/15/2020
+Practice Topic: String & loops
+
+package name: Practice_07_15_2020
+
+Task:
+     write a program that can find the frequency of a word from a string(not from a sentence)
+     Ex:
+        input:
+            Cat Cat Cat
+            Cat
+
+
+        output:
+            2
+
+        input:
+            JavaJavaJava
+            Java
+
+        output:
+            3
+
+     */
+
+}

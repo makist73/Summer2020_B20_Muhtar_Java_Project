a43c37104eb762e108d6c0a845bb342ec4de07d5
cybertekschool <github@cybertekschool.com>
2020-07-15 21:44:35 -0400

    Day27 Class

--- src/day27_recap/ArraysUtility.java
+++ src/day27_recap/ArraysUtility.java
@@ -1,5 +1,7 @@
 package day27_recap;
 
+import java.util.Arrays;
+
 public class ArraysUtility {
     /*
     sort():
@@ -8,6 +10,43 @@ public class ArraysUtility {
      */
 
     public static void main(String[] args) {
+
+        String[] names = {"Odina", "Liliia", "Berk", "Emine"};
+
+        Arrays.sort(names);
+
+        System.out.println(Arrays.toString(names));
+
+        int[] score = {80,75, 65, 110, 90, 45, 56, 78};
+
+        Arrays.sort(score);
+        System.out.println(Arrays.toString(score));
+
+        System.out.println("Maximum: " + score[ score.length-1 ]);
+        System.out.println("Minimum: "+score[0]);
+
+        System.out.println("=======================================");
+
+        String[] s1 = {"A", "B", "C"};
+        String[] s2 = {"A", "B", "C"};
+
+        System.out.println(    Arrays.equals(s1, s2)       );
+
+        String[] s3 = {"C", "A", "B"};
+        System.out.println(  Arrays.equals(s1, s3) ); // false
+
+
+
+        String[] a1 = {"A", "C", "B"};
+        String[] a2 = {"B", "A", "C"};
+
+        Arrays.sort(a1); // a1: {A, B, C};
+        Arrays.sort(a2); // a2: {A, B, C};
+
+        System.out.println(  Arrays.equals(a1, a2) );
+
+       String w1 =  Arrays.toString(a1);
+        System.out.println(w1);
 
 
     }

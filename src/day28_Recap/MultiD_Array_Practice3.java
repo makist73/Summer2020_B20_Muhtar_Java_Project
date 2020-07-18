f5a9f2cfa26035ca7ab9fad746b6952b4ed1abd0
cybertekschool <github@cybertekschool.com>
2020-07-18 16:53:52 -0400

    Day28 Class

--- src/day28_Recap/MultiD_Array_Practice3.java
+++ src/day28_Recap/MultiD_Array_Practice3.java
@@ -10,7 +10,6 @@ public class MultiD_Array_Practice3 {
         //                      0      1         0         1
         int[][][] arr3D = { {{1,2,3},{4,5}} , {{6,7,8}, {9,10}}   };
         //                          0               1
-
         System.out.println(Arrays.deepToString(arr3D));
 
         // {{6,7,8}, {9,10}}
@@ -23,7 +22,6 @@ public class MultiD_Array_Practice3 {
         System.out.println( arr3D[1][0][2] );
 
         System.out.println("==========================================");
-
         for(int i =0; i <=arr3D.length-1; i++){ //i: index of 2D arrays
             int[][] each2D =  arr3D[i];
             for(int j = 0; j <= each2D.length-1; j++){ //j: index of 1D array
@@ -35,7 +33,16 @@ public class MultiD_Array_Practice3 {
             }
             System.out.println();
         }
+        System.out.println("============================================");
 
+        for(int[][] each2D : arr3D ){
+            for(int[] each1D : each2D ){
+                for(int eachElement : each1D){
+                    System.out.print(eachElement+" ");
+                }
+            }
+            System.out.println();
+        }
 
 
 

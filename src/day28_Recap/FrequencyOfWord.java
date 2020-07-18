af941eb4386790a0bc799ed403b1afbfd4e1e5e8
cybertekschool <github@cybertekschool.com>
2020-07-18 10:35:28 -0400

    Day28 Class

---
+++ src/day28_Recap/FrequencyOfWord.java
@@ -0,0 +1,36 @@
+package day28_Recap;
+//  1. write a program return the frequency of a word from a string
+
+public class FrequencyOfWord {
+
+    public static void main(String[] args) {
+        String str = "javajava";
+        //            01234567
+
+        //substring(0, 4) ==> java
+       // substring(1, 5) ==> avaj
+       // substring(2, 6) ==> vaja
+        // substring(3, 7) ==> ajav
+        // substring(4, 8) ==> java
+
+        //substring(i, i+4)
+
+        //"java"
+
+        int count = 0;
+        for(int i= 0 ; i <= str.length()-4; i++){ //i:0 , 1, 2,3, 4
+
+            String s = str.substring(i, i+4);
+            if(s.equals("java")){
+                count++;
+            }
+
+        }
+
+        System.out.println(count);
+
+
+
+    }
+
+}

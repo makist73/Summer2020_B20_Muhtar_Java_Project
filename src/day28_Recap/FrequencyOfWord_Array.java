f5e2d3303dbf38be2d4dc9dc2038df793b18414f
cybertekschool <github@cybertekschool.com>
2020-07-18 11:01:37 -0400

    Day28 Class

---
+++ src/day28_Recap/FrequencyOfWord_Array.java
@@ -0,0 +1,23 @@
+package day28_Recap;
+/*
+ 2. write a program that can count the frequency of a word from an array of string
+            Ex:
+                arr: {"Java", "C#", "Python", "Java"};
+                word: "java"
+
+                output:
+                        2
+            MUST USE for each loop
+ */
+
+public class FrequencyOfWord_Array {
+
+    public static void main(String[] args) {
+
+        String[] arr = {"Java", "C#", "Python", "Java"};
+        String word = "java";
+
+
+    }
+
+}

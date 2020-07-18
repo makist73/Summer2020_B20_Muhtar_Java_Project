f2bdd1220aaa9833d00c128e53808df5b12ce9fc
cybertekschool <github@cybertekschool.com>
2020-07-18 15:03:50 -0400

    Day28 Class

--- src/day28_Recap/MultiD_Array.java
+++ src/day28_Recap/MultiD_Array.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class MultiD_Array {
 
-    public static void main(String[] args) {
+    public static void main(String kfc[]) {
         int[] arr1D = {1,2,3};
 
      //                    0 1 2      0 1 2 3 4      0 1  2  3  4
-        int[][] arr2D = { {1,2,3} ,  {4,5,6,7,8} ,  {9,10,11,12,13}    };
+        int arr2D[][] = { {1,2,3} ,  {4,5,6,7,8} ,  {9,10,11,12,13}    };
         //                  0             1                2
         //  {4,5,6,7,8}
         System.out.println( Arrays.toString(arr2D[1] )  ) ;      // single dimensional int array

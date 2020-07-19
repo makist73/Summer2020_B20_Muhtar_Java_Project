802fcee1390eceb4eaf4e7d9e04dce4cd3c088a7
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-19 11:07:30 -0400

    Day29 Class

--- src/day29_CustomMethods/MethodsWithoutParameter2.java
+++ src/day29_CustomMethods/MethodsWithoutParameter2.java
@@ -1,5 +1,4 @@
 package day29_CustomMethods;
-
 /*
 step1: print hello world 5 times
 step2: print your school name
@@ -8,26 +7,31 @@ step4: print your name
 step5: print hello world 5 times
  */
 
-public class methodWithoutParameter {
+public class MethodsWithoutParameter2 {
+
+    /*
+    Access-Modifier  Specifiers  return-type   name(){
+            statements
+		}
+     */
 
     public static void main(String[] args) {
 
-        for(int i = 10; i >= 6; i-- ){
-            System.out.println("Hello World");
-        }
-
+        printHello5X();
         System.out.println("Cybertek");
-
-        for(int i = 10; i >= 6; i-- ){
-            System.out.println("Hello world");
-        }
-
+        printHello5X();
         System.out.println("Muhtar");
+        printHello5X();
 
+    }
+
+
+
+
+    public static void  printHello5X(){
         for(int i = 10; i >= 6; i-- ){
             System.out.println("Hello World");
         }
-
     }
 
 

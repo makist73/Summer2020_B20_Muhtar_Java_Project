9be8e5bd0ed20d8e63dca73d50abde1fef7ef40b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-19 15:50:13 -0400

    Day29 Class

---
+++ src/day0/MethodCall.java
@@ -0,0 +1,17 @@
+package day0;
+
+import Library.CustomMethods;
+
+public class MethodCall {
+
+    public static void main(String[] args) {
+
+        String str = "Cybertek School";
+        CustomMethods.reverse(str);
+
+        String str2 = "abcabcaaaaabbbbbbcccccddddeeeeeee";
+        CustomMethods.removeDup(str2);
+
+    }
+
+}

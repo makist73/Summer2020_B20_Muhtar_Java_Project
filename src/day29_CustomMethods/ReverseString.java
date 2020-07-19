c50b052b8aeac21dc72dd0d63598f08f2566dc3f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-19 14:47:19 -0400

    Day29 Class

--- src/day29_CustomMethods/ReverseString.java
+++ src/day29_CustomMethods/ReverseString.java
@@ -3,7 +3,21 @@ package day29_CustomMethods;
 public class ReverseString {
 
 
-    public static void
+    public static void main(String[] args) {
+
+        reverse("Muhtar");
+        reverse("Cybertek");
+
+    }
+
+
+    public static void reverse(String str){
+        String result = "";
+        for(int i = str.length()-1;  i >=0 ; i--){
+            result += str.charAt(i);
+        }
+        System.out.println(result);
+    }
 
 
 }

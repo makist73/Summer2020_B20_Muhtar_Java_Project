2e377a74269b81417b0fa210d3c7671b38b881a6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-19 14:55:44 -0400

    Day29 Class

---
+++ src/day29_CustomMethods/UniqueElements.java
@@ -0,0 +1,35 @@
+package day29_CustomMethods;
+
+public class UniqueElements {
+
+    public static void main(String[] args) {
+        String[] arr = {"A", "B", "B", "C"};
+        uniques(arr);
+
+        System.out.println("Hello");
+
+    }
+
+
+    public static void uniques(String[] arr){
+
+        for( String a: arr){ // gets each of the element
+            int count = 0;
+            for( String each: arr ){ // gets the frequency of the element
+                if( a.equals(each) ){
+                    count++;
+                }
+            }
+            if(count == 1){ // unique
+                System.out.print(a+" ");
+            }
+        }
+
+        System.out.println();
+
+    }
+
+
+
+
+}

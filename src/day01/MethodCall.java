5f666c83502f34b46aed0ef98751328522c112a8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-19 16:11:57 -0400

    Day29 Class

---
+++ src/day01/MethodCall.java
@@ -0,0 +1,19 @@
+package day01;
+
+import Library.CustomMethods2;
+
+public class MethodCall {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {300,200,100, 10000, 300000, -5000, 40000};
+
+        CustomMethods2.maxNum(numbers);
+        CustomMethods2.minNum(numbers);
+
+
+
+    }
+
+
+}

c3478ca9101813631427a369391f16fa81a1e33e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-20 18:57:12 -0400

    Office Hour:07/20/2020

---
+++ src/Office_Hours/Practice_07_20_2020/notes.java
@@ -0,0 +1,23 @@
+package Office_Hours.Practice_07_20_2020;
+
+public class notes {
+
+    /*
+    07/20/2020
+Practice Topic: nested loop & arrays
+
+package name: Practice_07_20_2020
+
+Task:
+	write a program that can find the maximum number from any given two dimensional array
+
+		solution one: use nested for loops
+
+		solution two: use nested for each loops
+
+		solution three: use for loop and for each loop together
+
+
+		class: Max_2DArray
+     */
+}

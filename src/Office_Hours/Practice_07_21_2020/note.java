421689f032cb03385b4cc3a99cf6a5363cf801ad
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-21 18:49:44 -0400

    office hour:07/21/2020

---
+++ src/Office_Hours/Practice_07_21_2020/note.java
@@ -0,0 +1,25 @@
+package Office_Hours.Practice_07_21_2020;
+
+public class note {
+    /*
+    07/21/2020
+Practice topic:  Arrays
+
+Package name: Practice_07_21_2020
+
+Task:
+	1. Arrays: Descending order
+
+		solution 1: for loop
+
+		solution 2: for each loop ( you need extra variable for index number)
+
+
+
+
+
+tomorrow:
+	combining arrays
+
+     */
+}

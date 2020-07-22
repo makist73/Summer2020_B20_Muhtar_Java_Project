549a7aa31dc8d504226e659602cd2c4ab9641731
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-21 21:00:21 -0400

    Day31 Class

--- src/Library/Util.java
+++ src/Library/Util.java
@@ -14,7 +14,6 @@ public class Util {
         return nonDup;
     }
 
-
     public static String reverse(String str){
         String result = "";
         for(int i = str.length()-1;  i >=0 ; i--){
@@ -23,7 +22,29 @@ public class Util {
         return result;
     }
 
+    public static int frequency(String str, char ch){ // finds the frequency of char from string str
+        int count = 0;
+        for(char each  : str.toCharArray() ){
+            if(each == ch){
+                count++;
+            }
+        }
 
+        return count;
+    }
+
+    public static String uniques(String str){
+        String uniques = "";
+
+        for(char each  : str.toCharArray() ){
+            int count =frequency(str, each);  // frequency of every character
+            if(count == 1){
+                uniques += each;
+            }
+        }
+
+        return uniques;
+    }
 
 
 
@@ -41,7 +62,6 @@ public class Util {
         return max;
     }
 
-
     public static int minNum(int[] arr){
         int min= arr[0];
 
@@ -53,7 +73,6 @@ public class Util {
 
        return min;
     }
-
 
     public static int[] combine2Arrays(int[] arr1, int[] arr2){
         int[] arr3 = new int[arr1.length + arr2.length];

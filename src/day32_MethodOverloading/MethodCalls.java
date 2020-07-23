bc4f457aee3a998cdbbb16338145e45a0a21bb81
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-22 20:54:17 -0400

    Day32 Class

--- src/day32_MethodOverloading/MethodCalls.java
+++ src/day32_MethodOverloading/MethodCalls.java
@@ -1,5 +1,8 @@
 package day32_MethodOverloading;
 import Library.Util;
+
+import java.util.Arrays;
+
 public class MethodCalls {
 
     public static void main(String[] args) {
@@ -20,6 +23,16 @@ public class MethodCalls {
         String reversedName = Util.reverse(fullName);
 
         System.out.println(reversedName);
+
+        System.out.println("=================================");
+
+        int[] a= {200, 400, 500, 200, 1000, 50, 40, 30, 400, 60, 78};
+        int n = 2500;
+        a = Util.addElement(a, n);
+
+        System.out.println( Arrays.toString(a) );
+
+
 
 
     }

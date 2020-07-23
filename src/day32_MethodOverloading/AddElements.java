e7f1d7c5c48a341ca5c22a643c028dbefac4b9c7
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-22 20:48:38 -0400

    Day32 Class

---
+++ src/day32_MethodOverloading/AddElements.java
@@ -0,0 +1,52 @@
+package day32_MethodOverloading;
+
+import java.util.Arrays;
+
+public class AddElements {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4, 5,6,7,8,9};
+        int num = 1000;
+
+        // {1,2,3,4};
+
+        int[] arr2 = new int[arr.length+1];
+       arr2[arr2.length-1] = num;
+
+        int i =0;
+        for(int each: arr){
+           arr2[i]  = each;
+            i++;
+        }
+
+        System.out.println(Arrays.toString(arr2) );
+
+        System.out.println("===============================");
+
+        int[] scores = {85,65,45,55, 95};
+        int n1 = 87;
+
+            scores = addElement(scores, n1);
+
+        System.out.println( Arrays.toString(scores));
+
+
+
+    }
+
+
+    public static int[] addElement(int[] arr, int num){
+        int[] arr2 = new int[arr.length+1];
+        arr2[arr2.length-1] = num;
+
+        int i =0;
+        for(int each: arr){
+            arr2[i]  = each;
+            i++;
+        }
+
+        return arr2;
+    }
+
+
+}

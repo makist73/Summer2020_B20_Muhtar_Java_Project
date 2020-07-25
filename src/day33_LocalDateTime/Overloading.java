f15eaffcb59d2b0637e81519cafffd6e3d3f6b0a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-25 12:54:06 -0400

    Day33 class

---
+++ src/day33_LocalDateTime/Overloading.java
@@ -0,0 +1,34 @@
+package day33_LocalDateTime;
+
+public class Overloading {
+
+
+
+    public static void main(int[] kfc){
+        System.out.println("int array");
+    }
+
+
+    public static void main(double[] arr){
+        System.out.println("double array");
+    }
+
+
+    static public void main(String[] m) {
+
+        System.out.println("String array");
+        int[] arr ={1,2,3};
+
+        /*
+        println, substring, indexOf, replace, sort, toString, equals...
+         */
+
+
+
+    }
+
+
+
+
+
+}

3ae3eb8cecac288b48d422ecf5444d6a3837131e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-25 16:37:10 -0400

    Day33 class

--- src/day33_LocalDateTime/TimeFormatting.java
+++ src/day33_LocalDateTime/TimeFormatting.java
@@ -35,11 +35,21 @@ public class TimeFormatting {
 
 
 
-        // May/20/2019 Dayname 4:30 pm
+        // May/20/19 Dayname 4:30 pm
+        DateTimeFormatter DTFormat = DateTimeFormatter.ofPattern("MMM/dd/yy EEEE hh:mm a");
+        LocalDateTime time1 = LocalDateTime.of(2019, 5, 20,16,30);
+        System.out.println(time1);
+        System.out.println(time1.format(DTFormat));
+
+        System.out.println(  LocalDateTime.now().format(DTFormat)  );
 
 
+        // Saturday 4:34:45 pm
+        DateTimeFormatter dtf =  DateTimeFormatter.ofPattern("EEEE hh:mm:ss a");
+        LocalDateTime Now = LocalDateTime.now();
+        System.out.println(Now);
 
-
+        System.out.println(Now.format(dtf));
 
 
     }

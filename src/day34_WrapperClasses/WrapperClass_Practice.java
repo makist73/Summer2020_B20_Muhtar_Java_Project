bec4af38169bd194ae477cfa1b247e4a4fe75999
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-26 12:33:26 -0400

    Day34 Class

--- src/day34_WrapperClasses/WrapperClass_Practice.java
+++ src/day34_WrapperClasses/WrapperClass_Practice.java
@@ -29,5 +29,18 @@ public class WrapperClass_Practice {
 
         System.out.println(ch2);
 
+
+        String[] nums1 = {"12.5", "13.5", "14.5", "15.5"};
+
+        double[] nums2 = new double[nums1.length];
+
+        for( int i = 0; i <= nums1.length-1; i++){
+               nums2[i] = Double.parseDouble(nums1[i]) ;
+        }
+
+
+        System.out.println(Arrays.toString(nums2));
+
+
     }
 }

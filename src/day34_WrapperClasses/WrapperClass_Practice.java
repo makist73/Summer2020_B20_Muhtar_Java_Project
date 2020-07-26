6957969a88cb5c94c104ec6a8eb760d581e47e42
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-26 12:09:33 -0400

    Day34 Class

---
+++ src/day34_WrapperClasses/WrapperClass_Practice.java
@@ -0,0 +1,33 @@
+package day34_WrapperClasses;
+
+import java.util.Arrays;
+
+public class WrapperClass_Practice {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+
+        Integer[] arr2 = {1,2,3,4};
+        for(int each  : arr2){
+            System.out.println(each);
+        }
+
+
+       // double[] arr3 = {1,2,3,4,5,6,7};
+        Double[] arr4 = {1.0,2.0,3.0,4.0,5.0,6.0,7.0};
+
+       // Double y1 = 4;
+
+        for(double  each: arr4){
+            System.out.println(each);
+        }
+
+        char[] arr5 = {65, 67, 68,69};
+        System.out.println(Arrays.toString(arr5));
+
+        char ch = 79;
+        Character ch2 = 79;
+
+        System.out.println(ch2);
+
+    }
+}

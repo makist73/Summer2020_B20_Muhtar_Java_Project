8edc82d90071e6a5ca7acd9c97f479a1355506d0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-26 10:33:29 -0400

    Day33 class

---
+++ src/day34_WrapperClasses/warmup.java
@@ -0,0 +1,25 @@
+package day34_WrapperClasses;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/*
+1. use the LocalDate & Time get the current date and time in the following format:
+
+			Sunday, 10:28 AM, Jul/26/2020
+ */
+public class warmup {
+
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEEE, hh:mm a, MMM/dd/yyyy");
+
+        LocalDateTime today = LocalDateTime.now();
+
+        System.out.println(today);
+
+        System.out.println( today.format(dtf) );
+
+
+    }
+
+}

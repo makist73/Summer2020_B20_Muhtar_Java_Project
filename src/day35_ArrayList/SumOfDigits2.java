fb1d6e07bdfcc946bb2d7ef792676bed81298d40
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-27 20:54:46 -0400

    Day35 Class

---
+++ src/day35_ArrayList/SumOfDigits2.java
@@ -0,0 +1,28 @@
+package day35_ArrayList;
+
+public class SumOfDigits2{
+
+    public static void main(String[] args) {
+        String str = "a1b2c3";
+        int sum = 0;
+
+        for(char each  : str.toCharArray() ){
+            if(Character.isDigit(each)){
+                sum += Integer.parseInt(""+each);
+            }
+        }
+
+
+        System.out.println(sum);
+
+
+
+    }
+
+    /*
+Character:
+    isDigit(char): verifies if the char is digit, returns boolean
+    isLtter(char): verifies if the char is letter, returns boolean
+     */
+
+}

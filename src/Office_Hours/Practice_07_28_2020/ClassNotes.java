04f5bf0cf496e754603d7482c00f4fb64ebed6e8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-28 18:57:45 -0400

    Office Hour: 07/28/2020

---
+++ src/Office_Hours/Practice_07_28_2020/ClassNotes.java
@@ -0,0 +1,24 @@
+package Office_Hours.Practice_07_28_2020;
+
+public class ClassNotes {
+    /*
+    07/28/2020
+Practice Tipic: Methods
+
+Package name: Practice_07_28_2020
+
+practice task:
+	1. method: reverseString
+
+	2. method: palindrome Test
+
+			"ABC"
+			"CBA"
+
+			"ABC".equalsIgnoreCase("CBA")
+
+
+
+
+     */
+}

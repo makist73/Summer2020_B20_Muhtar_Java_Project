67f266182ed99de55a35778fb02cc3bc29905eeb
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-27 21:54:31 -0400

    Day35 Class

--- src/day35_ArrayList/ArrayList_Methods.java
+++ src/day35_ArrayList/ArrayList_Methods.java
@@ -23,6 +23,41 @@ public class ArrayList_Methods {
         System.out.println(earlyBirdList);
         System.out.println( earlyBirdList.size());
 
+        System.out.println("===========================================");
+
+    // remove(int index)
+        ArrayList<Integer> list = new ArrayList<>();
+            list.add(1);  //0
+            list.add(2);  //1
+            list.add(3);  //2
+            list.add(4);  //3
+            list.add(5); // 4
+
+            list.remove(2);  //[1, 2, 4, 5]
+            //list.remove(4);
+            list.remove(3); // [1, 2, 4]
+
+        System.out.println(list);
+
+        System.out.println("===============================================");
+
+    // remove(Element)
+        ArrayList<Integer> list2 = new ArrayList<>();
+            list2.add(1);  //0
+            list2.add(2);  //1
+            list2.add(3);  //2
+            list2.add(4);  //3
+            list2.add(5); // 4
+
+       // int a = 1; // [1, 3, 4, 5]
+
+        Integer a = 1; //[2, 3, 4, 5]
+
+        list2.remove(a);
+
+        System.out.println(list2);
+
+
 
     }
 

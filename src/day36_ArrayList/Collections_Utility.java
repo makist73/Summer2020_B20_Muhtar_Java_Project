97f1492f1d3b4319784c732f127bc85fb3ed5fc5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-28 21:42:53 -0400

    Day36 Class

--- src/day36_ArrayList/Collections_Utility.java
+++ src/day36_ArrayList/Collections_Utility.java
@@ -27,6 +27,38 @@ public class Collections_Utility {
         System.out.println("Minimum number: "+list.get(0));
 
 
+        ArrayList<String> list2 = new ArrayList<>();
+                list2.add("A");
+                list2.add("B");
+                list2.add("C");
+                list2.add("D");
+                list2.add("E");
+
+        System.out.println(list2);
+
+        Collections.swap(list2, 1,  2); //{A, C, B, D, E};
+
+        System.out.println(list2);
+
+
+        Collections.swap(list2, 0, list2.size()-1); //{E, C, B, D, A};
+
+        System.out.println(list2);
+
+
+        ArrayList<Character> charList = new ArrayList<>();
+                charList.add('A');
+                charList.add('B');
+                charList.add('A');
+                charList.add('A');
+                charList.add('A');
+                charList.add('C');
+
+        int count =  Collections.frequency(charList, 'B');
+
+        System.out.println(count);
+
+
 
     }
 

ad7661d8a9efe04ecdadf117b4e167d102443557
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-07-28 21:30:49 -0400

    Day36 Class

--- src/day36_ArrayList/Collections_Practice.java
+++ src/day36_ArrayList/Collections_Practice.java
@@ -1,5 +1,8 @@
 package day36_ArrayList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Collections_Practice {
     /*
      {30,20, 40, 50, 15};
@@ -8,7 +11,24 @@ public class Collections_Practice {
      */
     public static void main(String[] args) {
 
+        ArrayList<Integer> list = new ArrayList<>();
+                    list.add(30);
+                    list.add(20);
+                    list.add(40);
+                    list.add(50);
+                    list.add(15);  //  {30,20, 40, 50, 15};
 
+        Collections.sort(list);  // [15, 20, 30, 40, 50]
+        System.out.println(list);
+
+        ArrayList<Integer> descendingList = new ArrayList<>();
+
+        for(int i= list.size()-1; i >= 0; i--){
+          //  System.out.print( list.get(i) +" ");
+            descendingList.add(  list.get(i) );
+        }
+
+        System.out.println(descendingList);
 
     }
 

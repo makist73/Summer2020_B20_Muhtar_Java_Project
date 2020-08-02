46cd55a000b3c0fc9f9fe596702fb2f7ebed3063
cybertekschool <github@cybertekschool.com>
2020-08-02 10:57:58 -0400

    Day37 Class

---
+++ src/day37_ArrayList/FrequencyOfChars.java
@@ -0,0 +1,33 @@
+package day37_ArrayList;
+
+import Library.Util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FrequencyOfChars {
+
+    public static void main(String[] args) {
+        String str = "AAABBC";
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for(String each  : str.split("") ){
+            list.add(each);
+        }
+
+        System.out.println(list);
+
+        String nonDup = Util.removeDup(str); //"ABC"   to avoid duplication
+
+        for(String each : nonDup.split("")){  // each: A, B, C
+            int count =  Collections.frequency(list, each);
+            System.out.print(each+count);
+        }
+
+
+
+
+    }
+
+}

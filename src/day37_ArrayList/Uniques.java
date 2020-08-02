36abfeb8d5ca18ed953599811c731dd54ef44232
cybertekschool <github@cybertekschool.com>
2020-08-02 10:41:01 -0400

    Day37 Class

--- src/day37_ArrayList/Uniques.java
+++ src/day37_ArrayList/Uniques.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Uniques {
 
     public static void main(String[] args) {
-        String str = "AABBCDDEAF";
+        String str = "AAABCCCCDF";
         String uniques = "";
 
         ArrayList<String> list = new ArrayList<>();

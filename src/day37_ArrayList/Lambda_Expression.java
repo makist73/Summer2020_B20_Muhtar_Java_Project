d997188efc1d3672aeaef36a64974dca74de352b
cybertekschool <github@cybertekschool.com>
2020-08-02 15:05:02 -0400

    Day37 Class

--- src/day37_ArrayList/Lambda_Expression.java
+++ src/day37_ArrayList/Lambda_Expression.java
@@ -2,6 +2,7 @@ package day37_ArrayList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.function.Predicate;
 
 public class Lambda_Expression {
@@ -41,6 +42,26 @@ public class Lambda_Expression {
         names.removeIf(  p -> p.startsWith("M")  );
 
         System.out.println(names);
+
+
+        System.out.println("=======================================================");
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.addAll(Arrays.asList(1,1,1,2,2,2,3,3,3,4,5,6,7,8,8,9));
+
+        nums.removeIf(  p ->   Collections.frequency(nums, p) != 1  );
+                            //  for the elements that are not unique
+
+        System.out.println(nums);
+
+        System.out.println("=========================================================");
+
+        ArrayList<Character> chars = new ArrayList<>();
+
+
+
+
+
 
 
 

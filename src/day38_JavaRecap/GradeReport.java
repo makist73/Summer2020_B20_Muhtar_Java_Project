67eebdd6556ea68bafe097a5f18b13ac8072b612
cybertekschool <github@cybertekschool.com>
2020-08-04 20:32:04 -0400

    Day38 Class

--- src/day38_JavaRecap/GradeReport.java
+++ src/day38_JavaRecap/GradeReport.java
@@ -8,7 +8,7 @@ public class GradeReport {
     public static void main(String[] args) {
 
         ArrayList<Integer> list = new ArrayList<>();
-        list.addAll( Arrays.asList( 100,90,85,75,55,45,73,73,35,47,60, 87, 77, 67, 57, 47, 93, 83, 73, 63, 53, 43) );
+        list.addAll( Arrays.asList(62, 63, 64, 66, 67 ,81, 82 ,100,90,85,75,55,45,73,73,35,47,60, 87, 77, 67, 57, 47, 93, 83, 73, 63, 53, 43) );
 
         System.out.println( list );
 
@@ -24,7 +24,6 @@ public class GradeReport {
         gradeB.addAll( list ); // first store all the grades
         gradeB.removeIf( p -> p < 80  || p > 89 ); // second remove the grades that are not B  ( F, D, C, A)
                 // if the garde is either less than 80 or greater 89 should be removed
-
         System.out.println("Grade B: "+gradeB);
 
 
@@ -33,10 +32,12 @@ public class GradeReport {
         gradeC.removeIf( p -> p < 70 || p > 79 ); // second remove the grades that are not C  ( F, D, B, A)
         System.out.println("Grade C: "+gradeC);
 
+
         ArrayList<Integer> gradeD = new ArrayList<>(); // 60 ~ 69
         gradeD.addAll(list);
         gradeD.removeIf( p -> p <60 || p > 69);
         System.out.println("Grade D: "+gradeD);
+
 
         ArrayList<Integer> gradeF = new ArrayList<>(); // 0 ~ 59
         gradeF.addAll(list);
@@ -51,7 +52,12 @@ public class GradeReport {
 
         System.out.println("Grade F: "+gradeF);
 
-
+        System.out.println("=========================================");
+        System.out.println( gradeA.size()+" students made A" );
+        System.out.println( gradeB.size()+" students made B" );
+        System.out.println( gradeC.size()+" students made C" );
+        System.out.println( gradeD.size()+" students made D" );
+        System.out.println( gradeF.size()+" students failed" );
 
 
 

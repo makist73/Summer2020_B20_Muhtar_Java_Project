c5f7d5f3ee4c454c06e836732ffbf9497e114b91
cybertekschool <github@cybertekschool.com>
2020-08-09 16:08:45 -0400

    Day39 Class

---
+++ src/day39_CustomClass/Employee.java
@@ -0,0 +1,28 @@
+package day39_CustomClass;
+
+public class Employee {
+
+    /*
+    Attributes:
+		employeeName, gender, SSN, JobTitle, Salary
+     */
+
+    String name;
+    char gender;
+    long SSN;
+    String jobTitle;
+    double salary;
+
+    public void setEmployeeInfo(String employeeName, char employeeGender, long employeeSSN, String employeeJobTitle, double employeeSalary){
+        name = employeeName;
+        gender = employeeGender;
+        SSN = employeeSSN;
+        jobTitle = employeeJobTitle;
+        salary = salary;
+    }
+
+
+
+
+
+}

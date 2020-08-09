19fcd09001cdc1141a3cf7ddf8934f1a0086295a
cybertekschool <github@cybertekschool.com>
2020-08-09 11:00:02 -0400

    Day39 Class

---
+++ src/day39_CustomClass/Car.java
@@ -0,0 +1,19 @@
+package day39_CustomClass;
+
+public class Car {
+    /*
+        Attributes:
+            model, brand, year, color, mileage, price
+     */
+
+
+    String brand;
+    String model;
+    int year;
+    String color;
+    double mileage;
+    double price;
+
+
+
+}
---
+++ src/day39_CustomClass/CarObjects.java
@@ -0,0 +1,41 @@
+package day39_CustomClass;
+
+public class CarObjects {
+
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+
+            car1.brand = "Toyota";
+            car1.model = "Corolla";
+            car1.year = 2020;
+            car1.color = "White";
+            car1.mileage = 20000;
+            car1.price = 19000.50;
+
+
+        System.out.println( car1.brand );
+        System.out.println( car1.model );
+        System.out.println( car1.year );
+        System.out.println( car1.color );
+
+        System.out.println("=============================");
+        Car car2 = new Car();
+            car2.brand = "Mercedes";
+            car2.model = "G 550";
+            car2.year = 2000;
+            car2.color = "Blue";
+
+        System.out.println( car2.brand  );
+        System.out.println( car2.model  );
+        System.out.println( car2.year  );
+        System.out.println( car2.color  );
+
+
+
+
+
+    }
+
+}

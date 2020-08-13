a870c221ae1b00ae69a79ba6f676b68afabb7864
cybertekschool <github@cybertekschool.com>
2020-08-12 21:27:26 -0400

    Day40 Class

--- src/day40_CustomClassPractice/Offer.java
+++ src/day40_CustomClassPractice/Offer.java
@@ -22,6 +22,24 @@ public class Offer {
     boolean hasBenefit;
 
 
+    public void setOfferInfo(double salary, String state, boolean hasPTO, boolean isFullTime, boolean isWFH, String jobTitle, boolean hasBenefit){
+        salary  = salary;
+        state = state;
+        hasPTO = hasPTO;
+        isFullTime = isFullTime;
+        isWFH = isWFH;
+        jobTitle = jobTitle;
+        hasBenefit = hasBenefit;
+    }
+
+
+    public void getOfferInfo(){// display the offer info
+        System.out.println("==============================================");
+        System.out.println("Salary: "+salary);
+        System.out.println("JobTitle: "+jobTitle);
+        System.out.println("Location: "+state);
+        System.out.println("==============================================");
+    }
 
 
 

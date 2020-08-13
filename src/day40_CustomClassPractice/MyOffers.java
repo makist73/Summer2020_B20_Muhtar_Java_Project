b0de9070556095ce5f4bdbd67c0e97710b97b7ee
cybertekschool <github@cybertekschool.com>
2020-08-12 21:55:40 -0400

    Day40 Class

--- src/day40_CustomClassPractice/MyOffers.java
+++ src/day40_CustomClassPractice/MyOffers.java
@@ -14,17 +14,36 @@ public class MyOffers {
         Offer offer6 = new Offer();
         Offer offer7 = new Offer();
 
-        offer1.setOfferInfo(100000, "VA", true, true, true, "SDET", true);
-        offer2.setOfferInfo(75_000,"VA",true,true, false, "SDET", true);
+        offer1.setOfferInfo(105000, "VA", true, true, true, "SDET", true);
+        offer2.setOfferInfo(175_000,"VA",true,true, false, "SDET", true);
         offer3.setOfferInfo(125000,"VA", true, true, true, "SDET", true);
-        offer4.setOfferInfo(100_000,"PA",true,false, false, "QA", true);
+        offer4.setOfferInfo(130_000,"PA",true,false, false, "QA", true);
         offer5.setOfferInfo(100_000,"CA",true,false, false, "QA", true);
         offer6.setOfferInfo(120000,"CO",true,true,true,"Senior SDET",true);
-        offer7.setOfferInfo(100_000,"GA",true,false, false, "QA", true);
+        offer7.setOfferInfo(110_000,"GA",true,false, false, "QA", true);
 
 
         ArrayList<Offer> jobOffers = new ArrayList<>();
-        jobOffers.addAll(Arrays.asList(offer1, offer2, offer3));
+        jobOffers.addAll(Arrays.asList(offer1, offer2, offer3, offer4, offer5, offer6, offer7));
+
+        for( Offer eachOffer  : jobOffers){
+            eachOffer.getOfferInfo();
+        }
+
+        System.out.println("==================================================");
+        // less than 120k:
+      //   jobOffers.removeIf( p -> p.salary < 120000  );
+     //   System.out.println("Number of offers: "+jobOffers.size());
+
+
+        // less than 120 or not from VA area
+      //  jobOffers.removeIf( p -> p.salary< 120000 || !p.state.equals("VA") );
+      //  System.out.println("Number of Offers: "+jobOffers.size());
+
+
+        //  does not have PTO or does not have benefit or is not full time, or less than 120k
+        jobOffers.removeIf( p -> p.hasPTO == false || p.hasBenefit == false || p.isFullTime == false || p.salary < 120000);
+        System.out.println("Number of Offers: "+jobOffers.size());
 
 
 

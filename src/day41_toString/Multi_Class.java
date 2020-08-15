93079903af43a6c8dcc43b48f4e17efba991bf32
cybertekschool <github@cybertekschool.com>
2020-08-15 16:11:33 -0400

    Day41 Class

---
+++ src/day41_toString/Multi_Class.java
@@ -0,0 +1,25 @@
+package day41_toString;
+
+public class Multi_Class {
+    public static void main(String[] args) {
+        System.out.println("Class 1");
+    }
+
+}
+
+
+
+class Test1{
+
+    public static void main(String[] args) {
+        System.out.println("Class 2");
+    }
+
+}
+
+class Test2{
+
+    public static void main(String[] args) {
+        System.out.println("Class 3");
+    }
+}

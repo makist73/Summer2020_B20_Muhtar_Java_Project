cd32523c6db1f3e03160e20023e78dba6c40b7b1
cybertekschool <github@cybertekschool.com>
2020-08-15 10:57:20 -0400

    Day41 Class

---
+++ src/day41_Static/Carpet.java
@@ -0,0 +1,62 @@
+package day41_Static;
+ /*
+        instance variables:
+                width, length, unitPrice, isPersian (boolean)
+
+        instance methods:
+                customOrder(): sets the carpet' width, length, unitprice, & isPersian
+                calcCost(): should be able to calculate the total cost of the carpet and return it as double
+                getCarpetInfo(): should be able to display all the info of the carpet including the total cost
+                of the carpet as calculated by calcCost()
+
+            total price of carpet= (width*length)*unitprice
+
+            if the carpet is persian  carpet that's came from Turkey, add 200$ to the totalPrice
+     */
+
+public class Carpet {
+
+        double width;
+        double length;
+        double unitPrice;
+        boolean isPersian;
+
+
+        public void customOrder(double width, double length, double unitPrice, boolean isPersian ){
+            this.width = width;
+            this.length = length;
+            this.unitPrice = unitPrice;
+            this.isPersian = isPersian;
+            // instance         local
+        }
+
+        public double calcCost(){
+            double totalPrice = (width * length)* unitPrice; // 200
+            if(isPersian){
+                totalPrice+= 200;
+            }
+            // return isPersian ? totalPrice+200:totalPrice
+            return  totalPrice ;
+        }
+
+        public void getCarpetInfo(){
+            System.out.println("=============================");
+            System.out.println("Width: "+width);
+            System.out.println("Length: "+ length);
+            System.out.println("Unit Price: "+ unitPrice);
+            System.out.println("Total Price: "+ calcCost());
+            System.out.println("=============================");
+        }
+
+
+
+
+/*
+carpet1
+carpet2
+carpet3
+ */
+
+
+
+}

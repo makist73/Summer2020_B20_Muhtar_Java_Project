6870aed4be2b33d28b02b412605835d9cc3606a9
cybertekschool <github@cybertekschool.com>
2020-08-15 16:50:11 -0400

    Day41 Class

--- src/day41_toString/Tester.java
+++ src/day41_toString/Tester.java
@@ -1,5 +1,25 @@
 package day41_toString;
 
+
+class TesterObject{
+
+    public static void main(String[] args) {
+        Tester[] testers = {new Tester(), new Tester(), new Tester(), new Tester(), new Tester()};
+        testers[0].setInfo("Aalia",150000,"Female",18);
+        testers[1].setInfo("Naz",150000,"Female",18);
+        testers[2].setInfo("Kalbinur",  155000 , "Female", 21);
+        testers[3].setInfo("Rakhat", 130000, "Female", 28);
+        testers[4].setInfo("Edward", 110000, "Male", 30);
+
+
+        for(Tester each  : testers ){
+            System.out.println(each);
+        }
+
+    }
+}
+
+
 public class Tester {
 
     String name;
@@ -19,3 +39,9 @@ public class Tester {
     }
 
 }
+
+
+
+
+
+

381c63d3a55ec451f171ef1d55826cc90a0d9516
cybertekschool <github@cybertekschool.com>
2020-08-15 12:51:09 -0400

    Day41 Class

---
+++ src/day41_Static/Car.java
@@ -0,0 +1,21 @@
+package day41_Static;
+
+public class Car {
+
+    String brand;
+    String model;
+    int year;
+    double price;
+    String color;
+
+    public void setCarInfo(String brand, String model, int year, double price, String color){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color= color;
+    }
+
+
+
+}

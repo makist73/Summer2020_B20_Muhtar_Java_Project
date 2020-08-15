48c64ed5e73832e221c73a7fe2dd8d47dbd11a8d
cybertekschool <github@cybertekschool.com>
2020-08-15 14:55:25 -0400

    Day41 Class

---
+++ src/day41_Static/Cat.java
@@ -0,0 +1,48 @@
+package day41_Static;
+/*
+	Cat:
+		attributes:
+				name, age, gender, color, breed
+		Actions:
+				setInfo, eat, drink, toString
+				use this. keyword
+
+	Make sure that you can print out the objects of the Cat in the print statement
+ */
+public class Cat {
+
+        String name;
+        int age;
+        char gender;
+        String color;
+        String breed;
+
+        public void setCatInfo(String name, int age, char gender, String color, String breed){
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.color = color;
+            this.breed = breed;
+        }
+
+        public void eat(String catFood){
+            System.out.println(name+" is eating "+catFood);
+        }
+
+        public void drink(String drink){
+            System.out.println(name+" is drinking "+drink);
+        }
+
+
+        /*
+        cat1:
+        cat2:
+        cat3:
+         */
+
+        public String toString(){
+            return "Name: "+name+", Breed: "+breed+", Gender: "+gender+", Age: "+age+", Color: "+color;
+        }
+
+
+}
---
+++ src/day41_Static/CatObjects.java
@@ -0,0 +1,16 @@
+package day41_Static;
+
+public class CatObjects {
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.setCatInfo("Cass", 2, 'F', "Black", "Persian");
+
+
+        System.out.println( cat1 );
+
+
+    }
+
+
+}

5d8965e3b8c5cbc96c709f01e21ffae47e4b9355
cybertekschool <github@cybertekschool.com>
2020-08-16 10:29:14 -0400

    Day42 Class

---
+++ src/day42_Static/Developer.java
@@ -0,0 +1,40 @@
+package day42_Static;
+/*
+create a class called Developer
+					Attributes:
+						name, employeeID, JobTitle, Salary
+
+					Actions:
+						setInfo(), coding(),  fixBug(), toString()
+ */
+
+public class Developer {
+
+    String name;
+    char gender;
+    long employeeID;
+    String jobTitle;
+    double salary;
+
+    public void setInfo(String name, char gender,long employeeID, String jobTitle, double salary){
+        this.name=name;
+        this.employeeID=employeeID;
+        this.jobTitle=jobTitle;
+        this.salary=salary;
+        this.gender = gender;
+    }
+
+    public void coding(){
+        System.out.println(name+" is coding");
+    }
+
+    public void fixBug(){
+        System.out.println(name+" is fixing the bug");
+    }
+
+    public String toString(){
+        return "Name: "+name+", Gender: "+gender+", Job Title: "+jobTitle+", Salary: "+salary+", Employee ID: "+employeeID;
+    }
+
+
+}

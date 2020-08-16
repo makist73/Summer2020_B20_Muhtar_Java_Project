c15dc80c1e9d0a8bbbdd757d86831171a03d66bf
cybertekschool <github@cybertekschool.com>
2020-08-16 12:12:19 -0400

    Day42 Class

--- src/day42_Static/CapitalOne.java
+++ src/day42_Static/CapitalOne.java
@@ -13,15 +13,51 @@ public class CapitalOne {
         developers[1].setInfo("roza", 'F', 3222L, "Dev", 550000);
         developers[2].setInfo("burak", 'M', 43333L, "Dev", 650000);
         developers[3].setInfo("Virginia", 'F', 76543, "Dev", 130000);
-        developers[4].setInfo("Akbar", 'M', 0005L, "Dev", 120000);
+        developers[4].setInfo("Akbar", 'M', 5435L, "Dev", 120000);
+
 
 
         ScrumTeam team1 = new ScrumTeam();
+
         team1.setInfo("Ramazan", "Hamit", "Ismail");
         team1.addTester(testers);
         team1.addDeveloper(developers);
 
         System.out.println(team1);
+
+        System.out.println("================================");
+        for(Tester each : team1.testers ){// each: every single testers in the scrum team
+            System.out.println(each.name +" : $"+each.salary);
+        }
+
+        System.out.println("=====================================");
+
+        for(Developer each : team1.developers){
+            System.out.println(each.name +" : $"+each.salary);
+        }
+
+        System.out.println("=======================================");
+        team1.removeTester(15333);
+        team1.removeDeveloper(2111);
+        team1.removeDeveloper(43333);
+        System.out.println(team1);
+
+        System.out.println("======================================");
+        Developer dev1 =  new Developer();
+        dev1.setInfo("luisa", 'f', 2113L, "Dev Lead" , 500000);
+
+        team1.addDeveloper(dev1);
+
+        System.out.println(team1);
+
+
+
+        ScrumTeam[] scrumTeams = {team1, new ScrumTeam(), new ScrumTeam()};
+        /*
+        print all developers and testers from scrum team
+
+        find the maximum salary
+         */
 
 
     }

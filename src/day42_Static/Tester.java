42f40b97a8b12e8c7bc6a3b135583f410d06e61a
cybertekschool <github@cybertekschool.com>
2020-08-16 10:24:29 -0400

    Day42 Class

---
+++ src/day42_Static/Tester.java
@@ -0,0 +1,41 @@
+package day42_Static;
+/*
+create a class called Testers
+					Attributes:
+						name, employeeID, JobTitle, Salary
+
+					Actions:
+						setInfo(), smokeTesting(),  creatingTicket(), toString()
+
+ */
+public class Tester {
+
+    String name;
+    char gender;
+    long employeeID;
+    String jobTitle;
+    double salary;
+
+    public void setInfo(String name, char gender,long employeeID, String jobTitle, double salary){
+        this.name=name;
+        this.employeeID=employeeID;
+        this.jobTitle=jobTitle;
+        this.salary=salary;
+        this.gender = gender;
+    }
+
+    public void smokeTesting(){
+        System.out.println(name+" smoke testing");
+    }
+
+    public void creatingTicket(){
+        System.out.println(name+" is creating ticket");
+    }
+
+    public String toString(){
+        return "Name: "+name+", Gender: "+gender+", Job Title: "+jobTitle+", Salary: "+salary+", Employee ID: "+employeeID;
+    }
+
+
+
+}

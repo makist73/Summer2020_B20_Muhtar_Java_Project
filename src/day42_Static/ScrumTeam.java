848b52391a74f86a22da3c27bac07a54904f012f
cybertekschool <github@cybertekschool.com>
2020-08-16 10:54:54 -0400

    Day42 Class

---
+++ src/day42_Static/ScrumTeam.java
@@ -0,0 +1,61 @@
+package day42_Static;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+/*
+create a class called ScrumTeam
+
+					Attributes:
+							PO, BA, SM,
+							ArrayList<Tester> testers = new ArrayList<>(),
+							ArrayList<Developer> testers = new ArrayList<>(),
+
+					Actions:
+							setInfo(): sets the names of: PO, BA, SM
+							addTester(Tester tester): adds the given tester to the testers arraylist
+							addTesters(Tester[] testers): adds the given testers to the testers arraylist
+							addDeveloper(Developer developer): adds the given developer to the developers arraylist
+							addDevelopers(Developer[] developers): adds the given developers to the developers arraylist
+							removeTester(long employeeID): removes the given tester from the testers arraylist
+							removeDeveloper(long employeeID): removes the developer from the developers arraylist
+ */
+
+
+public class ScrumTeam {
+
+    ArrayList<Tester> testers = new ArrayList<>();
+    ArrayList<Developer> developers = new ArrayList<>();
+    String PO;
+    String BA;
+    String SM;
+
+    public void setInfo(String PO, String BA, String SM){
+        this.PO = PO;
+        this.BA = BA;
+        this.SM = SM;
+    }
+
+    public void addTester(Tester tester){
+        testers.add(tester);
+    }
+
+    public void addTester(Tester[] testers){
+        this.testers.addAll(Arrays.asList(testers) );
+    }
+
+    public void removeTester(long employeeID){ // 007
+        testers.removeIf( p -> p.employeeID == employeeID);
+                    //              each employee id,  == 007
+    }
+
+
+
+
+    /*
+    team1
+    team2
+    team3
+     */
+
+
+}

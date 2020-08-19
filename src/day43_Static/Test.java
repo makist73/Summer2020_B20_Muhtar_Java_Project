7f927cba84406519f3b01cb1d2b36d64e8af59e9
cybertekschool <github@cybertekschool.com>
2020-08-18 21:07:16 -0400

    Day43 Class

---
+++ src/day43_Static/Test.java
@@ -0,0 +1,14 @@
+package day43_Static;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(staticBlock3.a);
+        System.out.println(staticBlock3.b);
+        System.out.println(staticBlock3.c);
+
+
+    }
+
+}
---
+++ src/day43_Static/staticBlock3.java
@@ -0,0 +1,23 @@
+package day43_Static;
+
+public class staticBlock3 {
+
+    static int a;
+    static int b;
+    static int c;
+
+    static{
+        a = 100;
+        b = 200;
+        c = 300;
+    }
+
+/*
+    public static void main(String[] args) {
+        a = 100;
+        b = 200;
+        c = 300;
+    }
+*/
+
+}

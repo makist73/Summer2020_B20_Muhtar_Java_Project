ed9b35ccbaad1ba5ebd61578167283fb70c37c0b
cybertekschool <github@cybertekschool.com>
2020-08-19 19:36:04 -0400

    Day44 Class

--- src/day44_Constructor/Employee.java
+++ src/day44_Constructor/Employee.java
@@ -21,6 +21,14 @@ public class Employee {
     double salary;
     char gender;
 
+    static boolean isEmployed ;
+    static boolean hasSalary;
+
+    static{ // runs first, only one time
+        isEmployed = true;
+        hasSalary = true;
+    }
+
     public void setEmployeeInfo(String name, int id, int ssn, String jobTitle, double salary, char gender){
             this.name = name;
             this.id = id;
@@ -28,8 +36,13 @@ public class Employee {
             this.jobTitle = jobTitle;
             this.salary = salary;
             this.gender = gender;
+
     }
 
+    public String toString(){
+        return "Name: "+name+"\n Gender: "+gender+"\n ID: "+id+"\nSSN: "+ssn+"\n JobTitle:"+jobTitle+
+                "\n Salary: "+salary;
+    }
 
 
 

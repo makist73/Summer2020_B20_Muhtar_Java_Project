d3d313c1cda3ee22fe2a1d5d99f001dcc8277d41
cybertekschool <github@cybertekschool.com>
2020-08-18 21:39:33 -0400

    Day43 Class

---
+++ src/day43_Static/Data.java
@@ -0,0 +1,32 @@
+package day43_Static;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Data {
+
+    static int[] arr = new int[3];  // 1,2,3
+    static ArrayList<Integer> list = new ArrayList<>();  // 4,5,6
+
+    static{
+
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println("Static block");
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(arr)); //[1,2,3]
+        System.out.println(list); //[4,5,6]
+
+    }
+
+}
--- src/day43_Static/Test.java
+++ src/day43_Static/Test.java
@@ -1,12 +1,19 @@
 package day43_Static;
 
+import java.util.Arrays;
+
 public class Test {
 
-    public static void main(String[] args) {
 
+
+
+    public static void main(String[] args) {
         System.out.println(staticBlock3.a);
         System.out.println(staticBlock3.b);
         System.out.println(staticBlock3.c);
+
+        System.out.println( Arrays.toString(Data.arr) );
+        System.out.println(Data.list);
 
 
     }

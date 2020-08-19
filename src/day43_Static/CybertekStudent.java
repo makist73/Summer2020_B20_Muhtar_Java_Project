c9b3f3b470b24b7fc4711818b762e4bb9a351b28
cybertekschool <github@cybertekschool.com>
2020-08-18 20:23:26 -0400

    Day43 Class

---
+++ src/day43_Static/CybertekStudent.java
@@ -0,0 +1,15 @@
+package day43_Static;
+
+class A{
+    static int  a = 200;
+}
+
+public class CybertekStudent {
+    // for the advantages of static
+
+    String studentName;
+    int age;
+    char gender;
+    static  String schoolName = "Cybertek School";
+
+}
---
+++ src/day43_Static/studentObjects.java
@@ -0,0 +1,9 @@
+package day43_Static;
+
+public class studentObjects {
+
+    public static void main(String[] args) {
+
+
+    }
+}

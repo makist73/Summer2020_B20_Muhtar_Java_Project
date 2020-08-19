887d4aa45037193b418d44392a58b422cf9eb249
cybertekschool <github@cybertekschool.com>
2020-08-19 19:01:13 -0400

    Office Hour: 08/19/2020

--- src/Office_Hours/Practice_08_19_2020/PizzaObjects.java
+++ src/Office_Hours/Practice_08_19_2020/PizzaObjects.java
@@ -23,6 +23,20 @@ public class PizzaObjects {
 
         System.out.println("Total Price: $"+totalPrice);
 
+        System.out.println("=================================================================");
+
+        Pizza pizza1 = new Pizza();
+        pizza1.setPizzaInfo("Large", 3, 4);
+
+        Pizza pizza2 = new Pizza();
+        pizza2.setPizzaInfo("Medium", 3, 2);
+
+        System.out.println("Nadir' Pizza: $"+pizza1.calcPrice());
+
+        System.out.println();
+
+        System.out.println("Saim' Pizza: $"+pizza2.calcPrice());
+
 
 
 

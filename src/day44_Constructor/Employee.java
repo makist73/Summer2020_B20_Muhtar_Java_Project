60a64732ad0622e83693a3f5442f650bb9339658
cybertekschool <github@cybertekschool.com>
2020-08-19 19:26:02 -0400

    Day44 Class

---
+++ src/day44_Constructor/Employee.java
@@ -0,0 +1,39 @@
+package day44_Constructor;
+/*
+1. Create a class called Employee
+            instance variables:
+                    name, id, ssn, jobTitle, salary, gender
+
+            static variables:
+            		isEmployeed, hasSalary
+
+            actions:
+                    setEmployeeInfo(): can initialize all the instance variables
+                    toString(): can return the info of the employee as string
+ */
+
+public class Employee {
+
+    String name;
+    int id;
+    int ssn;
+    String jobTitle;
+    double salary;
+    char gender;
+
+    public void setEmployeeInfo(String name, int id, int ssn, String jobTitle, double salary, char gender){
+            this.name = name;
+            this.id = id;
+            this.ssn = ssn;
+            this.jobTitle = jobTitle;
+            this.salary = salary;
+            this.gender = gender;
+    }
+
+
+
+
+
+
+
+}

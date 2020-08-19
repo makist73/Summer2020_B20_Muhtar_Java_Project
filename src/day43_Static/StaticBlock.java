bfbf69fac63dafe5ade3c6f305027eed8662acf3
cybertekschool <github@cybertekschool.com>
2020-08-18 20:51:05 -0400

    Day43 Class

---
+++ src/day43_Static/StaticBlock.java
@@ -0,0 +1,15 @@
+package day43_Static;
+
+public class StaticBlock {
+
+
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+
+    static {
+        System.out.println("Static block");
+    }
+
+
+}

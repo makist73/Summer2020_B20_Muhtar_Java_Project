d642d277ee7eb5934781be807bc2e41d6ba4eb8d
cybertekschool <github@cybertekschool.com>
2020-08-19 21:41:08 -0400

    Day44 Class

---
+++ src/day44_Constructor/Student.java
@@ -0,0 +1,25 @@
+package day44_Constructor;
+
+public class Student {
+
+    String name;
+    int age;
+    char gender;
+    static String schoolName = "Cybertek University";
+
+    public void setInfo(String name, int age, char gender){
+        this.name =name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+
+    public String toString(){
+        return "Name: "+name+", Age: "+age+", Gender: "+gender+", School Name: "+schoolName;
+    }
+
+
+
+
+
+}

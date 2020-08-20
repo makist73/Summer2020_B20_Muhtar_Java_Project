866c1487157081ed277cec6c69924240de75f5a3
cybertekschool <github@cybertekschool.com>
2020-08-19 21:43:42 -0400

    Day44 Class

---
+++ src/day44_Constructor/StudentObjects.java
@@ -0,0 +1,27 @@
+package day44_Constructor;
+
+public class StudentObjects {
+
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        student1.setInfo("Mike", 18, 'M');
+
+
+        Student student2 = new Student();
+        student2.setInfo("Rakhat", 28, 'F');
+
+        Student student3 = new Student();
+        student3.setInfo("Belek", 30, 'M');
+
+
+
+
+
+
+    }
+
+
+
+}

d943c923095bd8452163aae764371cd8e52509d4
cybertekschool <github@cybertekschool.com>
2020-08-24 18:45:04 -0400

    Office Hour: 08/24/2020

---
+++ src/Office_Hours/Practice_08_24_2020/ArrayList_Recap.java
@@ -0,0 +1,60 @@
+package Office_Hours.Practice_08_24_2020;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+addAll(CollectionType):
+removeAll:
+retainAll
+containAll:
+ */
+
+public class ArrayList_Recap {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.addAll(   Arrays.asList(10,20,30,40,50 ) );
+
+        System.out.println( list1  );
+
+        for( int each  : list1){
+            System.out.println(each);
+        }
+
+        System.out.println("===========================================");
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.addAll( Arrays.asList(30,40,50,60,70,80,90,100, 200) );
+
+        // remove: 40, 50, 90, 200
+        list2.removeAll( Arrays.asList(40,50,90,200) );
+        System.out.println(list2);
+
+        Collections.swap(list2 ,  0,  list2.size()-1);
+        System.out.println(list2);
+
+        System.out.println( list2.get(0) );
+        System.out.println(list2.get( list2.size()-1 ));
+
+        // arr[0]  = 300
+        list2.set(0, 300);
+
+        System.out.println(list2);
+
+        Collections.sort(list2);
+
+        System.out.println(list2);
+
+
+        System.out.println("===============================================");
+
+        ArrayList<String> names = new ArrayList<>();
+        names.addAll( Arrays.asList("Ahmed", "Elkem", "Cristina", "Amton", "Ahmed", "Ahmed")  );
+
+
+    }
+
+}

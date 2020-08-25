191d41450636422d786829e82e61ed4ad3473fd0
cybertekschool <github@cybertekschool.com>
2020-08-25 18:44:16 -0400

    Office Hour: 08/25/2020

---
+++ src/Office_Hours/Practice_08_25_2020/ClassNotes.java
@@ -0,0 +1,57 @@
+package Office_Hours.Practice_08_25_2020;
+
+public class ClassNotes {
+
+    /*
+    08/25/1010
+Practice Topics: ArrayList
+				 Collections Utility
+
+
+package name: Practice_08_25_2020
+
+
+Array ===> Arrays
+Collection ===> Collections
+Map
+
+
+array: fixed size.
+		supports primitive or non primitive
+
+Collection: dynamic size (add, remove)
+			ONLy supports non-primitives
+
+
+ArrayList:
+	add:
+	addAll(CollectionType):
+
+	remove:
+	removeAll:
+	retainAll:
+	removeIf:
+	clear():
+
+
+	set():
+
+
+Collections Utility:
+		swap:
+		sort:
+		max:
+		min:
+		frequency:
+
+
+Tomorrow: 5pm ~ 7pm  review section for javatutles
+		custom methods
+		method overloading
+
+
+
+
+     */
+
+}

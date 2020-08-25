319c33681ad162ef9e8fcf22a0a379343e9e3e02
cybertekschool <github@cybertekschool.com>
2020-08-25 19:52:46 -0400

    Day45 Class

--- src/day45_Constructor/Item.java
+++ src/day45_Constructor/Item.java
@@ -1,6 +1,32 @@
 package day45_Constructor;
 
+/*
+1. create a class called Item
+                instance variables:
+                    name, unitPrice, quantity
+
+                add a constructor that can initialize the fields
+
+                instance methods:
+                    calcCost(): returns the total cost as double
+                                hint: quantity * unitPrice
+                    toString(): returns the name, unit price, quantity and total cost info as calculated by calcCost()
+ */
+
 public class Item {
+
+    String name;
+    double unitPrice;
+    int quantity;
+
+    public Item(String name, double unitPrice, int quantity){
+        this.name = name;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+
+
 
 
 

ceea8a98f6968e8fee5cf470a5196785b7e99021
cybertekschool <github@cybertekschool.com>
2020-08-25 20:43:53 -0400

    Day45 Class

---
+++ src/day45_Constructor/SalaryCalculator.java
@@ -0,0 +1,36 @@
+package day45_Constructor;
+
+/*
+2. Create class called SalaryCalculator
+                instance variables:
+                    hourlyRate, weeklyHours, stateTaxRate, fedralTaxRate
+
+                add a constructor can initialize those fields
+
+                instance methods:
+                    salary(): returns the total salary as double  (hourlyRate * weeklyHours * 48)
+                    stateTax(): retuns the total state tax as double ( salary * stateTaxRate)
+                    federalTax(): retuns the total federal tax as double ( salary * fedralTaxRate)
+                    salaryAftertax(): retuns the salary after tax as double ( salary - stateTax - federalTax)
+                    toString(): returns the hourlyRate, weeklyHours and salary info as calculated by salary()
+ */
+
+public class SalaryCalculator {
+
+    double hourlyRate;
+    int weeklyHour;
+    double stateTaxRate;
+    double federalTaxRate;
+
+    public SalaryCalculator(double hourlyRate, int weeklyHour, double stateTaxRate, double federalTaxRate) {
+        this.hourlyRate = hourlyRate;
+        this.weeklyHour = weeklyHour;
+        this.stateTaxRate = stateTaxRate;
+        this.federalTaxRate = federalTaxRate;
+    }
+
+
+
+
+
+}

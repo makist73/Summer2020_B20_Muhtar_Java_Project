596d5b4c933f7e6e3184e3264596e8ff3a1c20ed
cybertekschool <github@cybertekschool.com>
2020-08-26 19:16:18 -0400

    Office Hours: 08/26/2020

---
+++ src/day46_ConstructorCalls/ConstructorCall.java
@@ -0,0 +1,26 @@
+package day46_ConstructorCalls;
+
+public class ConstructorCall {
+
+    public ConstructorCall(){
+        method1();
+        method2();
+        method3();
+    }
+
+
+    public static void method1(){
+            method2();
+
+     // ConstructorCall();
+    }
+
+    public static void method2(){
+        method1();
+    }
+
+    public void method3(){
+      //  ConstructorCall();
+    }
+
+}

fbda94a7cadb896623b2874ba6b93c05404b8f8c
cybertekschool <github@cybertekschool.com>
2020-08-26 17:11:33 -0400

    Office Hours: 08/26/2020

---
+++ src/Office_Hours/Practice_08_26_2020/Practice.java
@@ -0,0 +1,31 @@
+package Office_Hours.Practice_08_26_2020;
+
+public class Practice {
+
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1,2,3,4,5,6};
+        for(int i=0; i <= arr1.length-1; i++ ){
+            System.out.print( arr1[i]+" ");
+        }
+        System.out.println();
+
+
+        int[] arr2 = {9,8,7,6,5,4};
+        for(int each : arr2){
+            System.out.print(each+" ");
+        }
+        System.out.println();
+
+
+        int[] arr3 = {100, 200, 300};
+        for(int i=0; i <= arr3.length-1; i++ ){
+            System.out.print( arr3[i]+" ");
+        }
+        System.out.println();
+
+
+    }
+
+}

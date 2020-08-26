bb24c12fb1813dc5d1123a67f2fe1f6fb1b23993
cybertekschool <github@cybertekschool.com>
2020-08-25 20:04:59 -0400

    Day45 Class

---
+++ src/day45_Constructor/ShoppingList.java
@@ -0,0 +1,12 @@
+package day45_Constructor;
+/*
+ create a class called ShoppingList
+                create 5 objects of Item and store them into List of Items
+                calculate the total cost of all Items in the list
+
+ */
+public class ShoppingList {
+
+
+
+}

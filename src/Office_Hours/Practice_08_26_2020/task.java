08c1e292516d976c4d097ac746da9ed3ec8474be
cybertekschool <github@cybertekschool.com>
2020-08-26 18:41:19 -0400

    Office Hours: 08/26/2020

---
+++ src/Office_Hours/Practice_08_26_2020/task.java
@@ -0,0 +1,26 @@
+package Office_Hours.Practice_08_26_2020;
+
+public class task {
+
+     static int a;
+
+    public static void main(String[] args) {
+        a = 1000;
+
+        System.out.println(a);
+       // method();
+
+    }
+
+    static{
+       a= 500;
+        //method();
+    }
+
+    public void method(){
+        System.out.println(a);
+    }
+
+
+
+}

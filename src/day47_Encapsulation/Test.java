8b5e993f6c4e2a7f8881a79bb2428c39bfcc28e6
cybertekschool <github@cybertekschool.com>
2020-08-29 10:17:14 -0400

    Day47 Class

--- src/day47_Encapsulation/Test.java
+++ src/day47_Encapsulation/Test.java
@@ -2,5 +2,11 @@ package day47_Encapsulation;
 
 public class Test {
 
+    public static void main(String[] args) {
+
+        System.out.println( AccessModifiers.publicVariable );
+        AccessModifiers.publicMethod();
+
+    }
 
 }

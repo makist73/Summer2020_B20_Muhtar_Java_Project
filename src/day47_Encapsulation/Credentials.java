8c5e0391d40cd1cdf4bbed3acc123bd97d6b9dd2
cybertekschool <github@cybertekschool.com>
2020-08-29 13:04:37 -0400

    Day47 Class

---
+++ src/day47_Encapsulation/Credentials.java
@@ -0,0 +1,61 @@
+package day47_Encapsulation;
+
+/*
+	1. create class Credentials
+			instance variables:
+					public name
+					public age
+					private username
+					private password
+
+					getter & setter
+
+					add a constructor to set name & age
+
+ */
+
+public class Credentials {
+
+    public String name;
+    public int age;
+
+    public static String companyName;
+
+    private String userName;
+    private String passWord;
+
+    static{
+        companyName = "FaceBook";
+    }
+
+    public Credentials(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord){
+        this.passWord = passWord;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassWord(){
+        return passWord;
+    }
+
+
+
+
+
+
+
+
+
+
+}

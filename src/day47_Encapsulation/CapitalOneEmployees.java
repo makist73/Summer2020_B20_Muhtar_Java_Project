c6a0f615b502bee46891a971bbc1a62007e11b81
cybertekschool <github@cybertekschool.com>
2020-08-29 14:35:34 -0400

    Day47 Class

---
+++ src/day47_Encapsulation/CapitalOneEmployees.java
@@ -0,0 +1,21 @@
+package day47_Encapsulation;
+
+/*2. create a class called CapitalOneEmployees
+				instance variables:
+						employeeName
+						employeeAge
+						jobTitle
+					private	ID
+					private salary
+					private address
+
+					company name = "Capital One"
+
+					generate getters & setters for private variables
+ */
+
+public class CapitalOneEmployees {
+
+
+
+}

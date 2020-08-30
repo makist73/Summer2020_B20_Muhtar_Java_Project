d9cc8dd4feff8e01e278cf3d1a332c25ba56c285
cybertekschool <github@cybertekschool.com>
2020-08-30 12:13:19 -0400

    Day48 Class

---
+++ src/day48_Inheritance/AnimalTask/Dog.java
@@ -0,0 +1,17 @@
+package day48_Inheritance.AnimalTask;
+
+public class    Dog       extends   Animal {
+    //       sub(child)            super(parent)
+
+    /*
+     variables: 2
+     methods: 3
+     */
+
+    public void bark(){
+        System.out.println(name+" is barking");
+    }
+
+
+
+}

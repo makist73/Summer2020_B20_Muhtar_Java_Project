056ff77c0fa0fbeb508ad53ba88c90327b7d53dc
cybertekschool <github@cybertekschool.com>
2020-08-30 16:45:06 -0400

    Day48 Class

--- src/day48_Inheritance/DeviceTask/Laptop.java
+++ src/day48_Inheritance/DeviceTask/Laptop.java
@@ -1,4 +1,27 @@
 package day48_Inheritance.DeviceTask;
 
-public class Laptop {
+/*
+4. create a sub class of Device called Laptop:
+					attributes: brand, model, price, MadeIn, hasBattery, hasMemory, screenSize, hasCPU, hasMouse, hasKeyBoard, OS
+					methods: coding, setInfo, toString
+
+					add a constructor that can set the fileds
+ */
+
+public class Laptop extends Device{
+    public static boolean hasCPU;
+    public static boolean hasMouse;
+    public static boolean hasKeyBoard;
+
+    public String OS;
+
+    public Laptop(String brand, String model, double price, boolean hasBattery, boolean hasMemory, String screenSize, String OS){
+        setInfo(brand, model, price, hasBattery, hasMemory, screenSize);
+        this.OS = OS;
+    }
+
+    public void coding(String language){
+        System.out.println("coding "+language+" in "+brand+" "+model);
+    }
+
 }

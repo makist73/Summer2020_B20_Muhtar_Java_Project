0e5389e194dfcddb8b2a5696def15622e224c65f
cybertekschool <github@cybertekschool.com>
2020-08-30 12:05:53 -0400

    Day48 Class

---
+++ src/day48_Inheritance/AnimalTask/Animal.java
@@ -0,0 +1,16 @@
+package day48_Inheritance.AnimalTask;
+
+public class Animal {
+
+   public String name;
+   public String size;
+
+   public void eat(){
+       System.out.println(name+" is eating");
+   }
+
+   public void sleep(){
+       System.out.println(name+" is sleeping");
+   }
+
+}

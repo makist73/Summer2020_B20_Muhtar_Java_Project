a022e59a75b6e110a3572bee48c087e0796adec8
cybertekschool <github@cybertekschool.com>
2020-08-30 15:42:41 -0400

    Day48 Class

---
+++ src/day48_Inheritance/Inheritance1.java
@@ -0,0 +1,28 @@
+package day48_Inheritance;
+
+class A{
+    static int a = 10;
+
+}
+
+class B extends A{
+    static int b = 20;
+    /*
+    variables: a, b
+     */
+
+}
+
+class C extends B{
+    static int c = 30;
+    /*
+    variables: a, b, c
+     */
+
+}
+
+public class Inheritance1 {
+
+
+
+}

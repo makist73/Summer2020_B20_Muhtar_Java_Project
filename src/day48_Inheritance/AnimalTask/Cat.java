e0800dba8cfc7d5f5ac0e7aa6d06e240f1b85fdd
cybertekschool <github@cybertekschool.com>
2020-08-30 12:46:42 -0400

    Day48 Class

---
+++ src/day48_Inheritance/AnimalTask/Cat.java
@@ -0,0 +1,20 @@
+package day48_Inheritance.AnimalTask;
+
+public class Cat extends Animal {
+
+    /*
+    variables: 4
+    methods: 5
+     */
+
+    public Cat(String name, String size, int age, char gender) {
+        setInfo(name, size, age, gender);
+    }
+
+    public void meow(){
+        System.out.println(name + " meowing");
+    }
+
+
+
+}

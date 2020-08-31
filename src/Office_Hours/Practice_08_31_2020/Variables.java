3bcf52bef0e3ee2a7219b8c934ee22d7565fc0a9
cybertekschool <github@cybertekschool.com>
2020-08-31 17:23:43 -0400

    Day48 Class

---
+++ src/Office_Hours/Practice_08_31_2020/Variables.java
@@ -0,0 +1,18 @@
+package Office_Hours.Practice_08_31_2020;
+
+public class Variables {
+    int z = 400; // instance variable
+
+    static int y = 600;  // static variable
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        System.out.println(a);
+
+       // static int b = 200;
+        int c = 300; // local
+
+    }
+
+}

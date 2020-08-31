bf6b08c4adbab17b785946ee3e78b017b606c989
cybertekschool <github@cybertekschool.com>
2020-08-31 18:43:17 -0400

    Office Hours: 08/31/2020

---
+++ src/Office_Hours/Practice_08_31_2020/ClassNotes.java
@@ -0,0 +1,94 @@
+package Office_Hours.Practice_08_31_2020;
+
+public class ClassNotes {
+
+    /*
+    08/31/2020
+practice topic: static & instance
+
+package name: Practice_08_31_2020
+
+local variable: declared within the block or method
+				cannot be used outside its block
+				MUST be initialized before use
+
+intance variables: declared outside any method or block
+
+static variables: declared outside any method or block with static keyword
+
+
+instance vs static: both are the attributes of the class
+
+	instance: to store unique feature in each object
+				each object has a copy
+				belongs to the object. can be called through the object
+
+	static: to store one common feature for all objects
+			all objects have one copy ONLY
+			belong to the class. can be called through class name
+			static only accepts static
+
+
+Dog Class:
+		instance: breed, age
+		static:  isPet, isAnimal, numberOfLegs
+
+	dog1:  husky              isPet         5 years old      isAnimal     4 legs
+
+	dog2:  pitbull			  isPet			3 years old      isAnimal     4 legs
+
+	dog3:  Golden Retriver	  isPet         4 years old      isAnimal     4 legs
+
+
+
+Phone class:
+		instance: brand, model
+		static: call, text,
+
+
+	phone1:  iPhone       11 pro   call    text    facetime(), iCloud()
+
+	phone2:  Samsung      s20      call    text    freeze()
+
+	phone3:  Nokia        Brick    call    text    breakTheFloor();
+
+
+
+Car class:
+		instances: brand, model
+		statics: wheels, engine, brakes
+
+	car1:  4 wheels  engine    brakes    Toyota    Corolla
+
+	car2:  4 wheels  engine	   brakes    BMW       X5
+
+	car3:  4 wheels  engine    brakes    Honda     Accord
+
+
+
+Test cases:
+	static: driver
+	instance: expectedresults
+
+		test case 1: chrome    result1
+
+		test case 2: chrome		result2
+
+		test case 3: chrome		result3
+
+
+
+
+to initialize:
+
+	static: static block
+	instance: Constructor
+
+
+
+     */
+
+
+
+
+}

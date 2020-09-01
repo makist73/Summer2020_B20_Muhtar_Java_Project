6193e15987368f8e5dc4ed404de9eaab89b56029
cybertekschool <github@cybertekschool.com>
2020-08-31 21:21:12 -0400

    Day49 Class

---
+++ src/day49_Inheritance/ScrumTeamTask/AppleInc.java
@@ -0,0 +1,27 @@
+package day49_Inheritance.ScrumTeamTask;
+/*
+  create a class called AppleInc:
+                    1. create an array of Testers and store the testers info in your group
+                    2. create an array of developers store the developers info in your group
+                    3. create an object of ScrumTeam and store the testers & developers above to the scrum team
+
+ */
+public class AppleInc {
+
+    public static void main(String[] args) {
+        Tester tester1 = new Tester("Rakhat", 30, 'F', 125000, 123456, "SDET");
+        Tester tester2 = new Tester("Murat",30,'M',110_000,10711453,"SDET");
+        Tester tester3 = new Tester("Milovan",33,'M',125000,1,"SDET");
+
+        Tester[] testers = {tester1, tester2, tester3};
+
+        Developer developer1 = new Developer("Ebrahim", 31, 'M', 130000, 8765432, "SDET");
+        Developer developer2 = new Developer("Ahmet",30,'M',150_000,10711453,"Lead Developer");
+        Developer developer3 = new Developer("Yadigarjan", 40,'M', 120000, 3212313,"SDET");
+        Developer developer4= new Developer("fatime",30,'F', 150000,123456,"softwareEngineer");
+
+
+    }
+
+
+}

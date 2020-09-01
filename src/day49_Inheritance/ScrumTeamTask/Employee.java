96f418e0be101f4f256a9ee39e430dcc5cd3ba14
cybertekschool <github@cybertekschool.com>
2020-08-31 20:13:42 -0400

    Day49 Class

--- src/day49_Inheritance/ScrumTeamTask/Employee.java
+++ src/day49_Inheritance/ScrumTeamTask/Employee.java
@@ -15,12 +15,23 @@ public class Employee extends Person{
     public int ID;
     public String jobTitle;
 
-    public void setInfo(String name, int age, char gender, double Salary, int ID, String jobTitle){
+    public void setInfo(String name, int age, char gender, double salary, int ID, String jobTitle){
         setInfo(name, age, gender);
         this.salary = salary;
         this.ID=ID;
         this.jobTitle=jobTitle;
     }
+    // method overloading: sets name, age, gender, salary, id, jonbtitle
+
+    public void work(){
+        System.out.println(name + " is working");
+    }
+
+    public String toString() {
+        return "Name is "+name+ ", age is "+age+", gender type: "+gender+", Salary= $"+salary+", Employee ID: "+ID
+                +", Job title is: "+jobTitle;
+    }
+
 
 
 }

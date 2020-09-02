22b8123f9e08b3515054afeafd50fcae9c5a54cf
cybertekschool <github@cybertekschool.com>
2020-09-02 19:19:36 -0400

    Day50 Class

---
+++ src/day50_Inheritance_Overriding/AccessModifiers.java
@@ -0,0 +1,11 @@
+package day50_Inheritance_Overriding;
+
+public class AccessModifiers {
+
+    public static int publicData = 100;
+    protected static int protectedData = 200;
+    static int defaultData = 300;
+    private int privateData = 400;
+
+
+}
---
+++ src/day50_Inheritance_Overriding/Test.java
@@ -0,0 +1,14 @@
+package day50_Inheritance_Overriding;
+
+public class Test extends AccessModifiers{
+
+    public static void main(String[] args) {
+
+        System.out.println(Test.publicData);
+        System.out.println(Test.protectedData);
+        System.out.println(Test.defaultData);
+     // System.out.println(Test.privateData);
+
+    }
+
+}

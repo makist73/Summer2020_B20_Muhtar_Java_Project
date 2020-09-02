a0312f9aa726ce0c2a2b70d6380225803a754d0e
cybertekschool <github@cybertekschool.com>
2020-09-02 18:31:50 -0400

    Office Hour: 09/02/2020

---
+++ src/Office_Hours/Practice_09_02_2020/ClassNote.java
@@ -0,0 +1,47 @@
+package Office_Hours.Practice_09_02_2020;
+
+public class ClassNote {
+    /*
+    09/02/2020
+Practice Topic: Constructor
+				static block
+
+package name: Practice_09_02_2020
+
+
+object
+	new Constructor
+
+
+Circle:
+	Attributes:
+			radius, PI, diameter
+
+		static: PI
+		instance: radius, diameter
+
+
+	circle1:  r=3    d=6		PI=3.14    area() = 30..
+
+	circle2:  r=5    d=10		PI=3.14    area() = 80..
+
+	circle3:  r=6    d=12		PI=3.14    area() = 100..
+
+
+reg methods:
+		AM   specifier  return-Type  name(Parameter){
+
+		}
+
+
+constructor: special methods
+
+		AM className(Parameter){
+
+		}
+
+
+
+
+     */
+}

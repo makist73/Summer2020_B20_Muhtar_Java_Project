a6d521072f09b40c92aaaeb667d1e172a771eb74
cybertekschool <github@cybertekschool.com>
2020-09-02 17:37:15 -0400

    Day49 Class

---
+++ src/Office_Hours/Practice_09_02_2020/Circle.java
@@ -0,0 +1,15 @@
+package Office_Hours.Practice_09_02_2020;
+
+public class Circle {
+    public double radius;
+    public double diameter;
+    public static double PI = 3.14;
+
+    public Circle(double radius){
+        this.radius = radius;
+        diameter = radius * 2;
+    }
+
+
+
+}

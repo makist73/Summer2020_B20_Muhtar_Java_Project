e78ffd71af0c75d116418b79e8a87e3d9b4685a3
cybertekschool <github@cybertekschool.com>
2020-09-02 20:17:17 -0400

    Day50 Class

--- src/day50_Inheritance_Overriding/CarTask/Car.java
+++ src/day50_Inheritance_Overriding/CarTask/Car.java
@@ -1,9 +1,15 @@
 package day50_Inheritance_Overriding.CarTask;
+/*
+Car:
+	start(): Insert the key
+			 twist ignition key to start
 
+ */
 public class Car {
 
     public void start(){
-
+        System.out.println("Insert the key");
+        System.out.println("twist ignition key to start");
     }
 
 
---
+++ src/day50_Inheritance_Overriding/CarTask/Honda.java
@@ -0,0 +1,10 @@
+package day50_Inheritance_Overriding.CarTask;
+/*
+Honda:
+	start(): Insert the key
+			 twist ignition key to start
+ */
+public class Honda extends Car{
+
+
+}

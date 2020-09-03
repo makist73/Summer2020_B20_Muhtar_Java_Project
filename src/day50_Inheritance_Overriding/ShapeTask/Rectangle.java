21ddcd8c7dcf598fa6e5c07301860945b73fa655
cybertekschool <github@cybertekschool.com>
2020-09-02 21:36:35 -0400

    Day50 Class

---
+++ src/day50_Inheritance_Overriding/ShapeTask/Rectangle.java
@@ -0,0 +1,25 @@
+package day50_Inheritance_Overriding.ShapeTask;
+
+public class Rectangle extends Shape{
+
+    public double width;
+    public double length;
+
+    public Rectangle(double width, double length){
+        this.width = width;
+        this.length = length;
+        area = calculateArea();
+        perimeter = calculatePerimeter();
+    }
+
+
+    public double calculateArea(){
+        return width * length;
+    }
+
+    public double calculatePerimeter(){
+        return (width + length) * 2;
+    }
+
+
+}

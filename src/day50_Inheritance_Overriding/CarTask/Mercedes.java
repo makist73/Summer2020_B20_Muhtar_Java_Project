333c6cc605a63be3160fc579bebb478f4e28c31d
cybertekschool <github@cybertekschool.com>
2020-09-02 20:21:14 -0400

    Day50 Class

---
+++ src/day50_Inheritance_Overriding/CarTask/Mercedes.java
@@ -0,0 +1,13 @@
+package day50_Inheritance_Overriding.CarTask;
+/*
+Mercedes:
+	start(): push the start button
+ */
+
+public class Mercedes extends Car{
+
+    public void start(){
+        System.out.println("push the start button");
+    }
+
+}

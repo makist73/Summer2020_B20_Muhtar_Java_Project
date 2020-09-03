20760b9dd4200aea98b4ed6a328fa6453e57b0a9
cybertekschool <github@cybertekschool.com>
2020-09-02 20:37:19 -0400

    Day50 Class

---
+++ src/day50_Inheritance_Overriding/CarTask/CarObjects.java
@@ -0,0 +1,24 @@
+package day50_Inheritance_Overriding.CarTask;
+
+public class CarObjects {
+
+
+    public static void main(String[] args) {
+
+        Honda honda = new Honda();
+        Mercedes mercedes = new Mercedes();
+        Tesla tesla = new Tesla();
+        Jeep jeep = new Jeep();
+
+        honda.start();
+        System.out.println("================================");
+        mercedes.start();
+        System.out.println("==================================");
+        tesla.start();
+        System.out.println("===================================");
+        jeep.start();
+
+
+    }
+
+}

6d51c483ed4ee924e4e2a03365fc17529763e627
cybertekschool <github@cybertekschool.com>
2020-09-02 20:25:13 -0400

    Day50 Class

---
+++ src/day50_Inheritance_Overriding/CarTask/Tesla.java
@@ -0,0 +1,12 @@
+package day50_Inheritance_Overriding.CarTask;
+/*
+Tesla:
+	start(): voice control
+ */
+public class Tesla extends Car{
+
+    public void start(){
+        System.out.println("voice control");
+    }
+
+}

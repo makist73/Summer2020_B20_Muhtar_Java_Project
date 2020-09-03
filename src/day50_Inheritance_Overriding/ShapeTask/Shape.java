c97562679bca3c02827a0d0e2de40a80a97be2cb
cybertekschool <github@cybertekschool.com>
2020-09-02 20:51:00 -0400

    Day50 Class

---
+++ src/day50_Inheritance_Overriding/ShapeTask/Shape.java
@@ -0,0 +1,23 @@
+package day50_Inheritance_Overriding.ShapeTask;
+/*
+	1. create a class called Shape
+						vairables: area, perimeter
+						methods: calculateArea(), calculatePerimeter()
+ */
+public class Shape {
+
+    public double area;
+    public double perimeter;
+
+
+    public double calculateArea(){
+        return 0;
+    }
+
+    public double calculatePerimeter(){
+        return 0;
+    }
+
+
+
+}

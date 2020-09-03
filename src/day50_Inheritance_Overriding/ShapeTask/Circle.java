1e2b891aa6ce369c06a30c1f9c53242a6f2c7ab6
cybertekschool <github@cybertekschool.com>
2020-09-02 21:04:52 -0400

    Day50 Class

---
+++ src/day50_Inheritance_Overriding/ShapeTask/Circle.java
@@ -0,0 +1,33 @@
+package day50_Inheritance_Overriding.ShapeTask;
+
+public class Circle extends Shape {
+    /*
+    variable:  area, perimeter
+    methods:  calculateArea, calculatePerimeter()
+     */
+
+    public double radius;
+    public double diameter;
+    public static double PI = 3.14;
+
+    public Circle(double radius){
+        this.radius = radius;
+        diameter = radius * 2;
+        area = calculateArea();
+        perimeter = calculatePerimeter();
+
+    }
+
+
+    public double calculateArea(){
+        return radius*radius*PI;
+    }
+
+
+    public double calculatePerimeter(){
+        return diameter*PI;
+    }
+
+
+
+}

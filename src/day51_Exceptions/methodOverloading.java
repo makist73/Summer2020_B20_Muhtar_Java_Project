0f88d6a64355f05fd90fa79af00728d4f6966632
cybertekschool <github@cybertekschool.com>
2020-09-05 11:07:01 -0400

    Day51 Class

---
+++ src/day51_Exceptions/methodOverloading.java
@@ -0,0 +1,18 @@
+package day51_Exceptions;
+
+public class methodOverloading {
+
+    public void method(int a){
+
+    }
+
+    private void method(double b){
+
+    }
+
+    protected long method(long c){
+        return 10;
+    }
+
+
+}
---
+++ src/day51_Exceptions/methodOverriding.java
@@ -0,0 +1,24 @@
+package day51_Exceptions;
+
+class A {
+
+    public void method(){
+
+    }
+
+}
+
+
+public class methodOverriding extends A {
+
+    @Override
+    public void method(){
+
+    }
+
+    @Override
+    public String toString(){
+        return "example";
+    }
+
+}

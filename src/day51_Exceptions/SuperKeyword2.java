a0d1f9fc3bd6e82db413072e41d8614d8514cd9f
cybertekschool <github@cybertekschool.com>
2020-09-05 12:45:30 -0400

    Day51 Class

--- src/day51_Exceptions/SuperKeyword2.java
+++ src/day51_Exceptions/SuperKeyword2.java
@@ -2,10 +2,24 @@ package day51_Exceptions;
 
 class B{
 
+    public B(){
+        System.out.println("Super class' default constructor");
+    }
+
 }
 
 
-public class SuperKeyword2 {
+public class SuperKeyword2 extends B{
+
+    public SuperKeyword2(){
+      //  super();  // call the super class' constructor
+        System.out.println("Sub class' default constructor");
+    }
+
+    public static void main(String[] args) {
+        new SuperKeyword2();
+
+    }
 
 
 }

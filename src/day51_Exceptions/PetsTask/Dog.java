4b3ee5b569f629010131fc20cd451fd2dac69319
cybertekschool <github@cybertekschool.com>
2020-09-05 14:34:01 -0400

    Day51 Class

---
+++ src/day51_Exceptions/PetsTask/Dog.java
@@ -0,0 +1,10 @@
+package day51_Exceptions.PetsTask;
+
+public class Dog extends Pet {
+
+    public Dog(String name, String breed, char gender, int age, String color) {
+        super(name, breed, gender, age, color);
+    }
+
+
+}

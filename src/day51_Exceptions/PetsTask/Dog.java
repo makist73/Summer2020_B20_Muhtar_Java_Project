87399476911c3c625fda5383fb6792e1a3a002e6
cybertekschool <github@cybertekschool.com>
2020-09-05 14:56:56 -0400

    Day51 Class

--- src/day51_Exceptions/PetsTask/Dog.java
+++ src/day51_Exceptions/PetsTask/Dog.java
@@ -4,9 +4,7 @@ public class Dog extends Pet {
 
 
     public Dog(String name, String breed, char gender, int age, String color ) {
-        super(name, breed, gender, age, color);
-
-
+        super(name, breed, gender, age, color);  // calling parent constructor (Mandatory)
     }
 
     public String toString() {

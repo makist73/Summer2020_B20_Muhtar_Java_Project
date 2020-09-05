2d02d271a3078d2746d6ed861ea62991b6551bc6
cybertekschool <github@cybertekschool.com>
2020-09-05 15:41:19 -0400

    Day51 Class

--- src/day51_Exceptions/ExceptionsIntro.java
+++ src/day51_Exceptions/ExceptionsIntro.java
@@ -5,9 +5,18 @@ public class ExceptionsIntro {
     public static void main(String[] args) {
         int[] arr = {1,2,3};
 
-        System.out.println(arr[100]); // unchecked exceptions (unexpected event)
+        System.out.println("Test started");
 
-      //  Thread.sleep(3000);  // checked exception (unwanted event)
+       // System.out.println(arr[100]); // unchecked exceptions (unexpected event)
+
+       // Thread.sleep(3000);  // checked exception (unwanted event)
+
+
+        String str = "Cybertek";
+
+        System.out.println( str.charAt(-1));  // unchecked
+
+        System.out.println("Step 2");
 
 
     }

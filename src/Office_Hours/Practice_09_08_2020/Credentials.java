d405bf44545d93c1c0406b6f44f6358129e67b15
cybertekschool <github@cybertekschool.com>
2020-09-08 17:59:46 -0400

    Office Hour: 09/08/2020

--- src/Office_Hours/Practice_09_08_2020/Credentials.java
+++ src/Office_Hours/Practice_09_08_2020/Credentials.java
@@ -5,4 +5,21 @@ public class Credentials {
     private String userName = "Cybertek";
     private String passWord = "CyberTek1234";
 
+    public String getUserName(){  // Read ONLY
+        return userName;
+    }
+
+    public void setUserName(String userName){ // Modify
+        this.userName = userName;
+    }
+
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
 }
--- src/Office_Hours/Practice_09_08_2020/TestCases.java
+++ src/Office_Hours/Practice_09_08_2020/TestCases.java
@@ -2,5 +2,43 @@ package Office_Hours.Practice_09_08_2020;
 
 public class TestCases {
 
+    public static void main(String[] args) {
+
+        Credentials c1 = new Credentials();
+
+      //  System.out.println(c1.userName); // No direct access to the private
+      //  System.out.println(c1.passWord); // No direct access to the private
+
+        System.out.println("User Name Entered: "+c1.getUserName());
+
+       // c1.userName = "Batch20";
+
+        c1.setUserName("Batch20");
+
+        System.out.println("New User Name Entered: "+c1.getUserName());
+        System.out.println("User Name Entered Again: "+c1.getUserName());
+
+        System.out.println("============================================");
+
+        System.out.println("Pass Word Entered: "+c1.getPassWord());
+
+       // c1.passWord = "JavaTurtles";
+
+        c1.setPassWord("JavaTurtles");
+
+        System.out.println("New Pass Word Entered: "+ c1.getPassWord());
+        System.out.println("New Pass Word Entered Again: "+c1.getPassWord());
+
+
+
+
+    }
+
+
+    /*
+    public String getUserName(){
+        return userName;
+    }
+*/
 
 }

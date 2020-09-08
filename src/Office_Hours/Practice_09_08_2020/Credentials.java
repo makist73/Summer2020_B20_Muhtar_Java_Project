f1c2c6521ce2d5a9fb056255dd25e2013beef8bd
cybertekschool <github@cybertekschool.com>
2020-09-08 17:22:20 -0400

    Office Hour: 09/08/2020

---
+++ src/Office_Hours/Practice_09_08_2020/Credentials.java
@@ -0,0 +1,8 @@
+package Office_Hours.Practice_09_08_2020;
+
+public class Credentials {
+
+    private String userName = "Cybertek";
+    private String passWord = "CyberTek1234";
+
+}
---
+++ src/Office_Hours/Practice_09_08_2020/TestCases.java
@@ -0,0 +1,6 @@
+package Office_Hours.Practice_09_08_2020;
+
+public class TestCases {
+
+
+}

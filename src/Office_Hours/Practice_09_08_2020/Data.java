0c2316c64962d3e8e0ec9a91d54173558228cb6e
cybertekschool <github@cybertekschool.com>
2020-09-08 17:20:22 -0400

    Office Hour: 09/08/2020

---
+++ src/Office_Hours/Practice_09_08_2020/Data.java
@@ -0,0 +1,31 @@
+package Office_Hours.Practice_09_08_2020;
+
+public class Data {
+
+     private int a = 100;
+
+    private static  void  method1(){
+
+    }
+
+    public void method2(){
+        System.out.println(a);
+        method1();
+    }
+
+
+}
+
+
+class Test{
+
+    public static void main(String[] args) {
+        Data obj1 = new Data();
+
+      //  System.out.println( obj1.a );
+    //    obj1.method1();
+
+
+    }
+
+}

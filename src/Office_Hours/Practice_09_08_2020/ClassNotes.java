0e3082f955c18cc4d16852d0b1b2a3fc7ae3f4af
cybertekschool <github@cybertekschool.com>
2020-09-08 18:12:50 -0400

    Office Hour: 09/08/2020

---
+++ src/Office_Hours/Practice_09_08_2020/ClassNotes.java
@@ -0,0 +1,51 @@
+package Office_Hours.Practice_09_08_2020;
+
+public class ClassNotes {
+
+    /*
+    09/08/2020
+Practice Topic: Encapsulation:
+						private
+						getter & setter
+
+
+package name: Practice_09_08_2020
+
+
+access modifiers: determines the visibility
+
+		public > protected > default > private
+
+Encapsulation: hiding the data by giving private. use public getter/setter instance methods to read/modify
+
+		public getter (READ ONLY) : Return method (return-type MUST be same with private data)
+									No parameter
+
+		public setter (MODIFY ONLY): void method
+									 passes a parameter (DataType MUST match)
+
+
+tomorrow: Inheritance
+		  5pm ~ 7pm
+
+
+OOP:
+	1. Encapsulation: data hiding
+
+	2. Inheritance: sub/super classes, IS A relation
+					sub class inherits all visible methods & variables
+					Overriding
+
+					easy to maintain
+					re-usable
+
+
+	3. Abstraction
+	4. Polymorphism
+
+
+
+next topics: final
+			 Abstraction
+     */
+}

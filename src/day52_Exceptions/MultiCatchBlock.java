ef148dbe6a5451648b2c0be7cd70414d11ddadf2
cybertekschool <github@cybertekschool.com>
2020-09-08 20:01:20 -0400

    Day52 Class

---
+++ src/day52_Exceptions/MultiCatchBlock.java
@@ -0,0 +1,54 @@
+package day52_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class MultiCatchBlock {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10,20};
+
+        try {
+            System.out.println(arr[20]); // ArrayIndexOutOfBound
+
+        }catch(ArithmeticException e){
+            System.out.println("Arithmetic Exception is handled");
+
+        }catch(ClassCastException e){
+            System.out.println("Class Cast Exception");
+
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array Index Out of Bound Exception");
+
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Index Out Of Bound Exception");
+
+        }catch (RuntimeException e){
+            System.out.println("Run Time Exception");
+
+        } // catch(NoSuchElementException e){ }
+
+        System.out.println("=====================================");
+
+        String str = "Cybertek";
+        try {
+            System.out.println(str.charAt(200));
+        }catch(RuntimeException e){  // runtimeexception can handle any unchecked exception
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("=====================================");
+        try {
+            Thread.sleep(3000);
+        }catch (Exception e){
+
+        }
+
+
+        System.out.println("Completed");
+
+
+
+    }
+
+}

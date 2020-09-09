45db625e76248b5e9c9e0952ba6fb438cff2ea61
cybertekschool <github@cybertekschool.com>
2020-09-09 17:39:44 -0400

    Office Hour: 09/09/2020

---
+++ src/Office_Hours/Practice_09_09_2020/Phone.java
@@ -0,0 +1,34 @@
+package Office_Hours.Practice_09_09_2020;
+/*
+Phone: brand, model, price, madeIn, call(), text()
+	static: brand, madeIn
+	instance: model, price, call(), text(), toString()
+ */
+
+public class Phone {
+
+    public static String brand;
+    public static String madeIn;
+
+    public String model;
+    public double price;
+
+    public Phone(String model, double price){
+          this.model  = model;
+          this.price = price;
+    }
+
+
+    public void call(long phoneNumber){
+        System.out.println("Phone is calling: "+phoneNumber);
+    }
+
+    public void text(long phoneNumber){
+        System.out.println("Phone is texting to: "+phoneNumber);
+    }
+
+    public String toString(){
+        return "Brand: "+brand+" Model: "+model+" Price: "+price+" Made in: "+madeIn;
+    }
+
+}

940ad3dc54a6b44d8b847025e41d561f244c50e1
cybertekschool <github@cybertekschool.com>
2020-09-09 19:36:02 -0400

    Office Hour: 09/09/2020

---
+++ src/day53_FinalKeyword/BreaktimeException.java
@@ -0,0 +1,32 @@
+package day53_FinalKeyword;
+
+public class BreaktimeException extends  RuntimeException{  // custom unchecked exception
+
+    public BreaktimeException(){
+        super("It's time for a break");
+        System.err.println("Time to take 15 minutes break");
+    }
+
+    public BreaktimeException(String str){
+        super(str);
+        System.err.println("Time to take 15 minutes break");
+    }
+
+}
+
+class Test{
+
+    public static void main(String[] args) {
+
+   //throw new RuntimeException("This is Runtime exception");
+      throw new BreaktimeException("You need to give us a break");
+
+     //   System.out.println("Test Completed");
+
+     //   throw new Test();
+
+    }
+
+}
+
+

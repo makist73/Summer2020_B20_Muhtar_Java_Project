d1231f9764f4368df0471647a623a4abb77b5a39
cybertekschool <github@cybertekschool.com>
2020-09-08 22:04:30 -0400

    Day52 Class

--- src/day52_Exceptions/ThrowPractice.java
+++ src/day52_Exceptions/ThrowPractice.java
@@ -1,4 +1,37 @@
 package day52_Exceptions;
 
+import java.time.LocalTime;
+
 public class ThrowPractice {
+
+    public static void main(String[] args) {
+
+        String driverName = "chrome";
+
+        if(driverName.equals("chrome")){
+            System.out.println("set up chrome");
+        }else if(driverName.equals("firefox")){
+            System.out.println("set up firefox");
+        }else if(driverName.equals("opera")){
+            System.out.println("set up opera");
+        }else{
+            throw new RuntimeException("Invalid browser name");
+        }
+
+        System.out.println("============================================================");
+
+        if(LocalTime.now().getMinute() == 45){
+            throw new RuntimeException("it's break time");
+          //  System.out.println("Hello");
+        }else{
+            System.out.println("Continue");
+        }
+
+
+        throw new ArithmeticException();
+      //  throw new RuntimeException();
+
+
+
+    }
 }

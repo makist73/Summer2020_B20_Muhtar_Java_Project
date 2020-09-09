ea804460ab97437e091004e303dec23201ac2627
cybertekschool <github@cybertekschool.com>
2020-09-09 18:35:42 -0400

    Office Hour: 09/09/2020

--- src/Office_Hours/Practice_09_09_2020/PhoneShop.java
+++ src/Office_Hours/Practice_09_09_2020/PhoneShop.java
@@ -1,6 +1,6 @@
 package Office_Hours.Practice_09_09_2020;
 
-public class PhoneShop {
+public class PhoneShop { // PhoneShop HAS An Iphone
 
     public static void main(String[] args) {
 
@@ -11,7 +11,21 @@ public class PhoneShop {
 
         System.out.println(iphone);
 
-        System.out.println("===========================");
+        System.out.println("========================================");
+
+        Samsung samsung = new Samsung("Galaxy Note20", 799.99);
+
+        System.out.println(samsung);
+        samsung.call(1234);
+        samsung.text(123556);
+
+        System.out.println("====================================");
+
+        Nokia nokia = new Nokia("Brick", 25);
+
+        System.out.println(nokia);
+        nokia.call(34567);
+        nokia.text(987654);
 
     }
 

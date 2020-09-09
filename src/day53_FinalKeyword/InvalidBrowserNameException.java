88002fb6cdb0a924d7a6cc0e8ecc07437ca43de5
cybertekschool <github@cybertekschool.com>
2020-09-09 19:54:45 -0400

    Day53 Class

---
+++ src/day53_FinalKeyword/InvalidBrowserNameException.java
@@ -0,0 +1,45 @@
+package day53_FinalKeyword;
+
+public class InvalidBrowserNameException extends RuntimeException{
+
+    public InvalidBrowserNameException(){
+        super("Invalid Browser Name");
+    }
+
+    public InvalidBrowserNameException(String str){
+        super(str);
+        System.err.println("Are you kidding");
+    }
+
+}
+
+
+class Test2{
+
+    public static void main(String[] args) {
+        String name = "bacth20";
+
+        switch(name.toLowerCase()){
+            case "chrome":
+                System.out.println("Chrome is selected");
+                break;
+
+            case "firefox":
+                System.out.println("Fire fox is selected");
+                break;
+
+            case "safari":
+                System.out.println("Safari is selected");
+                break;
+
+            default:
+                throw new InvalidBrowserNameException("Please enter a valid browser name");
+        }
+
+
+
+        //  throw new InvalidBrowserNameException();
+
+    }
+
+}

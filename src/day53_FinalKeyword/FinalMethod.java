8ea32e07b5b55120c63b2a4a1d48b89242eb3a01
cybertekschool <github@cybertekschool.com>
2020-09-09 21:23:52 -0400

    Day53 Class

--- src/day53_FinalKeyword/FinalMethod.java
+++ src/day53_FinalKeyword/FinalMethod.java
@@ -1,12 +1,39 @@
 package day53_FinalKeyword;
 
-class A{
+class  A{  // the class cannot have sub classes
 
+   // public final A(){  }
+
+
+
+    public final void method1(){
+        System.out.println("Enter username and password");
+    }
+
+    private final void method1(int a){
+
+    }
+
+
+    public final static void method2(int b){
+
+    }
 
 }
 
 
-public class FinalMethod {
+public class FinalMethod extends A{
+
+    /*
+    @Override
+    public void method1(){
+        System.out.println("Enter something else");
+    }
+*/
+
+    public final static void main(String[] args) {
+        System.out.println("Hello");
+    }
 
 
 }

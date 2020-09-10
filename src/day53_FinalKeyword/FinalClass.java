60b9b9954f404db9b4f1512dac814d23bb438c01
cybertekschool <github@cybertekschool.com>
2020-09-09 21:37:06 -0400

    Day53 Class

---
+++ src/day53_FinalKeyword/FinalClass.java
@@ -0,0 +1,18 @@
+package day53_FinalKeyword;
+
+class C{
+
+}
+
+public final class FinalClass  extends  C {  // final class can be a child class
+
+
+
+}
+
+
+//  class B extends  FinalClass{  }   // final class can never be a parent class
+
+
+
+// class D extends String{ } // String is final class, cannot extended

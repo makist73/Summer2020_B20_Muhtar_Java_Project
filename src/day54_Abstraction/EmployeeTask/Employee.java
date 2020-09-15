59752e66815ad8e8a69b63e890f8e24419a18236
cybertekschool <github@cybertekschool.com>
2020-09-15 19:10:14 -0400

    Office: 09/15/2020

---
+++ src/day54_Abstraction/EmployeeTask/Employee.java
@@ -0,0 +1,30 @@
+package day54_Abstraction.EmployeeTask;
+/*
+1.create a class named Employee (meant to be super class):
+			Attributes:
+					name, gender(final), DOfB(final), jobTitle, Salary
+
+					create constructor that can initialize lal those attributes
+						(even final variable you should initialize them within constructor)
+ */
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    public String name;
+    final public char gender; // constant
+    final public LocalDate DOfB; // constant
+    public String jobTitle;
+    public double salary;
+
+    public Employee(String name, char gender, LocalDate DOfB, String jobTitle, double salary) {
+        this.name = name;
+        this.gender = gender;
+        this.DOfB = DOfB;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+
+}

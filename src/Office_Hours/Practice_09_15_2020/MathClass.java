9d513b5b61c02a4b891ffa432c3fb613c9466737
cybertekschool <github@cybertekschool.com>
2020-09-15 18:01:17 -0400

    Office: 09/15/2020

---
+++ src/Office_Hours/Practice_09_15_2020/MathClass.java
@@ -0,0 +1,30 @@
+package Office_Hours.Practice_09_15_2020;
+
+public class MathClass {
+
+    public static void main(String[] args) {
+        // 10*10*10
+       double r1 =  Math.pow(10, 3);
+
+        System.out.println(r1);
+
+        //2*2*2*2*2 = 32
+        System.out.println(  Math.pow(2, 5));
+
+        // |-1| ==> 1
+        System.out.println( Math.abs(-1));
+
+        //4 ==> 2,  9 ==> 3,  25 ==> 5
+        System.out.println(Math.sqrt(10));
+        System.out.println( Math.sqrt(100) );
+
+        System.out.println(Math.sin(30));
+        System.out.println(Math.cos(45));
+        System.out.println(Math.tan(90));
+
+        System.out.println(Math.PI);
+        System.out.println( Math.E );
+
+    }
+
+}

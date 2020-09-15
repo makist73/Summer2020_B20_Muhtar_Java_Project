f736626cd4fd66132f3b4443893c193675b96085
cybertekschool <github@cybertekschool.com>
2020-09-15 18:32:57 -0400

    Office: 09/15/2020

---
+++ src/Office_Hours/Practice_09_15_2020/ClassNotes.java
@@ -0,0 +1,19 @@
+package Office_Hours.Practice_09_15_2020;
+
+public class ClassNotes {
+    /*
+    09/15/2020
+Practice Topic: Inheritance Review
+				Math class
+
+Package name: Practice_09_15_2020
+
+Math class: presented in "java.lang"
+
+Object class : presented in "java.lang"
+			implicitly implemented to all the classes
+
+
+
+     */
+}

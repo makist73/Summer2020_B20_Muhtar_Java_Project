0cb6b30515a49314d17142fc1e766578b9158718
cybertekschool <github@cybertekschool.com>
2020-09-16 19:31:17 -0400

    Day55 Class

---
+++ src/day55_Abstraction/ShapesTask/Shape.java
@@ -0,0 +1,22 @@
+package day55_Abstraction.ShapesTask;
+/*
+1. create an abstract class called Shape
+			attributes:
+				name(static), area, perimeter, hasVolume(static), volume
+			abstract methods: calculateArea(), calculatePerimeter(), calculateVolume()
+								return-types: double
+ */
+
+public abstract class Shape { //cannot create object, cannot be final
+
+    public static String name;
+    public double area;
+    public double perimeter;
+    public static boolean hasVolume;
+    public double volume;
+
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+    public abstract double calculateVolume();
+
+}

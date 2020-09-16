c8c827579049343a0a607427982832e8d7d59d2a
cybertekschool <github@cybertekschool.com>
2020-09-16 18:38:58 -0400

    Office Hour: 09/16/2020

---
+++ src/Office_Hours/Practice_09_16_2020/ClassNote.java
@@ -0,0 +1,53 @@
+package Office_Hours.Practice_09_16_2020;
+
+public class ClassNote {
+
+    /*
+    09/16/2020
+Practice Topic: Exceptions
+					unchecked & checked
+
+					try & catch & finally
+					throws
+
+					throw
+
+package name: Practice_09/16/2020
+
+
+exception handling:
+
+	1. try&catch (proper way): in a block
+
+			try{
+				exception code
+			}catch(ExceptionClass  e){
+				e.getMessage()
+			}finally{
+					// always gets executed
+			}
+
+		All exceptions in selenium are unchecked exception
+
+
+	2. throws(temporary fix): method signature, for chekced exception
+							caller will be responsible (bypass)
+
+
+
+
+
+throw: to manullay throw exception
+
+		browserName: chrome, firefox, safari, edge, IE..
+
+			if(browserName.equals("cybertek")){
+				throw ExceptionObject
+			}
+
+
+
+
+     */
+
+}

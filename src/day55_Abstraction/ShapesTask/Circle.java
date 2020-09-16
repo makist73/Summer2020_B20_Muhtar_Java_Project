1960915fae4715dabba67d5b335b395074046d72
cybertekschool <github@cybertekschool.com>
2020-09-16 19:51:04 -0400

    Day55 Class

--- src/day55_Abstraction/ShapesTask/Circle.java
+++ src/day55_Abstraction/ShapesTask/Circle.java
@@ -16,6 +16,18 @@ public class Circle extends Shape{
 
     public double radius;
 
+    /*
+    6 variables:
+            2 statics: name, hasVolume
+            4 instance: radius, area, perimeter, volume
+     */
+
+    public Circle(double radius){
+        this.radius = radius;
+        area = calculateArea();
+        perimeter = calculatePerimeter();
+        volume = calculateVolume();
+    }
 
     @Override
     public double calculateArea(){
@@ -30,6 +42,11 @@ public class Circle extends Shape{
     @Override
     public double calculateVolume(){
        return 0;
+    }
+
+    static{
+        name = "Circle";
+        hasVolume = false;
     }
 
 

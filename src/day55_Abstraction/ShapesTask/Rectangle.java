855684e5cf1cac0ab0ac718a44a958801455562f
cybertekschool <github@cybertekschool.com>
2020-09-16 19:58:20 -0400

    Day55 Class

---
+++ src/day55_Abstraction/ShapesTask/Rectangle.java
@@ -0,0 +1,14 @@
+package day55_Abstraction.ShapesTask;
+/*
+3. create a sub class of shape called Rectangle
+			attributes: width, length, name(static), area, perimeter, hasVolume(static), volume
+
+			override the abstract methods
+
+			add a constructor that takes two arguments for W & L of rectangle the and initialize the instance variables: width, length, area, perimeter, volume
+
+			add a static block that can initialize the static variables of the rectangle
+ */
+
+public class Rectangle {
+}

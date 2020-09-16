3613fc53880ec6b2a9c4b25a6870c3065f3ccc0b
cybertekschool <github@cybertekschool.com>
2020-09-16 18:23:02 -0400

    Office Hour: 09/16/2020

---
+++ src/Office_Hours/Practice_09_16_2020/ThrowsKeyword2.java
@@ -0,0 +1,17 @@
+package Office_Hours.Practice_09_16_2020;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsKeyword2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+       // System.out.println(9/0); // Must occure during runtime ONLY
+        Thread.sleep(3000);  // Must occure during compile time ONLY
+        System.out.println("Completed");
+
+
+    }
+
+}

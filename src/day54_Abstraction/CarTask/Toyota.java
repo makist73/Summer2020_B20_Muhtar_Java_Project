3f702dba483a222f0c5d569851ef2a4905b37999
cybertekschool <github@cybertekschool.com>
2020-09-15 21:42:54 -0400

    Day54 Class

---
+++ src/day54_Abstraction/CarTask/Toyota.java
@@ -0,0 +1,10 @@
+package day54_Abstraction.CarTask;
+
+public class Toyota extends Car{
+
+    @Override
+    public void start() {
+        System.out.println("Starting Toyota by: Inserting the key");
+    }
+
+}

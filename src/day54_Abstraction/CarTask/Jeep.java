67a5f2f5c232334f5ec0aaa9ea0aac6bb1dd40a1
cybertekschool <github@cybertekschool.com>
2020-09-15 21:45:08 -0400

    Day54 Class

---
+++ src/day54_Abstraction/CarTask/Jeep.java
@@ -0,0 +1,12 @@
+package day54_Abstraction.CarTask;
+
+public class Jeep extends Car {
+
+
+    @Override
+    public void start() {
+        System.out.println("Starting jeep by: jump start");
+    }
+
+
+}

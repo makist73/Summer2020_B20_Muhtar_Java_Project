565ba1a22c45c2c87099fc3a53ab031896b43261
cybertekschool <github@cybertekschool.com>
2020-09-15 21:40:06 -0400

    Day54 Class

---
+++ src/day54_Abstraction/CarTask/Tesla.java
@@ -0,0 +1,10 @@
+package day54_Abstraction.CarTask;
+
+public class Tesla extends Car{
+
+    @Override
+    public void start(){
+        System.out.println("Starting Tesla by: Voice Control");
+    }
+
+}

3563360879b07bbf705a812722785e293de6d41d
cybertekschool <github@cybertekschool.com>
2020-09-19 10:16:04 -0400

    Day56 Class

---
+++ src/day56_Abstraction/PhoneTask/Phone.java
@@ -0,0 +1,39 @@
+package day56_Abstraction.PhoneTask;
+
+/*
+    1. create an abstract class called Phone
+            attributes: brand, model, price, size
+            abstract methods: calling(), texting();
+            instance method: toString()
+
+            add a constructor that can initialize all the instances
+ */
+
+public abstract class Phone {
+
+    public String brand;
+    public String model;
+    public double price;
+    public String size;
+
+    public abstract void calling(long phoneNumber);
+    public abstract void texting(long phoneNumber);
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                ", size='" + size + '\'' +
+                '}';
+    }
+
+    public Phone(String brand, String model, double price, String size) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.size = size;
+    }
+
+}

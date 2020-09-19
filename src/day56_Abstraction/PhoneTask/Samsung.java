6fa2e9f826547af41fc491c5bd371c19510b4ae2
cybertekschool <github@cybertekschool.com>
2020-09-19 10:36:33 -0400

    Day56 Class

--- src/day56_Abstraction/PhoneTask/Samsung.java
+++ src/day56_Abstraction/PhoneTask/Samsung.java
@@ -1,9 +1,13 @@
 package day56_Abstraction.PhoneTask;
 
-public class Samsung extends Phone{
+public final class Samsung extends Phone{
 
     public Samsung( String model, double price, String size) {
         super("Samsung", model, price, size);
+        if(price <= 0 || price >1200){
+            throw new RuntimeException("Invalid Price: "+price );
+        }
+
     }
 
     @Override
@@ -16,6 +20,10 @@ public class Samsung extends Phone{
         System.out.println("Samsung is texting to "+phoneNumber);
     }
 
+    public void freezing(){
+        System.out.println("Samsung is freezing");
+    }
+
     @Override
     public String toString() {
         return "Samsung Phone{" +
@@ -25,4 +33,5 @@ public class Samsung extends Phone{
                 ", size='" + size + '\'' +
                 '}';
     }
+
 }

638ea09f0a80287dfb328d0674dc0453a41f13f4
cybertekschool <github@cybertekschool.com>
2020-09-19 12:35:20 -0400

    Day56 Class

---
+++ src/day56_Abstraction/AnimalTask/Animal.java
@@ -0,0 +1,9 @@
+package day56_Abstraction.AnimalTask;
+
+public abstract class Animal { // only the common features for all animals
+
+    public abstract void eat();
+    public abstract void sleep();
+
+
+}
---
+++ src/day56_Abstraction/AnimalTask/Flyable.java
@@ -0,0 +1,9 @@
+package day56_Abstraction.AnimalTask;
+
+public interface Flyable {
+
+    boolean hasWings = true;
+
+    public abstract void fly();
+
+}

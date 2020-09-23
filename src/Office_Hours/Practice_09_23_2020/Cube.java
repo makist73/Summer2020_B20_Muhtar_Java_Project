f51447da35c5c2e36fbaa845ce4036b8c03a7c3d
cybertekschool <github@cybertekschool.com>
2020-09-23 18:17:28 -0400

    Office Hour: 09/23/2020

--- src/Office_Hours/Practice_09_23_2020/Cube.java
+++ src/Office_Hours/Practice_09_23_2020/Cube.java
@@ -1,9 +1,25 @@
 package Office_Hours.Practice_09_23_2020;
 
-public class Cube  {
+public class Cube extends Shape implements Volume {
 
     public double side;
 
 
+    @Override
+    public double calculateArea() {
+        return Math.pow(side, 2) * 6; // side * side * 6
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return side * 4 * 6;
+    }
+
+
+
+    @Override
+     public double calculateVolume() {
+        return Math.pow(side,3);
+    }
 
 }

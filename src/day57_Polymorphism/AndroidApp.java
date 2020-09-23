8987dd80497ebdb422cc9a02733ab07bcd5c71e1
cybertekschool <github@cybertekschool.com>
2020-09-23 19:42:38 -0400

    Day57 Class

---
+++ src/day57_Polymorphism/AndroidApp.java
@@ -0,0 +1,11 @@
+package day57_Polymorphism;
+/*
+  2. create an interface named AndroidApp that can inherit from Downloadable
+            variable: AppStoreName
+ */
+public interface AndroidApp extends Downloadable {
+
+    String AppStoreName = "Google Play";  // public static final
+    // 1 abstract method
+
+}

6b76be80e6bfeb8af457ab4e81c96ce6b3c72443
cybertekschool <github@cybertekschool.com>
2020-09-23 19:54:17 -0400

    Day57 Class

---
+++ src/day57_Polymorphism/Phone.java
@@ -0,0 +1,32 @@
+package day57_Polymorphism;
+
+/*
+    4. create an abstract class named Phone
+            instance variables: brand, model, price, size
+            abstract methods: texting(), calling()
+
+            add constructor that can initialize the fields
+
+            if the price is set to negative or 0, the system should throw an exception with a message of:
+                    Invalid Price, cannot be negative
+ */
+
+public abstract class Phone {
+
+    public String brand, model, size;
+    public double price;
+
+    public Phone(String brand, String model, String size, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.size = size;
+        if(price <=0){
+            throw new RuntimeException("Price of the phone cannot be negative or zero");
+        }
+        this.price = price;
+    }
+
+    public abstract void texting(long phoneNumber);
+    public abstract void calling(long phoneNumber);
+
+}

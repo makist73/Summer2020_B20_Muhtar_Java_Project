9ece5e649ebf3ca90d1cdb406e53f6cf1ebe9123
cybertekschool <github@cybertekschool.com>
2020-09-23 19:46:06 -0400

    Day57 Class

---
+++ src/day57_Polymorphism/AppleApp.java
@@ -0,0 +1,11 @@
+package day57_Polymorphism;
+/*
+ 3. create an interface named AppleApp that can inherit from Downloadable
+            variable: AppStoreName
+ */
+public interface AppleApp extends Downloadable {
+
+    String AppStoreName = "App Store";
+    // 1 abstract method
+
+}

c64afc9e0f8ea1cc03d2cba800fac596186ed5ac
cybertekschool <github@cybertekschool.com>
2020-09-23 18:10:33 -0400

    Office Hour: 09/23/2020

---
+++ src/Office_Hours/Practice_09_23_2020/Cube.java
@@ -0,0 +1,9 @@
+package Office_Hours.Practice_09_23_2020;
+
+public class Cube  {
+
+    public double side;
+
+
+
+}
--- src/Office_Hours/Practice_09_23_2020/Shape.java
+++ src/Office_Hours/Practice_09_23_2020/Shape.java
@@ -4,6 +4,7 @@ public abstract class Shape {
 
     public double area;
     public double perimeter;
+    public double volume;
 
     public abstract double calculateArea();
     public abstract double calculatePerimeter();

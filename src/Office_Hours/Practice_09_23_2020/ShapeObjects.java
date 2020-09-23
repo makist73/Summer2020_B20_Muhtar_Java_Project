9c5f6648cc186dc94ac95ae6ec70371bc0de8681
cybertekschool <github@cybertekschool.com>
2020-09-23 18:25:29 -0400

    Office Hour: 09/23/2020

---
+++ src/Office_Hours/Practice_09_23_2020/ShapeObjects.java
@@ -0,0 +1,21 @@
+package Office_Hours.Practice_09_23_2020;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+      //  Shape shape1 = new Shape();
+
+        Square square1 = new Square(10);
+
+        System.out.println( square1.calculateArea() );
+        System.out.println(square1.area);
+        System.out.println(square1.perimeter);
+        System.out.println(square1.calculatePerimeter());
+        System.out.println(square1);
+
+
+
+
+    }
+
+}
--- src/Office_Hours/Practice_09_23_2020/Square.java
+++ src/Office_Hours/Practice_09_23_2020/Square.java
@@ -23,5 +23,13 @@ public class Square extends Shape {
         return 4*side;
     }
 
-
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", area=" + area +
+                ", perimeter=" + perimeter +
+                ", volume=" + volume +
+                '}';
+    }
 }

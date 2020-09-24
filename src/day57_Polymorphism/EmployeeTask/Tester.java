f7a168048243ba6551c81e64c6a36c19cb12cc7b
cybertekschool <github@cybertekschool.com>
2020-09-23 21:37:36 -0400

    Day57 Class

---
+++ src/day57_Polymorphism/EmployeeTask/Tester.java
@@ -0,0 +1,15 @@
+package day57_Polymorphism.EmployeeTask;
+
+public class Tester extends Employee {
+
+
+    public Tester(String name, int id, String jobTitle, double salary, char gender) {
+        super(name, id, jobTitle, salary, gender);
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Tester "+name+" is working");
+    }
+
+}

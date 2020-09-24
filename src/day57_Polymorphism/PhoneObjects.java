f66d49062089273e8e5962fad1b10983c38e774d
cybertekschool <github@cybertekschool.com>
2020-09-23 20:50:21 -0400

    Day57 Class

--- src/day57_Polymorphism/PhoneObjects.java
+++ src/day57_Polymorphism/PhoneObjects.java
@@ -4,7 +4,7 @@ public class PhoneObjects {
     public static void main(String[] args) {
         Iphone iphone = new Iphone("12", "5.3", 1000);
         Samsung samsung = new Samsung("S20", "6.1", 899 );
-        Huawei huawei = new Huawei("Robery", "5.3", 25);
+        Huawei huawei = new Huawei("Robbery", "5.3", 25);
 
         System.out.println(iphone);
         System.out.println(samsung);

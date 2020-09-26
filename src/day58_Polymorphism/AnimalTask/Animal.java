a71b1418b872b2040898fab94e7c88f7dc41bbdb
cybertekschool <github@cybertekschool.com>
2020-09-26 10:08:04 -0400

    Day58 Class

---
+++ src/day58_Polymorphism/AnimalTask/Animal.java
@@ -0,0 +1,30 @@
+package day58_Polymorphism.AnimalTask;
+
+/*
+1. cerate a class Animal
+            variables: age, gender
+            methods: eat, sleep, toString
+
+        DO NOT make it abstract yet we need this for today's example
+
+ */
+
+public class Animal {
+
+        public int age;
+        public char gender;
+
+    public Animal(int age, char gender) {
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+
+    public void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+
+}

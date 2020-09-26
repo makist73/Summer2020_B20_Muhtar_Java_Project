70311ce2a786d10784b7eef155289fdbf4dbf7ef
cybertekschool <github@cybertekschool.com>
2020-09-26 10:18:45 -0400

    Day58 Class

---
+++ src/day58_Polymorphism/AnimalTask/Dog.java
@@ -0,0 +1,30 @@
+package day58_Polymorphism.AnimalTask;
+/*
+3. create subclass of Animal named Dog
+            variable: dogName
+            methods: bark, toString
+ */
+
+public class Dog extends Animal{
+
+    public String dogName;
+
+    public Dog(String dogName,int age, char gender) {
+        super(age, gender);
+        this.dogName = dogName;
+    }
+
+    public void bark(){
+        System.out.println("Dog "+dogName+" is barking");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "dogName='" + dogName + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+}

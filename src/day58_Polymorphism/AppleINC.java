462a96e5af0f29b8b864c69527e666dc9ce3c508
cybertekschool <github@cybertekschool.com>
2020-09-26 12:50:21 -0400

    Day58 Class

---
+++ src/day58_Polymorphism/AppleINC.java
@@ -0,0 +1,71 @@
+package day58_Polymorphism;
+
+import day57_Polymorphism.EmployeeTask.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AppleINC {
+
+    public static void main(String[] args) {
+
+        Employee[] workers =  {
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Tester("Musa Moylam", 123456, "SDET", 130000, 'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new ScrumMaster("Kamil", 78945, "Scrum Master", 120000, 'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new ScrumMaster("Kamil", 78945, "Scrum Master", 120000, 'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new Developer("Abdul",123456789,"QA",110000,'M'),
+                new ScrumMaster("Kamil", 78945, "Scrum Master", 120000, 'M')
+
+        };
+
+        ArrayList<Employee> scrumTeam = new ArrayList<>(Arrays.asList(workers));
+        int countTester = 0;
+        int countDeveloper = 0;
+        int countScrumMaster = 0;
+
+        for(Employee each : scrumTeam){
+            if(each instanceof Tester){
+                countTester++;
+            }else if(each instanceof Developer){
+                countDeveloper++;
+            }else{
+                countScrumMaster++;
+            }
+        }
+
+
+
+        System.out.println("Tester: "+countTester);
+        System.out.println("Developer: "+countDeveloper);
+        System.out.println("Scrum Master: "+countScrumMaster);
+
+
+
+
+
+
+
+
+
+    }
+
+}

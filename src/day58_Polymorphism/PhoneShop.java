627438e96b3c971fd8432f1c23523b74946ef2bf
cybertekschool <github@cybertekschool.com>
2020-09-26 12:15:05 -0400

    Day58 Class

--- src/day58_Polymorphism/PhoneShop.java
+++ src/day58_Polymorphism/PhoneShop.java
@@ -21,7 +21,7 @@ public class PhoneShop {
                 new Iphone("12", "6.1", 1000),
                 new Samsung("S20", "7", 1100),
                 new Samsung("S20", "7", 1100),
-                new Samsung("S20", "7", 1100),
+                new Iphone("12", "6.1", 1000),
                 new Samsung("S20", "7", 1100)
         };
 
@@ -31,14 +31,34 @@ public class PhoneShop {
         int countSamSung = 0;
         int countHuaWei =0;
 
+        /*
         for(int i = 0; i <= phoneShop.size()-1; i++){
-            if( phoneShop.get(i) instanceof Iphone  ){
+            Phone each = phoneShop.get(i);  // get each objects from array
+
+            if( each instanceof Iphone  ){ // checks if the objects is Iphone
                 countIphone++;
-            }else if(phoneShop.get(i) instanceof Samsung){
+            }else if(each instanceof Samsung){ // checks if the object is samsung
                 countSamSung++;
+            }else {
+                countHuaWei++;
             }
 
         }
+        */
+
+        for(Phone each  : phoneShop) {
+            if (each instanceof Iphone) {
+                countIphone++;
+            }else if (each instanceof Samsung) {
+                countSamSung++;
+            }else{
+                countHuaWei++;
+            }
+        }
+
+        System.out.println("Iphone: "+countIphone);
+        System.out.println("Samsung: "+countSamSung);
+        System.out.println("Huawei: "+countHuaWei);
 
 
 

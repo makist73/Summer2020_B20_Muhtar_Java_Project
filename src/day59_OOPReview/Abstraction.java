d5661060dacb5ee7d8ad8bfd1a178e7f898ae4de
cybertekschool <github@cybertekschool.com>
2020-09-30 21:12:11 -0400

    Day59 Class

---
+++ src/day59_OOPReview/Abstraction.java
@@ -0,0 +1,41 @@
+package day59_OOPReview;
+
+interface X{
+    public abstract void method1();
+    void method2();
+
+    int a = 10; // public static final
+    int b = 20;
+
+   // X(){ }
+
+  //  static { }
+
+}
+
+abstract  class Y{
+        int a;
+        static int b;
+
+    public abstract void method1();
+
+    Y(){
+
+    }
+
+    static{
+
+    }
+
+}
+
+
+
+public class Abstraction {
+    public static void main(String[] args) {
+     //   Y obj = new Y();
+      //  X obj2 = new X();
+
+    }
+
+}

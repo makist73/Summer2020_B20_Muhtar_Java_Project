0b79d227e607457e077f252747543f28103f91f3
cybertekschool <github@cybertekschool.com>
2020-10-03 16:53:51 -0400

    Day60 Class

---
+++ src/day60_Collection/Iterator_Practice2.java
@@ -0,0 +1,19 @@
+package day60_Collection;
+
+import java.util.ArrayList;
+
+public class Iterator_Practice2 {
+
+    public static void main(String[] args) {
+    String[] arr = {"Ahmed", "Aalia", "Abzal", "Ahmed", "Virginia", "Ahmed", "Ahmed"};
+
+
+        ArrayList<String> names = new ArrayList<>();
+
+
+
+
+    }
+
+
+}

c2b4c64324681c25ae248fd766b6e784145ff56b
cybertekschool <github@cybertekschool.com>
2020-10-07 17:32:03 -0400

    Office Hour: 10/07/2020

---
+++ src/Office_Hours/Practice_10_07_2020/ClassNotes.java
@@ -0,0 +1,46 @@
+package Office_Hours.Practice_10_07_2020;
+
+public class ClassNotes {
+    /*
+    10/07/2020
+Practice Topics: Collection:
+							List Interface
+							Set Interface
+							Iterable Interface
+
+
+Package name: Practice_10_07_2020
+
+
+Collection: Interface, extended by List, Set, Queue
+
+List vs Set: size is dynamic, only supports Objects
+
+	List: accepts duplicated objects, has index number, size is dynamic
+
+		ArrayList: Array Based
+					retriving object is faster ( get() )
+
+		LinkedList: doublly linked (fastest)
+					add & remove functions are faster ( add(), addAll(), remove(), removeAll(), retainAll(), removeIf() )
+
+		Vector: is synchronized
+				advantage: Thread-safe
+
+		Stack: is synchronized
+				pop(): Last in first out order
+
+
+	Set: does not accepts duplicated objects, does not have index number, size is dynamic
+
+
+
+synchronized :  Thread-safe
+		advantage: Thread-safe
+		disadvantage: slow
+
+Thread: process of scheduling the object by the operating system
+
+Thread-safety: in multiple threads, each thread gets executed one at a time
+     */
+}
---
+++ src/Office_Hours/Practice_10_07_2020/ListPractice.java
@@ -0,0 +1,20 @@
+package Office_Hours.Practice_10_07_2020;
+
+import java.util.*;
+
+public class ListPractice {
+
+    public static void main(String[] args) {
+
+        List<String> list = new Vector<>();
+        list.addAll(Arrays.asList("Nurbiye", "Emine", "Dawud", "Emine", "Emine") );
+        //   index:                 0         1          2         3        4
+
+        System.out.println(list);
+        System.out.println(list.get(2));
+
+
+
+    }
+
+}

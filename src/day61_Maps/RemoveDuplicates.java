b93046e297a0ef949f134aec3277ebff48e6edc5
cybertekschool <github@cybertekschool.com>
2020-10-07 19:37:21 -0400

    Day61 Class

---
+++ src/day61_Maps/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package day61_Maps;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,3,4,5,6,1,2,3,4,5,6));
+
+        LinkedHashSet<Integer> set = new LinkedHashSet<>(list); // contains the unique objects
+        list.clear();
+        list.addAll( set );
+
+        System.out.println(list);
+
+
+
+
+
+
+
+
+    }
+
+}

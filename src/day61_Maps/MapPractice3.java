804317f3302cfb37eddfc55e714c6c26f9303841
cybertekschool <github@cybertekschool.com>
2020-10-07 21:41:33 -0400

    Day61 Class

---
+++ src/day61_Maps/MapPractice3.java
@@ -0,0 +1,26 @@
+package day61_Maps;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapPractice3 {
+
+    public static void main(String[] args) {
+        Map<String, String> countries = new LinkedHashMap<>();
+                countries.put("East Turkistan", "Kashgar");
+                countries.put("Uzbekistan", "Tashkent");
+                countries.put("Kyrgyzstan","Bishkek");
+                countries.put("Spain", "Madrid");
+                countries.put("USA", "Washington");
+                countries.put("Ukraine", "Kiev");
+                countries.put("Azerbaijan", "Karabagh");
+                countries.put("Turkey","Ankara");
+
+
+                // country name is countryName, Capital city is cityName
+
+
+
+    }
+
+}

1afa46da5bf9d328ae58555387540115e564d5dd
cybertekschool <github@cybertekschool.com>
2020-10-17 11:46:19 -0400

    Day62 Class

---
+++ src/day62_Maps/ListOfMaps.java
@@ -0,0 +1,36 @@
+package day62_Maps;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ListOfMaps {
+
+    public static void main(String[] args) {
+        Map<String, String> scrum1 = new LinkedHashMap<>();
+        scrum1.put("Azat","Senior SoftwareDeveloper");
+        scrum1.put("Nurahmet","SDET");
+        scrum1.put("Arman","QA");
+        scrum1.put("Abide","SDET");
+        scrum1.put("Jeniffer","BA");
+
+        Map<String, String> scrum2 = new LinkedHashMap<>();
+        scrum2.put("Hajar", "Developer");
+        scrum2.put("Dania", "SDET");
+        scrum2.put("Rani", "PO");
+        scrum2.put("Jak", "SM");
+        scrum2.put("Michel", "SDET");
+
+        Map<String, String> scrum3 = new LinkedHashMap<>();
+        scrum3.put("Imran", "Senior SoftwareDeveloper");
+        scrum3.put("Dilfinar", "SDET");
+        scrum3.put("Atilla", "QA");
+        scrum3.put("Aidana", "SDET");
+        scrum3.put("Kamran", "BA");
+
+
+
+
+
+    }
+
+}

6650d33229698f5597f7275d8ad2bf46c9c46dee
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-04 13:11:32 -0500

    Boot Camp

---
+++ src/bootCamp/Notes.java
@@ -0,0 +1,90 @@
+package bootCamp;
+
+public class Notes {
+    /*
+
+    Java Bootcamp
+
+    Practice topics:
+                String: immutable object
+                	StringBuilder & StringBuffer: mutable objects
+                		Builder: not thread-safe
+                		Buffer: thread-safe
+
+                Loops: repeat
+                	for:
+                		for each:
+
+                	while:
+
+                	do-while
+
+                Arrays: size is fixed
+                		primitives & objects both supported
+                		can be multi-dimensional
+
+
+            		Wrapper Classs
+
+                Arraylist: size is dynamic
+                			Only supports objects
+
+
+                Methods:
+
+                Static & instance:
+                		static: belong to Class
+                				one copy
+
+                				only accepts static
+
+                		instance: belong to object
+                				multiple copies
+
+
+                Constructor & static block
+
+                		constructor: used for creating object . initializing instance
+                					execution depends on object
+
+
+                		static block: initializing statics
+                					only runs one time, before everything
+
+
+
+                4 access modifiers:
+                		private:
+                		default:
+                		protected:
+                		public:
+OOP: reusable
+	 less memory
+	 organized
+	 easy to maintain
+
+
+                OOP Encapsulation: hiding the data
+
+                		getter (read)
+                		setter (write)
+
+
+                OOP Inheritance: IS A relation (super & sub classes), extends keyword
+
+                Method Overriding: same method, different implementation
+                					MUST happen in sub class
+                					ONLY instance methods (non-static, private , final)
+                					access modifier must eb same or more visible
+
+                Final keyword
+                OOP Abstraction
+                OOP Polymorphism
+                Exceptions & handling
+                Collection
+
+
+
+
+     */
+}

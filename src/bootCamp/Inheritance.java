4c1c6494b34b9c3f10563d04a44e552faadd2af4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-04 15:10:59 -0500

    Boot Camp

--- src/bootCamp/Inheritance.java
+++ src/bootCamp/Inheritance.java
@@ -1,21 +1,27 @@
 package bootCamp;
 
-class  Shape{
+interface Volume {
+    int a = 100;  // public static final
+    double volume();
+}
+
+abstract class  Shape{
+
     public String name;
 
-    protected double area(){
-        return 0;
-    }
+    protected abstract double area();
 
 }
 
 final class Rectangle extends Shape{
+
     public double length, width;
 
     @Override
     public double area(){
         return length * width;
     }
+
 
 }
 
@@ -28,10 +34,30 @@ final class Circle extends Shape{
     }
 
 
+
 }
+
+final class Cube extends Shape implements Volume{
+
+    @Override
+    protected double area() {
+        return 0;
+    }
+
+
+    @Override
+    public double volume() {
+        return 0;
+    }
+}
+
+
 
 public class Inheritance {
     public static void main(String[] args) {
 
+        Circle c1 = new Circle();
+
+        System.out.println(Volume.a);
     }
 }
